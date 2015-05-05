4df6bb944908276c3927c24542e7cf0e4f9d765f
obrestad <eigilo@hig.no>
2015-05-05 17:38:10 +0200

    Removed MySQL HAProxy, as it seems unreliable.

--- manifests/mysqlcluster.pp
+++ manifests/mysqlcluster.pp
@@ -12,11 +12,11 @@ class profile::mysqlcluster {
     galera_package_name => "galera-3",
     vendor_type         => "mariadb",
     root_password       => $rootpassword,
-    local_ip            => $::ipaddress_eth1,
+    local_ip            => "0.0.0.0",
     before		=> Service["haproxy"],
     override_options    => {
       'mysqld' => {
-        'port' => '3307',
+        'port' => '3306',
       }
     },
   }->
@@ -42,26 +42,26 @@ class profile::mysqlcluster {
     user       => 'haproxy_check@%',
   }->
   
-  ::haproxy::listen { 'mysql-cluster':
-    collect_exported => false,
-    mode      => 'tcp',
-    ipaddress    => '*',
-    ports      => '3306',
-    options       => {
-      'option'  => [ 'tcplog',
-                   'mysql-check user haproxy_check'],
-      'balance' => 'roundrobin',
-    }
-  }->
+#  ::haproxy::listen { 'mysql-cluster':
+#    collect_exported => false,
+#   mode      => 'tcp',
+#    ipaddress    => '*',
+#    ports      => '3306',
+#    options       => {
+#      'option'  => [ 'tcplog',
+#                   'mysql-check user haproxy_check'],
+#      'balance' => 'roundrobin',
+#    }
+#  }->
   anchor { "profile::mysqlcluster::end" : }
 
-  ::Haproxy::Balancermember <<| listening_service == 'mysql-cluster' |>>
-
-  @@::haproxy::balancermember { $::fqdn:
-    listening_service => 'mysql-cluster',
-    server_names      => $::hostname,
-    ipaddresses       => $::ipaddress_eth1,
-    ports             => '3307',
-    options           => 'check',
-  }
+#  ::Haproxy::Balancermember <<| listening_service == 'mysql-cluster' |>>
+#
+#  @@::haproxy::balancermember { $::fqdn:
+#    listening_service => 'mysql-cluster',
+#    server_names      => $::hostname,
+#    ipaddresses       => $::ipaddress_eth1,
+#    ports             => '3307',
+#    options           => 'check',
+#  }
 }
