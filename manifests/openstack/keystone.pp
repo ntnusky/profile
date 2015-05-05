431ba00dbfa920041631c0ff16171491ab88ad2b
obrestad <eigilo@hig.no>
2015-05-05 15:56:01 +0200

    Added initial keystone setup. Creating keystone database.

---
+++ manifests/openstack/keystone.pp
@@ -0,0 +1,16 @@
+class profile::openstack::keystone {
+  $password = hiera("profile::mysql::keystonepass")
+  $allowed_hosts = hiera("profile::mysql::allowed_hosts")
+  
+  class { "::keystone::db::mysql":
+    user          => 'keystone',
+    password      => $password,
+    allowed_hosts => $allowed_hosts,
+    dbname        => 'keystone',
+    require       => Anchor['profile::mysqlcluster::end'],
+  }
+
+  #class{'keystone':
+  #}
+    
+}
