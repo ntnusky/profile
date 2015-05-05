3c6d0d41383941da0daea1af4539a620e5a034c1
obrestad <eigilo@hig.no>
2015-05-05 16:49:23 +0200

    Typo in keystone. Added anchors.

--- manifests/openstack/keystone.pp
+++ manifests/openstack/keystone.pp
@@ -15,6 +15,8 @@ class profile::openstack::keystone {
   
   include ::profile::openstack::repo
   
+  anchor { "profile::openstack::keystone::begin" : } ->
+
   class { "::keystone::db::mysql":
     user          => 'keystone',
     password      => $password,
@@ -24,24 +26,26 @@ class profile::openstack::keystone {
   } ->
 
   class { '::keystone':
-    admin_token         => $admin_token
+    admin_token         => $admin_token,
     database_connection => $database_connection,
     verbose             => $verbose,
     debug               => $debug,
     enabled             => true,
     admin_bind_host     => "0.0.0.0",
-  }
+  } ->
    
   class { '::keystone::roles::admin':
     email        => $admin_email,
     password     => $admin_pass,
     admin_tenant => 'admin',
-  }
+  } ->
   
   class { 'keystone::endpoint':
     public_url   => "http://${public_ip}:5000",
     admin_url    => "http://${admin_ip}:35357",
     internal_url => "http://${admin_ip}:5000",
     region       => $region,
-  }
+  } ->
+  
+  anchor { "profile::openstack::keystone::end" : }
 }
