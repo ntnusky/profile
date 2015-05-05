cea73abf55ba382ba4f488d9daa2ba393caef5fe
obrestad <eigilo@hig.no>
2015-05-05 16:46:10 +0200

    Added keystone to controllers.

--- manifests/openstack/keystone.pp
+++ manifests/openstack/keystone.pp
@@ -1,6 +1,17 @@
 class profile::openstack::keystone {
   $password = hiera("profile::mysql::keystonepass")
   $allowed_hosts = hiera("profile::mysql::allowed_hosts")
+  $mysql_ip = hiera("profile::mysql::ip")
+
+  $region = hiera("profile::region")
+  $admin_ip = hiera("profile::api::admin::ip")
+  $public_ip = hiera("profile::api::public::ip")
+  
+  $admin_token = hiera("profile::keystone::admin_token")
+  $admin_email = hiera("profile::keystone::admin_email")
+  $admin_pass = hiera("profile::keystone::admin_password")
+
+  $database_connection = "mysql://keystone:${password}@${mysql_ip}/keystone"
   
   include ::profile::openstack::repo
   
@@ -10,9 +21,27 @@ class profile::openstack::keystone {
     allowed_hosts => $allowed_hosts,
     dbname        => 'keystone',
     require       => Anchor['profile::mysqlcluster::end'],
-  }
+  } ->
 
-  #class{'keystone':
-  #}
-    
+  class { '::keystone':
+    admin_token         => $admin_token
+    database_connection => $database_connection,
+    verbose             => $verbose,
+    debug               => $debug,
+    enabled             => true,
+    admin_bind_host     => "0.0.0.0",
+  }
+   
+  class { '::keystone::roles::admin':
+    email        => $admin_email,
+    password     => $admin_pass,
+    admin_tenant => 'admin',
+  }
+  
+  class { 'keystone::endpoint':
+    public_url   => "http://${public_ip}:5000",
+    admin_url    => "http://${admin_ip}:35357",
+    internal_url => "http://${admin_ip}:5000",
+    region       => $region,
+  }
 }
