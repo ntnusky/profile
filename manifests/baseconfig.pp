11feec30d5c09837cb576f4dae2736ca30674351
obrestad <eigilo@hig.no>
2015-05-12 13:44:22 +0200

    Added ssh public key to eigil's user.

--- manifests/baseconfig.pp
+++ manifests/baseconfig.pp
@@ -17,6 +17,12 @@ class profile::baseconfig {
 		mode  => 444,
 		source => "puppet:///modules/profile/hosts",
 	} ->  
+
+	ssh_authorized_key { "eigil@carajillo":
+		user => "eigil",
+		type => 'ssh-rsa',
+		key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDwe4N6Op3OEDYxe/SeHr58jgq7/Ip7uSDLYuOtJl40/IHVWyCwMfQwFWgIzNM+8Obpu9uRvwp85hF7PHoM5MTNgcPGhlJeFUkHbiUu3fhlj4k+YmiW9NpotNbVbTw0s3m2PLVruEvVm8fQ376XTJO2jTOfx7DC25dV+UqAe7XtmZra6l2wEZPr2UN5W7TGr3Th19dkBiQBQMxIGFd/toKOniFdq/+JBp6OH+ZMQ8QIgKkAQ/AKCxYROkxYvWblvOtjk0kehaJkn3b6wjgyMf80yxLvOd4jaX7LO/G8kXjXQ+1gwVImvG0Cw4yG5D20Y+UspwahXeRV5Ik72qsF3ezX",
+	} ->
 	
 	class { '::ntp':
 		servers => [ '0.no.pool.ntp.org', 
