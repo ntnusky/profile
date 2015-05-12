f8ad365212657ea3702a7cdadecd6f29f3ce24ed
obrestad <eigilo@hig.no>
2015-05-12 19:09:37 +0200

    Added root pubkey

--- manifests/baseconfig.pp
+++ manifests/baseconfig.pp
@@ -32,6 +32,12 @@ class profile::baseconfig {
 		key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDwe4N6Op3OEDYxe/SeHr58jgq7/Ip7uSDLYuOtJl40/IHVWyCwMfQwFWgIzNM+8Obpu9uRvwp85hF7PHoM5MTNgcPGhlJeFUkHbiUu3fhlj4k+YmiW9NpotNbVbTw0s3m2PLVruEvVm8fQ376XTJO2jTOfx7DC25dV+UqAe7XtmZra6l2wEZPr2UN5W7TGr3Th19dkBiQBQMxIGFd/toKOniFdq/+JBp6OH+ZMQ8QIgKkAQ/AKCxYROkxYvWblvOtjk0kehaJkn3b6wjgyMf80yxLvOd4jaX7LO/G8kXjXQ+1gwVImvG0Cw4yG5D20Y+UspwahXeRV5Ik72qsF3ezX",
 	} ->
 	
+	ssh_authorized_key { "root@manager":
+		user => "root",
+		type => 'ssh-rsa',
+		key => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDkc0epI9xYbQgxheiv2w6es/pRnyA0BvUSA/dUvnxjt/CRdWT+z57xaUYVmlwdLTzmisPSxvUov24URpRZhkwLpSKICpsY/hbHii0OE01fWTJrAThAD9SDzyhj6BnB8cLsA8M+tcvqdB9aqOfUfqr7aUwvjDDFzD57Wj7p9TWXl7U0pMHKa5tshNhZNdEVlUwLCU3PM7ucQxvRkNeKiKkul9nMEkw659JGlFQWbeD9mhPGjM8tNLuyBGb3K+RrtBRf5XZfuW8Xu05dB9hqmSjxGLrQuu50SRQdde1n6yh0B9+XvP7QQOXkmVTWspMbnJDnMwbycp9NLS4WCV5Rl3rN root@manager"
+	} ->
+	
 	class { '::ntp':
 		servers => [ '0.no.pool.ntp.org', 
 			'1.no.pool.ntp.org',  
