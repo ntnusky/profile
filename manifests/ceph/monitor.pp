7556614d4c5fead254030f889c5a1066ce42a160
obrestad <eigilo@hig.no>
2015-05-14 16:37:05 +0200

    Ordering in ceph

--- manifests/ceph/monitor.pp
+++ manifests/ceph/monitor.pp
@@ -15,24 +15,32 @@ class profile::ceph::monitor {
     mon_initial_members => $controllernames,
     mon_host            => $controlleraddresses,
     osd_pool_default_size => $replicas,
-  }->
+    before              => Anchor['profile::ceph::monitor::end'],
+    require             => Anchor['profile::ceph::monitor::begin'],
+  }
   ceph::mon { $::hostname:
     key => $mon_key,
-  }->
+    before              => Anchor['profile::ceph::monitor::end'],
+    require             => Anchor['profile::ceph::monitor::begin'],
+  }
   Ceph::Key {
     inject         => true,
     inject_as_id   => 'mon.',
     inject_keyring => "/var/lib/ceph/mon/ceph-${::hostname}/keyring",
-  }->
+  }
   ceph::key { 'client.admin':
     secret  => $admin_key,
     cap_mon => 'allow *',
     cap_osd => 'allow *',
     cap_mds => 'allow',
-  }->
+    before              => Anchor['profile::ceph::monitor::end'],
+    require             => Anchor['profile::ceph::monitor::begin'],
+  }
   ceph::key { 'client.bootstrap-osd':
     secret  => $bootstrap_osd_key,
     cap_mon => 'allow profile bootstrap-osd',
-  }->
+    before              => Anchor['profile::ceph::monitor::end'],
+    require             => Anchor['profile::ceph::monitor::begin'],
+  }
   anchor { "profile::ceph::monitor::end" : }
 }
