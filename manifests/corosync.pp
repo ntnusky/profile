991c106bda90e56185662dcb77a11f04eae299d9
obrestad <eigilo@hig.no>
2015-05-21 19:27:02 +0200

    Fixed topscope error.

--- manifests/corosync.pp
+++ manifests/corosync.pp
@@ -1,11 +1,11 @@
 class profile::corosync {
-  class { 'corosync':
+  class { '::corosync':
     enable_secauth    => true,
     authkey           => '/var/lib/puppet/ssl/certs/ca.pem',
     bind_address      => $ipaddress_eth1,
     multicast_address => '239.1.1.2',
   }
-  corosync::service { 'pacemaker':
+  ::corosync::service { 'pacemaker':
     version => '0',
   }
 }
