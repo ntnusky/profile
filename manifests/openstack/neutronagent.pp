6602eacfac0698e3d2223478049d577d56ed612d
obrestad <eigilo@hig.no>
2015-06-01 11:52:59 +0200

    Initial commit of neutron agent.

---
+++ manifests/openstack/neutronagent.pp
@@ -0,0 +1,59 @@
+class profile::openstack::neutronagent {
+  $password = hiera("profile::mysql::neutronpass")
+  $keystone_ip = hiera("profile::api::keystone::public::ip")
+  $mysql_ip = hiera("profile::mysql::ip")
+
+  $region = hiera("profile::region")
+  $admin_ip = hiera("profile::api::neutron::admin::ip")
+  $public_ip = hiera("profile::api::neutron::public::ip")
+  $service_plugins = hiera("profile::neutron::service_plugins")
+  
+  $rabbit_hosts = hiera("controller::management::addresses")
+  $rabbit_user = hiera("profile::rabbitmq::rabbituser")
+  $rabbit_pass = hiera("profile::rabbitmq::rabbitpass")
+
+  $database_connection = "mysql://neutron:${password}@${mysql_ip}/neutron"
+  
+  include ::profile::openstack::repo
+  
+  anchor{ "profile::openstack::neutronagent::begin" : }
+  anchor{ "profile::openstack::neutronagent::end" : }
+
+  class { '::neutron::agents::ml2::ovs':
+    enable_tunneling => true,
+    local_ip         => $::ipaddress_eth3,
+    enabled          => true,
+    tunnel_types     => ['gre'],
+  }
+
+  class { '::neutron':
+    core_plugin           => 'ml2',
+    allow_overlapping_ips => true,
+    service_plugins       => $service_plugins,
+    before                => Anchor["profile::openstack::neutronagent::end"],
+    require               => Anchor["profile::openstack::neutronagent::begin"],
+    rabbit_password       => $rabbit_pass,
+    rabbit_user           => $rabbit_user,
+    rabbit_hosts          => $rabbit_hosts,
+  }
+  
+  class { '::neutron::keystone::auth':
+    password         => $password,
+    public_address   => $public_ip,
+    admin_address    => $admin_ip,
+    internal_address => $admin_ip,
+    before           => Anchor["profile::openstack::neutronagent::end"],
+    require          => Anchor["profile::openstack::neutronagent::begin"],
+    region           => $region,
+  }
+  
+  class { '::neutron::server':
+    enabled           => false,
+    sync_db           => false,
+    auth_password     => $password,
+    auth_uri          => "http://${keystone_ip}:5000/",
+    connection        => $database_connection,
+    before            => Anchor["profile::openstack::neutron::end"],
+    require           => Anchor["profile::openstack::neutron::begin"],
+  }
+}
