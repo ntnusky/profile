4b4bec3c8a6c602530e5e6c61a9aba195ce14485
Eigil Obrestad <eigil-github@obrestad.org>
2015-06-12 13:54:19 +0200

    Removed ssh keys, as they are not generated yet.

--- manifests/users/eigil.pp
+++ manifests/users/eigil.pp
@@ -19,10 +19,10 @@ class profile::users::eigil {
     require  => User['eigil'],
   }
   
-  ssh_authorized_key { "eigil@manager":
-    user => "eigil",
-    type => 'ssh-rsa',
-    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCu6593qsTa1Z+dLR0VUjjh4rVEhTMBe8jz99ioc216GWet/7zwMvwoyrjxdyGWdq6F3tGFWuk9xzVkmSs8a+rg/FRCwZFbSC16xGOALqFtkygBCNORgPjeRch+ID5D64z7Yk6mBZHs4itABk29KTEcKuvFtWbEGdvUjbL01/GjgQiyhFg3F3SXeflnCmXOS05AtQ85c4/tUYJN59IX1VTEg6ZUKDGJrJRpJJiAg59TCrLAMGLQfm7lvRYpki2+AgiQn5ntJEiA5+A/XRKI8GqbKVZ6A3UVe55MqJqoXJUW7yNgPBE6Iwri4CbwJUN2My8H7v6N1733nD/W+7EG/PrN',
-    require => File['/home/eigil/.ssh'], 
-  }
+#  ssh_authorized_key { "eigil@manager":
+#    user => "eigil",
+#    type => 'ssh-rsa',
+#    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCu6593qsTa1Z+dLR0VUjjh4rVEhTMBe8jz99ioc216GWet/7zwMvwoyrjxdyGWdq6F3tGFWuk9xzVkmSs8a+rg/FRCwZFbSC16xGOALqFtkygBCNORgPjeRch+ID5D64z7Yk6mBZHs4itABk29KTEcKuvFtWbEGdvUjbL01/GjgQiyhFg3F3SXeflnCmXOS05AtQ85c4/tUYJN59IX1VTEg6ZUKDGJrJRpJJiAg59TCrLAMGLQfm7lvRYpki2+AgiQn5ntJEiA5+A/XRKI8GqbKVZ6A3UVe55MqJqoXJUW7yNgPBE6Iwri4CbwJUN2My8H7v6N1733nD/W+7EG/PrN',
+#    require => File['/home/eigil/.ssh'], 
+#  }
 }
--- manifests/users/erikh.pp
+++ manifests/users/erikh.pp
@@ -19,10 +19,10 @@ class profile::users::erikh {
     require  => User['erikh'],
   }
   
-  ssh_authorized_key { "erikh@manager":
-    user => "erikh",
-    type => 'ssh-rsa',
-    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCeuK+xxL/Y6EQDVv7HHrrrMygk85LfzwLxZxY5LR+xzUlwYjdkpFmoN18t4k1ziDUHRjTmp9pdQTVmYaB82l8bQqzU21nuOo7UrUUnue9/hXXI+If2WgFSxhmBsajmRm+jhzWKkUo8aVddAyhBhPMl0C+3GY3g5N0Hf59meXLNVXAhSgXvbINn66/NczUsMFulPAebckhf77I2Kv0ENPZndnW97nPKN7pb5Ng48mRNTo+zKJKi0v9JPZu8CaHZcPZgk3aBEFUpbOSfYYesUtd8ynIsV2mJkhmufKBU/Q9RneEBLG6CEghq30MQxTeKrg++jLQbd5UXrBYy1kMtHr8x',
-    require => File['/home/erikh/.ssh'], 
-  }
+#  ssh_authorized_key { "erikh@manager":
+#    user => "erikh",
+#    type => 'ssh-rsa',
+#    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCeuK+xxL/Y6EQDVv7HHrrrMygk85LfzwLxZxY5LR+xzUlwYjdkpFmoN18t4k1ziDUHRjTmp9pdQTVmYaB82l8bQqzU21nuOo7UrUUnue9/hXXI+If2WgFSxhmBsajmRm+jhzWKkUo8aVddAyhBhPMl0C+3GY3g5N0Hf59meXLNVXAhSgXvbINn66/NczUsMFulPAebckhf77I2Kv0ENPZndnW97nPKN7pb5Ng48mRNTo+zKJKi0v9JPZu8CaHZcPZgk3aBEFUpbOSfYYesUtd8ynIsV2mJkhmufKBU/Q9RneEBLG6CEghq30MQxTeKrg++jLQbd5UXrBYy1kMtHr8x',
+#    require => File['/home/erikh/.ssh'], 
+#  }
 }
