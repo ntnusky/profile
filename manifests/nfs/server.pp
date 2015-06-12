73d4c6b4b480caa47fb77feb40ac39f4026f13d4
Eigil Obrestad <eigil-github@obrestad.org>
2015-06-12 19:14:13 +0200

    Create the exports file.

--- manifests/nfs/server.pp
+++ manifests/nfs/server.pp
@@ -1,6 +1,12 @@
 class profile::nfs::server {
   include nfs::server
+
+  file{"/etc/exports":
+    ensure => present,
+  }
+
   nfs::server::export{ '/data/shared':
+    require => File['/etc/exports'],
     ensure  => 'mounted',
     clients => '172.17.1.0/24(rw,insecure,async,no_root_squash) localhost(rw)'
   }
