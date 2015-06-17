87d87c387629441c48da5573340b0521b3b185b2
Eigil Obrestad <eigil-github@obrestad.org>
2015-06-17 18:14:11 +0200

    Added ordering and final anchor.

--- manifests/ceph/monitor.pp
+++ manifests/ceph/monitor.pp
@@ -9,32 +9,33 @@ class profile::ceph::monitor {
   $replicas =  hiera("profile::ceph::replicas", undef)
   $journal_size =  hiera("profile::ceph::journal::size", 10000)
   
-  class { 'ceph::repo': }
+  class { 'ceph::repo': }->
   class { 'ceph':
     fsid  => $fsid,
     mon_initial_members => $controllernames,
     mon_host            => $controlleraddresses,
     osd_pool_default_size => $replicas,
-  }
+  }->
   ceph_config {
     'global/osd_journal_size': value => $journal_size;
-  }
+  }->
   ceph::mon { $::hostname:
     key => $mon_key,
-  }
+  }->
   Ceph::Key {
     inject         => true,
     inject_as_id   => 'mon.',
     inject_keyring => "/var/lib/ceph/mon/ceph-${::hostname}/keyring",
-  }
+  }->
   ceph::key { 'client.admin':
     secret  => $admin_key,
     cap_mon => 'allow *',
     cap_osd => 'allow *',
     cap_mds => 'allow',
-  }
+  }->
   ceph::key { 'client.bootstrap-osd':
     secret  => $bootstrap_osd_key,
     cap_mon => 'allow profile bootstrap-osd',
-  }
+  }->
+  anchor{'profile::ceph::monitor::end':}
 }
