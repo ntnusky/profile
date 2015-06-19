10c0624e9059778f3426bdc62ea53e1ef117e336
erikh <erikh@hig.no>
2015-06-19 12:26:31 +0200

    Fix: change reverse proxy setup

--- manifests/monitoring/reverseproxy.pp
+++ manifests/monitoring/reverseproxy.pp
@@ -4,21 +4,22 @@ class profile::monitoring::reverseproxy {
   include nginx
 
   file { '/etc/nginx/ssl':
-    ensure => directory
+    ensure => directory,
   } ->
   file { '/etc/nginx/ssl/nginx.key':
-    source        => 'puppet:///modules/profile/keys/private/nginx.key'
+    ensure => file,
+    source => 'puppet:///modules/profile/keys/private/nginx.key',
   } ->
   file { '/etc/nginx/ssl/nginx.crt':
-    source       => 'puppet:///modules/profile/keys/certs/nginx.crt'
+    ensure => file,
+    source => 'puppet:///modules/profile/keys/certs/nginx.crt',
   } ->
-
   nginx::resource::vhost { '172.17.1.12':
     listen_port    => 8081,
     ssl_port       => 8081,
     ssl            => true,
-    ssl_key        => '/etc/nginx/ssl/nginx.key'
-    ssl_cert       => '/etc/nginx/ssl/nginx.crt'
+    ssl_key        => '/etc/nginx/ssl/nginx.key',
+    ssl_cert       => '/etc/nginx/ssl/nginx.crt',
     proxy_redirect => 'http://localhost:5601',
   }
 
