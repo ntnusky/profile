9be6c29c816367091b3118a0445adf7afde94cc1
erikh <erikh@hig.no>
2015-06-19 13:00:23 +0200

    Fix attempt...

--- manifests/monitoring/reverseproxy.pp
+++ manifests/monitoring/reverseproxy.pp
@@ -14,7 +14,7 @@ class profile::monitoring::reverseproxy {
     ensure => file,
     source => 'puppet:///modules/profile/keys/certs/nginx.crt',
   } ->
-  nginx::resource::vhost { '172.17.1.12':
+  nginx::resource::vhost { 'monitor.skyhigh':
     use_default_location => false,
     listen_port          => 8081,
     ssl_port             => 8081,
@@ -24,7 +24,7 @@ class profile::monitoring::reverseproxy {
 #    proxy               => 'http://localhost:5601',
   }
   nginx::resource::location { 'kibana' :
-    vhost               => '172.17.1.12',
+    vhost               => 'monitor.skyhigh',
     location            => '/',
     proxy               => 'http://localhost:5601',
   }
