28cc020882e9f9a8942817546397c4bd875e1feb
erikh <erikh@hig.no>
2015-06-19 06:41:51 +0200

    Update: change reverse proxy setup

--- manifests/monitoring/reverseproxy.pp
+++ manifests/monitoring/reverseproxy.pp
@@ -3,10 +3,11 @@ class profile::monitoring::reverseproxy {
 
   include nginx
 
-  nginx::resource::upstream { 'kibanaproxy':
-    members => [
-      'localhost:5601'
-    ],
+  nginx::resource::vhost { '172.17.1.12':
+    listen_port    => 8081,
+    ssl_port       => 8081,
+    ssl            => true,
+    proxy_redirect => 'http://localhost:5601',
   }
 
 }
