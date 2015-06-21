22ec099c29e5c9268d0d4d1e4217905ef1e9267f
erikh <erikh@hig.no>
2015-06-21 09:23:40 +0200

    Fix.

--- manifests/monitoring/logstashforwarder.pp
+++ manifests/monitoring/logstashforwarder.pp
@@ -4,10 +4,10 @@ class profile::monitoring::logstashforwarder {
 # BLOCK OF SSL KEYS TO BE REQUIRED BY logstashforwarder
 
   class { '::logstashforwarder':
-    servers     => [ 'monitor.skyhigh' ],
-#    ssl_key     => 'puppet:///modules/profile/keys/private/logstash-forwarder.key',
+    servers     => [ 'monitor.skyhigh:9300' ],
+    ssl_key     => 'puppet:///modules/profile/keys/private/logstash-forwarder.key',
     ssl_ca      => 'puppet:///modules/profile/keys/certs/selfsigned.crt',
-#    ssl_cert    => 'puppet:///modules/profile/keys/certs/logstash-forwarder.crt',
+    ssl_cert    => 'puppet:///modules/profile/keys/certs/logstash-forwarder.crt',
     manage_repo => true,
     autoupgrade => true,
   }
