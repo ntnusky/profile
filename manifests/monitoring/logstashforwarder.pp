0a29cc25108ff807a73750a347f0657d9d46106b
erikh <erikh@hig.no>
2015-06-21 09:35:59 +0200

    Fix.

--- manifests/monitoring/logstashforwarder.pp
+++ manifests/monitoring/logstashforwarder.pp
@@ -5,9 +5,9 @@ class profile::monitoring::logstashforwarder {
 
   class { '::logstashforwarder':
     servers     => [ 'monitor.skyhigh:9300' ],
-#    ssl_key     => 'puppet:///modules/profile/keys/private/logstash-forwarder.key',
+    ssl_key     => 'puppet:///modules/profile/keys/private/logstash-forwarder.key',
     ssl_ca      => 'puppet:///modules/profile/keys/certs/selfsigned.crt',
-#    ssl_cert    => 'puppet:///modules/profile/keys/certs/logstash-forwarder.crt',
+    ssl_cert    => 'puppet:///modules/profile/keys/certs/logstash-forwarder.crt',
     manage_repo => true,
     autoupgrade => true,
   }
