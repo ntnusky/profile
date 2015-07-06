6c2e97653b6cdcc248725dd89785e66a264e31ea
erikh <erikh@hig.no>
2015-07-06 12:57:19 +0200

    Fix: fiddling with the logstash and concat module.

--- manifests/monitoring/elk.pp
+++ manifests/monitoring/elk.pp
@@ -41,10 +41,16 @@ class profile::monitoring::elk {
 #    install_contrib  => true, # DOES NOT WORK, contribs are being renamed 
 #                              # with version 1.5, check this out later
   }
-  logstash::configfile { 'logstash-logs.conf':
+  file { '/etc/logstash/conf.d/logstash.conf':
+    ensure => file,
     source => 'puppet:///modules/profile/logstash-logs.conf',
-    order => 10,
   }
+# The following generates an error /Stage[main]/Logstash::Config/File_concat[ls-config]: 
+# Failed to generate additional resources using 'eval_generate': undefined method `join' 
+# for "puppet:///modules/profile/logstash-logs.conf" so we copy manually for now, see above
+#  logstash::configfile { 'logstash-logs.conf':
+#    source => 'puppet:///modules/profile/logstash-logs.conf',
+#  }
 
 # K
 
