38d7e77d52cbe46684037570f2e31cc8e34aee97
erikh <erikh@hig.no>
2015-07-06 19:07:11 +0200

    Update: added more logs incl ceph.

--- manifests/monitoring/logstashforwarder.pp
+++ manifests/monitoring/logstashforwarder.pp
@@ -24,7 +24,33 @@ class profile::monitoring::logstashforwarder {
     paths  => [ '/var/log/neutron/*.log' ],
     fields => { 'type' => 'openstack', 'component' => 'neutron' },
   }
-
-
+  logstashforwarder::file { 'cinder':
+    paths  => [ '/var/log/cinder/*.log' ],
+    fields => { 'type' => 'openstack', 'component' => 'cinder' },
+  }
+  logstashforwarder::file { 'heat':
+    paths  => [ '/var/log/heat/*.log' ],
+    fields => { 'type' => 'openstack', 'component' => 'heat' },
+  }
+  logstashforwarder::file { 'keystone':
+    paths  => [ '/var/log/keystone/*.log' ],
+    fields => { 'type' => 'openstack', 'component' => 'keystone' },
+  }
+  logstashforwarder::file { 'glance':
+    paths  => [ '/var/log/glance/*.log' ],
+    fields => { 'type' => 'openstack', 'component' => 'glance' },
+  }
+  logstashforwarder::file { 'ceilometer':
+    paths  => [ '/var/log/ceilometer/*.log' ],
+    fields => { 'type' => 'openstack', 'component' => 'ceilometer' },
+  }
+  logstashforwarder::file { 'libvirt':
+    paths  => [ '/var/log/libvirt/*.log' ],
+    fields => { 'type' => 'libvirt' },
+  }
+  logstashforwarder::file { 'ceph':
+    paths  => [ '/var/log/ceph/*.log' ],
+    fields => { 'type' => 'ceph' },
+  }
 
 }
