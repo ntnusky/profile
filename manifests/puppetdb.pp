848d09718c35bccbc7bd7331c6469ccf6d39a6d3
erikh <erikh@hig.no>
2015-07-16 22:38:51 +0200

    Fix puppetdb.

--- manifests/puppetdb.pp
+++ manifests/puppetdb.pp
@@ -3,6 +3,7 @@ class profile::puppetdb {
   class { '::puppetdb': }
   class { '::puppetdb::master::config':
     terminus_package => 'puppetdb-terminus',
+    confdir          => '/etc/puppetdb/conf.d',
   }
 
 }
