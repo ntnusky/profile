a2729823f2f3d430b1c06c927b335d6a7e6bd71f
erikh <erikh@hig.no>
2015-07-19 06:39:53 +0200

    Fix: set puppetdb version to 2.3.6.

--- manifests/puppetdb.pp
+++ manifests/puppetdb.pp
@@ -3,6 +3,9 @@ class profile::puppetdb {
   class { '::java':
     distribution => 'jre',
   } ->
+  class { '::puppetdb::globals':
+    version => '2.3.6-1puppetlabs1',
+  }
   class { '::puppetdb':
     confdir          => '/etc/puppetdb/conf.d',
     postgres_version => '9.3',
