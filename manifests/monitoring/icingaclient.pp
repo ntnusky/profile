6c3a8ff5186321eee9edeb09f5f41ae210516e87
erikh <erikh@hig.no>
2015-07-19 06:51:53 +0200

    Fix: icingaclient.

--- manifests/monitoring/icingaclient.pp
+++ manifests/monitoring/icingaclient.pp
@@ -1,15 +1,15 @@
 class profile::monitoring::icingaclient {
 
   @@icinga2::object::host { $::fqdn:
-    display_name => $::fqdn,
-    ipv4_address => $::ipaddress,
-    groups => ['linux_servers'],
-    vars => {
-      os              => 'linux',
-      distro          => $::operatingsystem,
-    },
-    target_dir => '/etc/icinga2/objects/hosts',
-    target_file_name => "${fqdn}.conf"
+    display_name     => $::fqdn,
+    ipv4_address     => $::ipaddress_em1, # should be fqdn reverse lookup instead
+    groups           => ['linux_servers'],
+    vars             => {
+                         os     => 'linux',
+                         distro => $::operatingsystem,
+                        },
+    target_dir       => '/etc/icinga2/objects/hosts',
+    target_file_name => "${fqdn}.conf",
   }
 
 }
