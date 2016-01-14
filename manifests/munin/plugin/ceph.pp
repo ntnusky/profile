06c055177acc4e1cd6cbc11d052b625d6378166a
Eigil Obrestad <eigil-github@obrestad.org>
2016-01-14 22:42:02 +0100

    Ceph module permissions

--- manifests/munin/plugin/ceph.pp
+++ manifests/munin/plugin/ceph.pp
@@ -2,9 +2,11 @@ class profile::munin::plugin::ceph {
   munin::plugin { 'ceph_capacity':
     ensure => present,
     source => 'puppet:///modules/profile/muninplugins/ceph_capacity',
+	config => ['user root'],
   }
   munin::plugin { 'ceph_osd':
     ensure => present,
     source => 'puppet:///modules/profile/muninplugins/ceph_osd',
+	config => ['user root'],
   }
 }
