01b2e78d48677290f299a53f3f74bc326af2a0b2
Eigil Obrestad <eigil-github@obrestad.org>
2016-01-14 21:22:41 +0100

    Typo in router address

--- manifests/munin/snmp.pp
+++ manifests/munin/snmp.pp
@@ -1,66 +1,66 @@
 class profile::munin::snmp {
-  munin::plugin { 'snmp_gw.skyhgih_if_1':
+  munin::plugin { 'snmp_gw.skyhigh_if_1':
     ensure => link,
 	target => '/usr/share/munin/plugins/snmp__if_',
 	config => ['env.community skyhigh'],
   }
-  munin::plugin { 'snmp_gw.skyhgih_if_err_1':
+  munin::plugin { 'snmp_gw.skyhigh_if_err_1':
     ensure => link,
 	target => '/usr/share/munin/plugins/snmp__if_err_',
 	config => ['env.community skyhigh'],
   }
-  munin::plugin { 'snmp_gw.skyhgih_if_2':
+  munin::plugin { 'snmp_gw.skyhigh_if_2':
     ensure => link,
 	target => '/usr/share/munin/plugins/snmp__if_',
 	config => ['env.community skyhigh'],
   }
-  munin::plugin { 'snmp_gw.skyhgih_if_err_2':
+  munin::plugin { 'snmp_gw.skyhigh_if_err_2':
     ensure => link,
 	target => '/usr/share/munin/plugins/snmp__if_err_',
 	config => ['env.community skyhigh'],
   }
-  munin::plugin { 'snmp_gw.skyhgih_if_6':
+  munin::plugin { 'snmp_gw.skyhigh_if_6':
     ensure => link,
 	target => '/usr/share/munin/plugins/snmp__if_',
 	config => ['env.community skyhigh'],
   }
-  munin::plugin { 'snmp_gw.skyhgih_if_err_6':
+  munin::plugin { 'snmp_gw.skyhigh_if_err_6':
     ensure => link,
 	target => '/usr/share/munin/plugins/snmp__if_err_',
 	config => ['env.community skyhigh'],
   }
-  munin::plugin { 'snmp_gw.skyhgih_if_7':
+  munin::plugin { 'snmp_gw.skyhigh_if_7':
     ensure => link,
 	target => '/usr/share/munin/plugins/snmp__if_',
 	config => ['env.community skyhigh'],
   }
-  munin::plugin { 'snmp_gw.skyhgih_if_err_7':
+  munin::plugin { 'snmp_gw.skyhigh_if_err_7':
     ensure => link,
 	target => '/usr/share/munin/plugins/snmp__if_err_',
 	config => ['env.community skyhigh'],
   }
-  munin::plugin { 'snmp_gw.skyhgih_if_8':
+  munin::plugin { 'snmp_gw.skyhigh_if_8':
     ensure => link,
 	target => '/usr/share/munin/plugins/snmp__if_',
 	config => ['env.community skyhigh'],
   }
-  munin::plugin { 'snmp_gw.skyhgih_if_err_8':
+  munin::plugin { 'snmp_gw.skyhigh_if_err_8':
     ensure => link,
 	target => '/usr/share/munin/plugins/snmp__if_err_',
 	config => ['env.community skyhigh'],
   }
 
-  munin::plugin { 'snmp_gw.skyhgih_if_multi':
+  munin::plugin { 'snmp_gw.skyhigh_if_multi':
     ensure => link,
 	target => '/usr/share/munin/plugins/snmp__if_multi',
 	config => ['env.community skyhigh'],
   }
-  munin::plugin { 'snmp_gw.skyhgih_if_netstat':
+  munin::plugin { 'snmp_gw.skyhigh_if_netstat':
     ensure => link,
 	target => '/usr/share/munin/plugins/snmp__netstat',
 	config => ['env.community skyhigh'],
   }
-  munin::plugin { 'snmp_gw.skyhgih_if_uptime':
+  munin::plugin { 'snmp_gw.skyhigh_if_uptime':
     ensure => link,
 	target => '/usr/share/munin/plugins/snmp__uptime',
 	config => ['env.community skyhigh'],
