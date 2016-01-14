d0efb358d9005327a01fb78e4b9b1ad4f6a074e1
Eigil Obrestad <eigil-github@obrestad.org>
2016-01-14 20:53:25 +0100

    Step 2 of router monitoring

--- manifests/munin/snmp.pp
+++ manifests/munin/snmp.pp
@@ -57,12 +57,17 @@ class profile::munin::snmp {
   }
   munin::plugin { 'snmp_172.17.1.1_if_netstat':
     ensure => link,
-	target => '/usr/share/munin/plugins/snmp__if_netstat',
+	target => '/usr/share/munin/plugins/snmp__netstat',
 	config => ['env.community skyhigh'],
   }
   munin::plugin { 'snmp_172.17.1.1_if_uptime':
     ensure => link,
-	target => '/usr/share/munin/plugins/snmp__if_uptime',
+	target => '/usr/share/munin/plugins/snmp__uptime',
 	config => ['env.community skyhigh'],
+  }
+
+  munin::master::node_definition { '172.17.1.1':
+    address => '172.17.1.12',
+    config  => ['use_node_name no'],
   }
 }
