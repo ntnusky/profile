5d6124d50abf125e40e5c831d16d35f30726de0a
Eigil Obrestad <eigil-github@obrestad.org>
2016-01-14 20:45:02 +0100

    Initial config for monitoring of the router

---
+++ manifests/munin/snmp.pp
@@ -0,0 +1,68 @@
+class profile::munin::snmp {
+  munin::plugin { 'snmp_172.17.1.1_if_1':
+    ensure => link,
+	target => '/usr/share/munin/plugins/snmp__if_',
+	config => ['env.community skyhigh'],
+  }
+  munin::plugin { 'snmp_172.17.1.1_if_err_1':
+    ensure => link,
+	target => '/usr/share/munin/plugins/snmp__if_err_',
+	config => ['env.community skyhigh'],
+  }
+  munin::plugin { 'snmp_172.17.1.1_if_2':
+    ensure => link,
+	target => '/usr/share/munin/plugins/snmp__if_',
+	config => ['env.community skyhigh'],
+  }
+  munin::plugin { 'snmp_172.17.1.1_if_err_2':
+    ensure => link,
+	target => '/usr/share/munin/plugins/snmp__if_err_',
+	config => ['env.community skyhigh'],
+  }
+  munin::plugin { 'snmp_172.17.1.1_if_6':
+    ensure => link,
+	target => '/usr/share/munin/plugins/snmp__if_',
+	config => ['env.community skyhigh'],
+  }
+  munin::plugin { 'snmp_172.17.1.1_if_err_6':
+    ensure => link,
+	target => '/usr/share/munin/plugins/snmp__if_err_',
+	config => ['env.community skyhigh'],
+  }
+  munin::plugin { 'snmp_172.17.1.1_if_7':
+    ensure => link,
+	target => '/usr/share/munin/plugins/snmp__if_',
+	config => ['env.community skyhigh'],
+  }
+  munin::plugin { 'snmp_172.17.1.1_if_err_7':
+    ensure => link,
+	target => '/usr/share/munin/plugins/snmp__if_err_',
+	config => ['env.community skyhigh'],
+  }
+  munin::plugin { 'snmp_172.17.1.1_if_8':
+    ensure => link,
+	target => '/usr/share/munin/plugins/snmp__if_',
+	config => ['env.community skyhigh'],
+  }
+  munin::plugin { 'snmp_172.17.1.1_if_err_8':
+    ensure => link,
+	target => '/usr/share/munin/plugins/snmp__if_err_',
+	config => ['env.community skyhigh'],
+  }
+
+  munin::plugin { 'snmp_172.17.1.1_if_multi':
+    ensure => link,
+	target => '/usr/share/munin/plugins/snmp__if_multi',
+	config => ['env.community skyhigh'],
+  }
+  munin::plugin { 'snmp_172.17.1.1_if_netstat':
+    ensure => link,
+	target => '/usr/share/munin/plugins/snmp__if_netstat',
+	config => ['env.community skyhigh'],
+  }
+  munin::plugin { 'snmp_172.17.1.1_if_uptime':
+    ensure => link,
+	target => '/usr/share/munin/plugins/snmp__if_uptime',
+	config => ['env.community skyhigh'],
+  }
+}
