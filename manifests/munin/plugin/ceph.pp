70095bd12732d9d5360102a3826340a842e2c6aa
Eigil Obrestad <eigil-github@obrestad.org>
2016-02-03 23:14:31 +0100

    Distribute the ceph collector scripts

--- manifests/munin/plugin/ceph.pp
+++ manifests/munin/plugin/ceph.pp
@@ -9,4 +9,20 @@ class profile::munin::plugin::ceph {
     source => 'puppet:///modules/profile/muninplugins/ceph_osd',
 	config => ['user root'],
   }
+
+  # Install the collector scripts for the ceph plugins
+  file { '/usr/local/sbin/ceph-collector.sh':
+    ensure => file,
+	mode => 755,
+	owner => root,
+	group => root,
+    source => 'puppet:///modules/profile/muninplugins/ceph-collector.sh',
+  }
+  file { '/usr/local/sbin/ceph-collect.sh':
+    ensure => file,
+	mode => 755,
+	owner => root,
+	group => root,
+    source => 'puppet:///modules/profile/muninplugins/ceph-collect.sh',
+  }
 }
