fa69589ff36e9723f070e1900168f0aa8b0fa2be
Eigil Obrestad <eigil-github@obrestad.org>
2016-02-04 13:32:25 +0100

    Added munin plugins to monitor nova usage

---
+++ manifests/munin/plugin/nova.pp
@@ -0,0 +1,37 @@
+class profile::munin::plugin::nova {
+  $admin_ip = hiera("profile::api::keystone::admin::ip")
+  $admin_token = hiera("profile::keystone::admin_token")
+  $admin_pass = hiera("profile::keystone::admin_password")
+  $admin_url = "http://${admin_ip}:5000/v2.0/",
+  
+
+  munin::plugin { 'nova_cpu':
+    ensure => present,
+    source => 'puppet:///modules/profile/muninplugins/nova_cpu',
+	config => [
+	  'env.OS_TENANT_NAME admin',
+	  'env.OS_USERNAME $admin_token',
+	  'env.OS_PASSWORD $admin_pass',
+	  'env.OS_AUTH_URL $admin_url'],
+  }
+
+  munin::plugin { 'nova_ram':
+    ensure => present,
+    source => 'puppet:///modules/profile/muninplugins/nova_cpu',
+	config => [
+	  'env.OS_TENANT_NAME admin',
+	  'env.OS_USERNAME $admin_token',
+	  'env.OS_PASSWORD $admin_pass',
+	  'env.OS_AUTH_URL $admin_url'],
+  }
+
+  munin::plugin { 'nova_disk':
+    ensure => present,
+    source => 'puppet:///modules/profile/muninplugins/nova_cpu',
+	config => [
+	  'env.OS_TENANT_NAME admin',
+	  'env.OS_USERNAME $admin_token',
+	  'env.OS_PASSWORD $admin_pass',
+	  'env.OS_AUTH_URL $admin_url'],
+  }
+}
