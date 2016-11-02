2e185b4db33eb957145ad9af80d49855dfa9cf48
LarsErikP <pedersen.larserik@gmail.com>
2016-11-02 09:06:54 +0100

    So... The sql user must be created before you can grant him privileges.. Seems about right.

--- manifests/mysql/accessvm.pp
+++ manifests/mysql/accessvm.pp
@@ -25,12 +25,16 @@ class profile::mysql::accessvm {
     user => 'root',
   } ->
 
+  mysql_user { "'${accessuser}'@'172.16.%.%'":
+    ensure        => present,
+    password_hash => mysql_password($accesspw),
+  } ->
+
   mysql_grant { "${accessuser}@172.16.%.%/keystone.v_project_roles_per_user":
     ensure     => present,
     options    => ['GRANT'],
     privileges => ['SELECT'],
     table      => 'keystone.v_project_roles_per_user',
     user       => "${accessuser}@172.16.%.%",
-    password   => $accesspw,
   }
 }
