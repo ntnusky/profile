d1cf496dced785b78b2c1ad8856711ac0df46370
LarsErikP <pedersen.larserik@gmail.com>
2016-11-02 09:35:06 +0100

    Oh the typos, the never ending typos

--- manifests/mysql/accessvm.pp
+++ manifests/mysql/accessvm.pp
@@ -5,11 +5,11 @@
 class profile::mysql::accessvm {
   $host = hiera('profile::mysql::ip')
   $pw = hiera('profile::mysqlcluster::root_password')
-  $sql = 'CREATE OR REPLACE VIEW v_project_roles_per_user AS \
+  $sql = "CREATE OR REPLACE VIEW v_project_roles_per_user AS \
           SELECT u.local_id AS username, count(p.name) AS project_roles \
           FROM project as p INNER JOIN assignment as a ON a.target_ID = p.id \
           INNER JOIN id_mapping u ON u.public_id = a.actor_id \
-          GROUP BY username;'
+          GROUP BY username;"
   $accessuser = hiera('profile::access::db_user')
   $accesspw = hiera('profile::access::db_password')
 
@@ -17,7 +17,7 @@ class profile::mysql::accessvm {
                                   -u root \
                                   -p ${pw} \
                                   -D keystone \
-                                  -e ${sql} 2> /dev/null"
+                                  -e \"${sql}\" 2> /dev/null"
 
   Anchor['profile::openstack::keystone::end'] ->
 
