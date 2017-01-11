b82ecc1019dec17d36dfa8bd8b98995ba5a5d303
Eigil Obrestad <eigil-git@obrestad.org>
2017-01-11 16:52:08 +0100

    Make sure we are configuring puppet, even on 16.04

--- manifests/baseconfig.pp
+++ manifests/baseconfig.pp
@@ -61,40 +61,52 @@ class profile::baseconfig {
     ],
   }
 
+  # If we are running on ubuntu 14.04, add the puppet repos to get puppet 3.8.
   if ($::lsbdistcodename == 'trusty') {
     apt::source { 'puppetlabs':
       location   => 'http://apt.puppetlabs.com',
       repos      => 'main',
       key        => '1054B7A24BD6EC30',
       key_server => 'pgp.mit.edu',
-    } ->
-    package { 'puppet':
-      ensure => '3.8.7-1puppetlabs1',
-    } ->
-    ini_setting { 'Puppet Start':
-      ensure  => present,
-      path    => '/etc/default/puppet',
-      section => '',
-      setting => 'START',
-      value   => 'yes',
-    } ->
-    # This environment is not the one really used, as that is decided by
-    # foreman's ENC, but puppet gets really sad if this setting points to a
-    # non-existent environment. This setting might also get useful in the cases
-    # where an ENC  is not used.
-    ini_setting { 'Puppet environment':
-      ensure  => present,
-      path    => '/etc/puppet/puppet.conf',
-      section => 'agent',
-      setting => 'environment',
-      value   => $environment,
-      notify  => Service['puppet'],
-    } ->
-    service { 'puppet':
-      ensure => 'running',
+      before     => Package['puppet'],
     }
   }
+  
+  package { 'puppet':
+    ensure => 'present',
+  }
 
+  ini_setting { 'Puppet Start':
+    ensure  => present,
+    path    => '/etc/default/puppet',
+    section => '',
+    setting => 'START',
+    value   => 'yes',
+    require => Package['puppet'],
+  }
+
+  # This environment is not the one really used, as that is decided by
+  # foreman's ENC, but puppet gets really sad if this setting points to a
+  # non-existent environment. This setting might also get useful in the cases
+  # where an ENC  is not used.
+  ini_setting { 'Puppet environment':
+    ensure  => present,
+    path    => '/etc/puppet/puppet.conf',
+    section => 'agent',
+    setting => 'environment',
+    value   => $environment,
+    notify  => Service['puppet'],
+    require => Package['puppet'],
+  }
+
+  service { 'puppet':
+    ensure  => 'running',
+    require => Package['puppet'],
+  }
+
+  # If the puppet-version is new enough to support ecda ssh-keys (which is
+  # default in openssh these days), configure ssh. If puppet is too old; it
+  # should be updated so that ssh gets configured... :)
   if ($::puppetversion > '3.5.0') {
     class {'::ssh':
       server_options => {
@@ -113,22 +125,17 @@ class profile::baseconfig {
     }
 
     exec {'shosts.equiv':
-      command => 'cat /etc/ssh/ssh_known_hosts | grep -v "^#" | awk \'{print $1}\' | sed -e \'s/,/\n/g\' > /etc/ssh/shosts.equiv',
+      command => 'cat /etc/ssh/ssh_known_hosts | grep -v "^#" | \
+                  awk \'{print $1}\' | sed -e \'s/,/\n/g\' > \
+                  /etc/ssh/shosts.equiv',
       path    => '/bin:/usr/bin',
       require => Class['ssh::knownhosts'],
     }
   }
 
+  # Configure interfaces as instructed in hiera.
   $interfacesToConfigure = hiera('profile::interfaces', false)
   if($interfacesToConfigure) {
     setDHCP { $interfacesToConfigure: }
   }
-
-#  mount{'/fill':
-#    ensure => absent,
-#  } ->
-#  logical_volume { 'fill':
-#    ensure       => absent,
-#    volume_group => 'hdd',
-#  }
 }
