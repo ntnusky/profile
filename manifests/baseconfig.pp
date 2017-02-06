4e03ec912904401a4091df5edaa7a5e0bd5f0c15
Eigil Obrestad <eigil-git@obrestad.org>
2017-02-06 16:51:59 +0100

    Prevent the hosts from losing their gateway

--- manifests/baseconfig.pp
+++ manifests/baseconfig.pp
@@ -5,4 +5,12 @@ class profile::baseconfig {
   include ::profile::baseconfig::packages
   include ::profile::baseconfig::puppet
   include ::profile::baseconfig::ssh
+
+  # This is a properly ugly way to ensure that the hosts dont lose their
+  # arp-entry for the gateway.
+  cron { 'gwrefresh':
+    command => '/bin/ping vg.no -c 1 &> /dev/null',
+    user    => root,
+    minute    => '*',
+  }
 }
