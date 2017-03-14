de465d89aa8b65b46c7b625973c217c516b14086
LarsErikP <pedersen.larserik@gmail.com>
2017-03-14 10:17:48 +0100

    Uhm, wrong packages, apparently..

--- manifests/baseconfig/packages.pp
+++ manifests/baseconfig/packages.pp
@@ -22,7 +22,7 @@ class profile::baseconfig::packages {
   # Install ruby2.3 on Ubuntu 14.04
   if ($::lsbdistcodename == 'trusty') {
     apt::ppa { 'ppa:brightbox/ruby-ng':
-      require => Package['python-software-properties'],
+      require => Package['software-properties-common'],
       #  before  => Package['ruby2.3'],
     }
     #package { 'ruby2.3':
