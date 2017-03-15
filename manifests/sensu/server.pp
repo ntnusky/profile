e36d678c1c02af8bf3f22ca839e74c493495b9d5
LarsErikP <pedersen.larserik@gmail.com>
2017-03-15 09:21:26 +0100

    Add mailer handler to sensu-server

--- manifests/sensu/server.pp
+++ manifests/sensu/server.pp
@@ -5,6 +5,12 @@ class profile::sensu::server {
   $rabbithost = hiera('profile::rabbitmq::ip')
   $sensurabbitpass = hiera('profile::sensu::rabbit_password')
   $mgmt_nic = hiera('profile::interfaces::management')
+  $sensu_url = hiera('profile::sensu::mailer::url')
+  $mail_from = hiera('profile::sensu::mailer::mail_from')
+  $mail_to = hiera('profile::sensu::mailer::mail_to')
+  $smtp_address = hiera('profile::sensu::mailer::smtp_address')
+  $smtp_port = hiera('profile::sensu::mailer::smtp_port')
+  $smtp_domain = hiera('profile::sensu::mailer::smtp_domain')
 
   class { '::sensu':
     rabbitmq_host         => $rabbithost,
@@ -14,6 +20,30 @@ class profile::sensu::server {
     use_embedded_ruby     => true,
     api_bind              => '127.0.0.1',
     sensu_plugin_provider => 'sensu_gem',
+  }
+
+  sensu::handler { 'default':
+    type     => 'set',
+    handlers => [ 'stdout', 'mailer' ],
+  }
+
+  sensu::handler { 'stdout':
+    type    => 'pipe',
+    command => 'cat',
+  }
+
+  sensu::handler { 'mailer':
+    type    => 'pipe',
+    command => 'handler-mailer.rb',
+    source  => 'puppet:///modules/profile/sensu/handlers/handler-mailer.rb',
+    config  => {
+      admin_gui    => $sensu_url,
+      mail_from    => $mail_from,
+      mail_to      => $mail_to,
+      smtp_address => $smtp_address,
+      smtp_port    => $smtp_port,
+      smtp_domain  => $smtp_domain,
+    },
   }
 
   # Her skal uchiwa
