8947c1989b37f8f39bf55a919051134d44153889
LarsErikP <pedersen.larserik@gmail.com>
2017-03-16 10:52:11 +0100

    Checks are not standalone

--- manifests/sensu/checks.pp
+++ manifests/sensu/checks.pp
@@ -2,11 +2,13 @@
 class profile::sensu::checks {
   sensu::check { 'diskspace':
     command     => 'check-disk-usage.rb -w :::disk.warning|80::: -c :::disk.critical|90::: -I :::disk.mountpoints|all:::',
+    standalone  => false,
     subscribers => [ 'all' ],
   }
 
   sensu::check { 'load':
     command     => 'check-load.rb -w :::load.warning|1,5,10::: -c :::load.critical|10,15,25:::',
+    standalone  => false,
     subscribers => [ 'all'],
   }
 }
