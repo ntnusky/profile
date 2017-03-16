660d1742198be26903458f34404f6ddc66f6616c
LarsErikP <pedersen.larserik@gmail.com>
2017-03-16 09:01:51 +0100

    For once, it was broken semantics, not syntax

--- manifests/sensu/checks.pp
+++ manifests/sensu/checks.pp
@@ -1,11 +1,8 @@
 # Sensu check definitions
 class profile::sensu::checks {
   sensu::check { 'diskspace':
-    command     =>
-      'check-disk-usage.rb \
-      -w :::disk.warning|80::: \
-      -c :::disk.critical|90::: \
-      -I :::disk.mountpoints|all:::',
+    command     => 'check-disk-usage.rb -w :::disk.warning|80::: -c :::disk.critical|90::: -I :::disk.mountpoints|all:::',
+    standalone  => false,
     subscribers => [ 'all' ],
   }
 }
