b95640ea31dd22bc40dfe6ad6d51517e58c30215
LarsErikP <pedersen.larserik@gmail.com>
2017-03-16 10:46:42 +0100

    Added load check. Possibly with insane defaults

--- manifests/sensu/checks.pp
+++ manifests/sensu/checks.pp
@@ -2,7 +2,11 @@
 class profile::sensu::checks {
   sensu::check { 'diskspace':
     command     => 'check-disk-usage.rb -w :::disk.warning|80::: -c :::disk.critical|90::: -I :::disk.mountpoints|all:::',
-    standalone  => false,
     subscribers => [ 'all' ],
+  }
+
+  sensu::check { 'load':
+    command     => 'check-load.rb -w :::load.warning|1,5,10::: -c :::load.critical|10,15,25:::',
+    subscribers => [ 'all'],
   }
 }
