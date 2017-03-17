0568d17b8da15c69e1ed8121d2568d2c05fd15ae
LarsErikP <pedersen.larserik@gmail.com>
2017-03-17 11:59:34 +0100

    Allow the sensu user to run checks with sudo

--- manifests/baseconfig/sudo.pp
+++ manifests/baseconfig/sudo.pp
@@ -1,9 +1,14 @@
 # This class starts to configure sudo
 class profile::baseconfig::sudo {
   class { '::sudo': }
-  
+
   sudo::conf { 'insults':
     priority => 10,
     content  => 'Defaults	insults',
+  }
+
+  sudo::conf { 'sensu-client':
+    priority => 15,
+    source   => 'puppet:///modules/profile/sudo/sensu_sudoers',
   }
 }
