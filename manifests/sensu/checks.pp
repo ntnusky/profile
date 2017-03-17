081ed62fee7df087920132af42810e2e297841bb
LarsErikP <pedersen.larserik@gmail.com>
2017-03-17 13:20:10 +0100

    MySQL-statuc check. Might expose password in uchiwa... We'll see ;-)

--- manifests/sensu/checks.pp
+++ manifests/sensu/checks.pp
@@ -1,5 +1,8 @@
 # Sensu check definitions
 class profile::sensu::checks {
+
+  $mysqlpw = hiera('profile::mysqlcluster::status_password')
+
   sensu::check { 'diskspace':
     command     => 'check-disk-usage.rb -w :::disk.warning|80::: -c :::disk.critical|90::: -I :::disk.mountpoints|all:::',
     standalone  => false,
@@ -28,5 +31,11 @@ class profile::sensu::checks {
     command     => 'sudo check-ceph.rb -d',
     standalone  => false,
     subscribers => [ 'roundrobin:ceph' ],
+  }
+
+  sensu::check { 'mysql-status':
+    command     => "check-mysql-status.rb -h localhost -d mysql -u clustercheck -p ${mysqlpw} --check status",
+    standadlone => false,
+    subscribers => [ 'mysql' ],
   }
 }
