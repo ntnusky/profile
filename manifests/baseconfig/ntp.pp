819adc021ea8ebbcdfa715836a773453a52ead6f
Eigil Obrestad <eigil-git@obrestad.org>
2017-05-22 15:30:54 +0200

    Set timezone

--- manifests/baseconfig/ntp.pp
+++ manifests/baseconfig/ntp.pp
@@ -1,11 +1,18 @@
 # This class installs and configures NTP.
 class profile::baseconfig::ntp {
   $ntpServer = hiera('profile::ntp::server')
+  $tz = hiera('profile::ntp::timezone', 'Europe/Oslo')
+
   class { '::ntp':
     servers  => [ $ntpServer ],
     restrict => [
       'default kod nomodify notrap nopeer noquery',
       '-6 default kod nomodify notrap nopeer noquery',
     ],
+  }
+
+  file {'/etc/localtime':
+    ensure => link,
+    target => "/usr/share/zoneinfo/${tz}"
   }
 }
