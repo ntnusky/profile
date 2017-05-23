ae2bed3524c3dd456de1a2d2f008d2a7b628159f
LarsErikP <pedersen.larserik@gmail.com>
2017-05-23 14:41:01 +0200

    Add the ability to configure gateway on interfaces

--- manifests/baseconfig/configureinterface.pp
+++ manifests/baseconfig/configureinterface.pp
@@ -4,10 +4,12 @@ define profile::baseconfig::configureinterface {
   $method = hiera("profile::interfaces::${name}::method")
   $address = hiera("profile::interfaces::${name}::address", false)
   $netmask = hiera("profile::interfaces::${name}::netmask", '255.255.255.0')
+  $gateway = hiera("profile::interfaces::${name}::gateway", false)
 
   network::interface{ $name:
     method  => $method,
     address => $address,
     netmask => $netmask,
+    gateway => $gateway,
   }
 }
