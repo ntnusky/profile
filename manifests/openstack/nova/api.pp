cbbff6e2952e719e43baa5a5446071f946b72687
LarsErikP <pedersen.larserik@gmail.com>
2017-08-08 08:05:05 +0200

    nova-api must be enabled for metadata service to function i think)

--- manifests/openstack/nova/api.pp
+++ manifests/openstack/nova/api.pp
@@ -42,7 +42,6 @@ class profile::openstack::nova::api {
     neutron_metadata_proxy_shared_secret => $nova_secret,
     sync_db                              => $sync_db,
     sync_db_api                          => $sync_db,
-    enabled                              => false,
     service_name                         => 'httpd',
   }
 
