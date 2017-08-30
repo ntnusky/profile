7d0198df7300274fb5dee41b9ec2c18aa7dbfd6f
Eigil Obrestad <eigil-git@obrestad.org>
2017-08-30 14:44:12 +0200

    Add a tftp client

--- manifests/services/tftp.pp
+++ manifests/services/tftp.pp
@@ -1,8 +1,14 @@
 # This class installs a tftpserver and configures it to have its root at
 # /var/lib/tftpboot.
 class profile::services::tftp {
+  # Install and configure the tftp server.
   class { '::tftp':
     directory => '/var/lib/tftpboot/',
     options   => '--secure',
+  }
+  
+  # A tftp client is handy for testing.
+  package { 'tftp-hpa':
+    ensure => 'present',
   }
 }
