23edcc20c7e3d6cc563c7a42bc25bdc3185fc4a6
Eigil Obrestad <eigil-git@obrestad.org>
2017-08-30 15:30:18 +0200

    Create DHCP pools based on hieradata

--- manifests/services/dhcp.pp
+++ manifests/services/dhcp.pp
@@ -3,10 +3,13 @@ class profile::services::dhcp {
   $searchdomain = hiera('profile::dhcp::searchdomain')
   $dns_servers = hiera_array('profile::dns::servers')
   $interfaces = hiera_array('profile::interfaces')
+  $networks = hiera_array('profile::networks')
 
-  class { 'dhcp':
+  class { '::dhcp':
     dnssearchdomains => [$searchdomain],
     nameservers      => $dns_servers,
     interfaces       => $interfaces,
   }
+
+  profile::services::dhcp::pool { $networks: }
 }
---
+++ manifests/services/dhcp/pool.pp
@@ -0,0 +1,14 @@
+# Creates DHCP pool based on data from hiera
+define profile::services::dhcp::pool {
+  $id = hiera("profile::networks::${name}::id")
+  $mask = hiera("profile::networks::${name}::mask")
+  $gateway = hiera("profile::networks::${name}::gateway")
+  $range = hiera("profile::networks::${name}::range")
+
+  ::dhcp::pool { "${name}.${domain}":
+    network => $id,
+    mask    => $mask,
+    range   => $range,
+    gateway => $gateway,
+  }
+}
