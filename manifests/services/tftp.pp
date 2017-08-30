a8cfa6fbaa251e1925de0d863031709745d0aa08
Eigil Obrestad <eigil-git@obrestad.org>
2017-08-30 14:22:02 +0200

    Typo

--- manifests/services/tftp.pp
+++ manifests/services/tftp.pp
@@ -1,6 +1,6 @@
 # This class installs a tftpserver and configures it to have its root at
 # /var/lib/tftpboot.
-class profile::serices::tftpserver {
+class profile::services::tftp {
   class { '::tftp':
     directory => '/var/lib/tftpboot/',
     inetd     => true,
