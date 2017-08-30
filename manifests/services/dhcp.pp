b5fdd0cc194001f111c31a2b013d2198d3057697
Eigil Obrestad <eigil-git@obrestad.org>
2017-08-30 15:12:23 +0200

    List interfaces for DHCP server

--- manifests/services/dhcp.pp
+++ manifests/services/dhcp.pp
@@ -1,10 +1,12 @@
 # Installs and configures a DHCP server.
 class profile::services::dhcp {
   $searchdomain = hiera('profile::dhcp::searchdomain')
-  $dns_servers = hiera('profile::dns::servers')
+  $dns_servers = hiera_array('profile::dns::servers')
+  $interfaces = hiera_array('profile::interfaces')
 
   class { 'dhcp':
     dnssearchdomains => [$searchdomain],
     nameservers      => $dns_servers,
+    interfaces       => $interfaces,
   }
 }
