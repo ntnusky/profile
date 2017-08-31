6ee0607efc0674ff1f02ead0dc6d1aae2948cca3
Eigil Obrestad <eigil-git@obrestad.org>
2017-08-31 09:26:59 +0200

    enable OMAPI for the dhcp server

--- manifests/services/dhcp.pp
+++ manifests/services/dhcp.pp
@@ -5,10 +5,17 @@ class profile::services::dhcp {
   $interfaces = hiera_array('profile::interfaces')
   $networks = hiera_array('profile::networks')
 
+  $omapi_name = hiera('profile::dhcp::omapi::name')
+  $omapi_key = hiera('profile::dhcp::omapi::key')
+  $omapi_port = hiera('profile::dhcp::omapi::port', 7911)
+
   class { '::dhcp':
     dnssearchdomains => [$searchdomain],
     nameservers      => $dns_servers,
     interfaces       => $interfaces,
+    omapi_key        => $omapi_key,
+    omapi_name       => $omapi_name,
+    omapi_port       => $omapi_port,
   }
 
   profile::services::dhcp::pool { $networks: }
