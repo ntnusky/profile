99f0afbae10e1742d7523ee515347ee920e0f292
Eigil Obrestad <eigil-git@obrestad.org>
2017-08-31 09:36:46 +0200

    Configure DHCP for pxe booting

--- manifests/services/dhcp.pp
+++ manifests/services/dhcp.pp
@@ -9,13 +9,19 @@ class profile::services::dhcp {
   $omapi_key = hiera('profile::dhcp::omapi::key')
   $omapi_port = hiera('profile::dhcp::omapi::port', 7911)
 
+  $pxe_server = hiera('profile::dhcp::pxe::server')
+  $pxe_file = hiera('profile::dhcp::pxe::file', 'pxelinux.0')
+
   class { '::dhcp':
     dnssearchdomains => [$searchdomain],
-    nameservers      => $dns_servers,
     interfaces       => $interfaces,
+    nameservers      => $dns_servers,
+    ntpservers       => $ntp_servers,
     omapi_key        => $omapi_key,
     omapi_name       => $omapi_name,
     omapi_port       => $omapi_port,
+    pxeserver        => $pxe_server,
+    pxefilename      => $pxe_file,
   }
 
   profile::services::dhcp::pool { $networks: }
