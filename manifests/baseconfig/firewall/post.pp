4c47810e517da2b6e00a71431cc967019cc35cf6
Eigil Obrestad <eigil-git@obrestad.org>
2017-09-04 12:56:13 +0200

    Let the final firewall action be controlled by hieradata.

--- manifests/baseconfig/firewall/post.pp
+++ manifests/baseconfig/firewall/post.pp
@@ -1,8 +1,10 @@
 # This class installs and configures firewall post.
 class profile::baseconfig::firewall::post {
+  $action = hiera('profile::firewall::action::final', 'drop')
+
   firewall { '999 drop all':
     proto  => 'all',
-    action => 'accept',
+    action => $action,
     before => undef,
   }
 }
