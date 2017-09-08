26f03b25e2253d5076120df120318189905d7c96
Eigil Obrestad <eigil-git@obrestad.org>
2017-09-08 10:53:41 +0200

    Using the new networking module to configure networking

--- manifests/baseconfig/configureinterface.pp
+++ manifests/baseconfig/configureinterface.pp
@@ -4,18 +4,24 @@ define profile::baseconfig::configureinterface {
   $method = hiera("profile::interfaces::${name}::method")
   $address = hiera("profile::interfaces::${name}::address", false)
   $netmask = hiera("profile::interfaces::${name}::netmask", '255.255.255.0')
-  $gateway = hiera("profile::interfaces::${name}::gateway", false)
+  $gateway = hiera("profile::interfaces::${name}::gateway", undef)
+  $secondary_routing = hiera("profile::interfaces::${name}::secondaryrouting",
+      false)
 
   if($method == 'dhcp') {
-    network::interface{ $name:
-      method => $method,
+    network::interface { $name:
+      enable_dhcp => true,
     }
   } else {
     network::interface{ $name:
-      method  => $method,
-      address => $address,
-      netmask => $netmask,
-      gateway => $gateway,
+      ipaddress => $address,
+      netmask   => $netmask,
+      gateway   => $gateway,
     }
+  }
+
+  # If a separate routing-table is required for this interface:
+  if($secondary_routing) {
+
   }
 }
