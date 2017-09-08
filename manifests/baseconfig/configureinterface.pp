e63ca2d71b5d0a28ab58c52f2515f62298cccaf8
Eigil Obrestad <eigil-git@obrestad.org>
2017-09-08 12:15:26 +0200

    Allow separate routing tables for extra interfaces

--- manifests/baseconfig/configureinterface.pp
+++ manifests/baseconfig/configureinterface.pp
@@ -5,8 +5,7 @@ define profile::baseconfig::configureinterface {
   $address = hiera("profile::interfaces::${name}::address", false)
   $netmask = hiera("profile::interfaces::${name}::netmask", '255.255.255.0')
   $gateway = hiera("profile::interfaces::${name}::gateway", undef)
-  $secondary_routing = hiera("profile::interfaces::${name}::secondaryrouting",
-      false)
+  $secondary_routing = hiera("profile::interfaces::${name}::newtable", false)
 
   if($method == 'dhcp') {
     network::interface { $name:
@@ -22,6 +21,21 @@ define profile::baseconfig::configureinterface {
 
   # If a separate routing-table is required for this interface:
   if($secondary_routing) {
+    $table_id = hiera("profile::interfaces::${name}::newtable::id")
+    $netid = ip_network("${address}/${netmask}")
+    network::routing_table { "table-${name}":
+      table_id => $table_id,
+    }
 
+    network::rule { $name:
+      iprule => ["from ${netid} lookup table-${name}", ],
+    }
+
+    network::route { $name:
+      ipaddress => [ '0.0.0.0', $netid, ],
+      netmask   => [ '0.0.0.0', $netmask, ],
+      gateway   => [ $gateway, false, ],
+      table     => [ "table-${name}", "table-${name}",],
+    }
   }
 }
