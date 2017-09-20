9b0cd2b84d36a8407264b98b937a88ba292bc731
Eigil Obrestad <eigil-git@obrestad.org>
2017-09-20 15:39:27 +0200

    Add domainname to ns-name

--- manifests/services/dns/zone.pp
+++ manifests/services/dns/zone.pp
@@ -1,10 +1,15 @@
 # Creates DNS zone
 define profile::services::dns::zone {
-  $dns_servers = hiera_array('profile::dns::servers::names')
+  $dns_servers = hiera_array('profile::dns::servers::fqdn')
+  $dns_zones = hiera_array('profile::dns::zones')
   $dns_updaters = hiera_array('profile::dns::updaters', [])
 
+  $servers = $dns_servers.map |$server| {
+    "${server}.${dns_zones[0]}"
+  }
+
   ::dns::zone { $name :
-    nameservers  => $dns_servers,
+    nameservers  => $servers,
     allow_update => concat($dns_updaters, '127.0.0.1'),
     data_dir     => '/var/lib/bind/zones',
     require      => File['/var/lib/bind/zones'],
