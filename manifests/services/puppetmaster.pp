25eb3f514d59800585c58a44a43aaa458fa0b3f1
Eigil Obrestad <eigil-git@obrestad.org>
2017-09-21 09:39:36 +0200

    Use the same installation of the dashboard for everything

--- manifests/services/puppetmaster.pp
+++ manifests/services/puppetmaster.pp
@@ -1,13 +1,11 @@
 # Installs and configures a puppetmaster 
 class profile::services::puppetmaster {
-  # Install the puppetmaster-integration for the dashboards.
-  include ::profile::services::dashboard::install::onlycode
+  include ::profile::services::dashboard::install
 
   $cnf = '/etc/machineadmin/settings.ini'
 
-  $path = '/opt/machineadmin-code'
   cron { 'Dashboard-client puppet-environments':
-    command => "${path}/clients/puppetEnvironmentUpdater.py ${cnf}",
+    command => "/opt/machineadmin/clients/puppetEnvironmentUpdater.py ${cnf}",
     user    => 'root',
     minute  => '*',
   }
