922652a80ed663cc46814f8972eabe56840d1ddb
Eigil Obrestad <eigil-git@obrestad.org>
2017-09-25 12:04:03 +0200

    Configure password for postgres

--- manifests/services/postgresql/server.pp
+++ manifests/services/postgresql/server.pp
@@ -3,6 +3,7 @@ class profile::services::postgresql::server {
   $management_if = hiera('profile::interfaces::management')
   $management_ip = hiera("profile::interfaces::${management_if}::address")
   $database_port = hiera('profile::postgres::backend::port', 5433) 
+  $password = hiera('profile::postgres::password')
 
   class { '::postgresql::globals':
     manage_package_repo => true, 
@@ -13,6 +14,7 @@ class profile::services::postgresql::server {
     ip_mask_allow_all_users => '0.0.0.0/0',
     listen_addresses        => $management_ip,
     port                    => $databse_port,
+    postgres_password       => $password,
   }
 
   class { '::postgresql::server::contrib': }
