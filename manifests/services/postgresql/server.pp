819d78bf07b8a6621f799388c9eac7df70a1afb0
Eigil Obrestad <eigil-git@obrestad.org>
2017-09-25 14:51:41 +0200

    Allow postgresuser over network

--- manifests/services/postgresql/server.pp
+++ manifests/services/postgresql/server.pp
@@ -11,11 +11,12 @@ class profile::services::postgresql::server {
   }
 
   class { '::postgresql::server':
-    ip_mask_allow_all_users => '0.0.0.0/0',
-    listen_addresses        => $management_ip,
-    port                    => scanf($database_port, '%i')[0],
-    postgres_password       => $password,
-    manage_pg_ident_conf    => false,
+    ip_mask_deny_postgres_user => '0.0.0.0/32',
+    ip_mask_allow_all_users    => '0.0.0.0/0',
+    listen_addresses           => $management_ip,
+    port                       => scanf($database_port, '%i')[0],
+    postgres_password          => $password,
+    manage_pg_ident_conf       => false,
   }
 
   class { '::postgresql::server::contrib': }
