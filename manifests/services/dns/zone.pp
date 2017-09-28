232d66740442e81a94d62e3fcd81521199b9ecfc
Eigil Obrestad <eigil-git@obrestad.org>
2017-09-28 15:40:11 +0200

    Allow update using keys instead of ip-adresses

--- manifests/services/dns/zone.pp
+++ manifests/services/dns/zone.pp
@@ -2,7 +2,6 @@
 define profile::services::dns::zone {
   $dns_servers = hiera_array('profile::dns::servers::names')
   $dns_zones = hiera_array('profile::dns::zones')
-  $dns_updaters = hiera_array('profile::dns::updaters', [])
 
   $servers = $dns_servers.map |$server| {
     "${server}.${dns_zones[0]}"
@@ -10,7 +9,7 @@ define profile::services::dns::zone {
 
   ::dns::zone { $name :
     nameservers  => $servers,
-    allow_update => concat($dns_updaters, '127.0.0.1'),
+    allow_update => ['key update'],
     data_dir     => '/var/lib/bind/zones',
   }
 }
