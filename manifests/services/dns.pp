60cc2639b3ba2aef56ef1d9011e05b188297bd34
Eigil Obrestad <eigil-git@obrestad.org>
2017-09-28 15:29:19 +0200

    Create keys to secure DNS transfer and updates

--- manifests/services/dns.pp
+++ manifests/services/dns.pp
@@ -3,13 +3,24 @@ class profile::services::dns {
   $dns_forwarders = hiera('profile::dns::forwarders')
   $dns_zones = hiera_array('profile::dns::zones')
 
+  $update_key = hiera('profile::dns::key::update')
+  $transfer_key = hiera('profile::dns::key::transfer')
+
   include ::dns::server
   include ::profile::services::dashboard::clients::dns
 
-  dns::server::options{'/etc/bind/named.conf.options':
+  ::dns::server::options{'/etc/bind/named.conf.options':
     forwarders        => $dns_forwarders,
     dnssec_validation => 'no',
     dnssec_enable     => false,
+  }
+
+  ::dns::tsig { 'update':
+    secret => $update_key,
+  }
+
+  ::dns::tsig { 'transfer':
+    secret => $transfer_key,
   }
 
   ::profile::services::dns::zone { $dns_zones : }
