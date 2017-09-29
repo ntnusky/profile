27353b116815584c892ce176c54f401b17baf343
Eigil Obrestad <eigil-git@obrestad.org>
2017-09-29 12:39:29 +0200

    Grab IP for routing-table from interface if DHCP is used

--- manifests/baseconfig/configureinterface.pp
+++ manifests/baseconfig/configureinterface.pp
@@ -2,10 +2,16 @@
 # the interface according to these settings.
 define profile::baseconfig::configureinterface {
   $method = hiera("profile::interfaces::${name}::method")
-  $address = hiera("profile::interfaces::${name}::address", false)
-  $netmask = hiera("profile::interfaces::${name}::netmask", '255.255.255.0')
-  $gateway = hiera("profile::interfaces::${name}::gateway", undef)
   $secondary_routing = hiera("profile::interfaces::${name}::newtable", false)
+
+  if($method == 'dhcp') {
+    $address = $facts['networking']['interface'][$name]['ip']
+    $netmask = $facts['networking']['interface'][$name]['netmask']
+  } else {
+    $address = hiera("profile::interfaces::${name}::address", false)
+    $netmask = hiera("profile::interfaces::${name}::netmask", '255.255.255.0')
+    $gateway = hiera("profile::interfaces::${name}::gateway", undef)
+  }
 
   if($method == 'dhcp') {
     network::interface { $name:
@@ -20,22 +26,21 @@ define profile::baseconfig::configureinterface {
   }
 
   # If a separate routing-table is required for this interface:
-  if($secondary_routing) {
+  if($secondary_routing) and ($address != undef) {
     $table_id = hiera("profile::interfaces::${name}::newtable::id")
     $netid = ip_network("${address}/${netmask}")
+
     network::routing_table { "table-${name}":
       table_id => $table_id,
-    }
-
-    network::rule { $name:
-      iprule => ["from ${netid} lookup table-${name}", ],
-    }
-
+    }->
     network::route { $name:
       ipaddress => [ '0.0.0.0', $netid, ],
       netmask   => [ '0.0.0.0', $netmask, ],
       gateway   => [ $gateway, false, ],
       table     => [ "table-${name}", "table-${name}",],
+    }->
+    network::rule { $name:
+      iprule => ["from ${netid} lookup table-${name}", ],
     }
   }
 }
