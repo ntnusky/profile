77f8da1a4818908ce24f422260d0075fa6ecdd4f
Eigil Obrestad <eigil-git@obrestad.org>
2017-10-10 11:32:25 +0200

    Add keepalived as well

---
+++ manifests/services/puppetmaster/loadbalancer.pp
@@ -0,0 +1,5 @@
+# Configures the haproxy in frontend for the puppetmasters 
+class profile::services::puppetmaster::loadbalancer {
+  require ::profile::services::keepalived::haproxy::management
+  contain ::profile::services::puppetmaster::loadbalancer
+}
