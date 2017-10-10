6f8784a2192f9015d703db1b82114fd297fe0cee
Eigil Obrestad <eigil-git@obrestad.org>
2017-10-10 11:38:56 +0200

    Use correct classnames

--- manifests/services/puppetmaster/loadbalancer.pp
+++ manifests/services/puppetmaster/loadbalancer.pp
@@ -1,5 +1,5 @@
 # Configures the haproxy in frontend for the puppetmasters 
 class profile::services::puppetmaster::loadbalancer {
   require ::profile::services::keepalived::haproxy::management
-  contain ::profile::services::puppetmaster::loadbalancer
+  contain ::profile::services::puppetmaster::haproxy
 }
