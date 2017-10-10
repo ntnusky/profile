06da350418b187cd49f14c79659d54626b863cc4
Eigil Obrestad <eigil-git@obrestad.org>
2017-10-10 12:22:35 +0200

    Do not install haproxy on backend nodes

--- manifests/services/puppetmaster/haproxy/backend.pp
+++ manifests/services/puppetmaster/haproxy/backend.pp
@@ -1,7 +1,5 @@
 # Configures the haproxy backend for this puppetmaster 
 class profile::services::puppetmaster::haproxy::backend {
-  require ::profile::services::haproxy
-
   $if = hiera('profile::interfaces::management')
   $ip = $::facts['networking']['interfaces'][$if]['ip'] 
 
