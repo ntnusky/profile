c5caefc9b3ce8f31083252a90ddc464f9250b59c
Eigil Obrestad <eigil-git@obrestad.org>
2017-10-10 11:50:59 +0200

    Configure the puppet backend for haproxy

--- manifests/services/puppetmaster/haproxy/frontend.pp
+++ manifests/services/puppetmaster/haproxy/frontend.pp
@@ -1,5 +1,5 @@
 # Configures the haproxy in frontend for the puppetmasters 
-class profile::services::puppetmaster::haproxy {
+class profile::services::puppetmaster::haproxy::frontend {
   require ::profile::services::haproxy
 
   $ip = hiera('profile::haproxy::management::ip')
