a20b06a1312676a0729576cb03533e44cebfdefe
Eigil Obrestad <eigil-git@obrestad.org>
2017-10-10 14:33:34 +0200

    Remove unused variables

--- manifests/services/keepalived.pp
+++ manifests/services/keepalived.pp
@@ -1,15 +1,6 @@
 # This class installs the keepalived daemon and configures the system to allow
 # processes to bind to addresses not yet owned by the system.
 class profile::services::keepalived {
-  # Keepalived settings
-  $vrrp_password = hiera('profile::keepalived::vrrp_password')
-  $vrid = hiera('profile::mysql::vrrp::id')
-  $vrpri = hiera('profile::mysql::vrrp::priority')
-  
-  $management_if = hiera('profile::interfaces::management')
-  
-  $mysql_ip = hiera('profile::mysql::ip')
-  
   # Make sure to restart keepalived whenever its configuration changes.
   Concat['/etc/keepalived/keepalived.conf'] ~> Service['keepalived']
 
