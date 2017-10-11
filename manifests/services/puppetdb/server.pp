4723c08f03bb7c17fa980205ac8e555bb50bb470
Eigil Obrestad <eigil-git@obrestad.org>
2017-10-11 13:09:58 +0200

    Specify interface for puppetdb to listen on

--- manifests/services/puppetdb/server.pp
+++ manifests/services/puppetdb/server.pp
@@ -1,19 +1,21 @@
 # Installs and configures puppetdb 
 class profile::services::puppetdb::server {
-  contain ::profile::services::puppetdb::keepalived 
-
   $dbhost = hiera('profile::postgres::ip')
   $dbport = hiera('profile::postgres::port', '5432')
   $dbname = hiera('profile::puppetdb::database::name')
   $dbuser = hiera('profile::puppetdb::database::user')
   $dbpass = hiera('profile::puppetdb::database::pass')
 
+  $if = hiera('profile::interfaces::management')
+  $ip = $::facts['networking']['interfaces'][$if]['ip']
+
   class { 'puppetdb::server':
-    database          => 'postgres',
-    database_port     => $dbport,
-    database_host     => $dbhost,
-    database_username => $dbuser,
-    database_password => $dbpass,
-    database_name     => $dbname,
+    database           => 'postgres',
+    database_port      => $dbport,
+    database_host      => $dbhost,
+    database_username  => $dbuser,
+    database_password  => $dbpass,
+    database_name      => $dbname,
+    ssl_listen_address => $ip,
   }
 }
