3e31098417027468c6e5662055b432afbdf265b1
Eigil Obrestad <eigil-git@obrestad.org>
2017-10-12 15:01:11 +0200

    Remove the loadbalancer class as it is not used anymore

--- manifests/services/puppetmaster/loadbalancer.pp
+++
@@ -1,5 +0,0 @@
-# Configures the haproxy in frontend for the puppetmasters 
-class profile::services::puppetmaster::loadbalancer {
-  require ::profile::services::keepalived::haproxy::management
-  contain ::profile::services::puppetmaster::haproxy::frontend
-}
