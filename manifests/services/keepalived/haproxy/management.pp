bc9fe58602f39b73295b3d34c5c4bb5b4d83770c
Eigil Obrestad <eigil-git@obrestad.org>
2017-10-26 16:08:30 +0200

    Second attempt on IPv6 for the load-balancer

--- manifests/services/keepalived/haproxy/management.pp
+++ manifests/services/keepalived/haproxy/management.pp
@@ -2,10 +2,13 @@
 class profile::services::keepalived::haproxy::management {
   require ::profile::services::keepalived
 
-  $ip = hiera('profile::haproxy::management::ip')
-  $ipv6 = hiera('profile::haproxy::management::ipv6', false)
-  $vrrp_id = hiera('profile::haproxy::management::vrrp::id')
-  $vrrp_priority = hiera('profile::haproxy::management::vrrp:priority')
+  $v4_ip = hiera('profile::haproxy::management::ipv4')
+  $v4_id = hiera('profile::haproxy::management::ipv4::id')
+  $v4_priority = hiera('profile::haproxy::management::ipv4::priority')
+  $v6_ip = hiera('profile::haproxy::management::ipv6')
+  $v6_id = hiera('profile::haproxy::management::ipv6::id')
+  $v6_priority = hiera('profile::haproxy::management::ipv6::priority')
+
   $vrrp_password = hiera('profile::keepalived::vrrp_password')
   $management_if = hiera('profile::interfaces::management')
 
@@ -13,20 +16,25 @@ class profile::services::keepalived::haproxy::management {
     script => '/usr/bin/killall -0 haproxy',
   }
 
-  if($ipv6) {
-    $vip = ["${ip}/32", "${ipv6}/128"]
-  } else {
-    $vip = ["${ip}/32"]
-  }
-
-  keepalived::vrrp::instance { 'management-haproxy':
+  keepalived::vrrp::instance { 'management-haproxy-v4':
     interface         => $management_if,
     state             => 'BACKUP',
-    virtual_router_id => $vrrp_id,
-    priority          => $vrrp_priority,
+    virtual_router_id => $v4_id,
+    priority          => $v4_priority,
     auth_type         => 'PASS',
     auth_pass         => $vrrp_password,
-    virtual_ipaddress => $vip,
+    virtual_ipaddress => ["${v4_ip}/32"],
+    track_script      => 'check_haproxy',
+  }
+
+  keepalived::vrrp::instance { 'management-haproxy-v6':
+    interface         => $management_if,
+    state             => 'BACKUP',
+    virtual_router_id => $v6_id,
+    priority          => $v6_priority,
+    auth_type         => 'PASS',
+    auth_pass         => $vrrp_password,
+    virtual_ipaddress => ["${v6_ip}/64"],
     track_script      => 'check_haproxy',
   }
 }
