5ec9e8eb17a71b5580c1ad3e3bb2a272a8991f98
LarsErikP <pedersen.larserik@gmail.com>
2017-10-27 15:15:09 +0200

    Define libvirt networks with OVS

--- manifests/services/libvirt.pp
+++ manifests/services/libvirt.pp
@@ -1,35 +1,16 @@
 # Configure libvirt hosts
 class profile::services::libvirt {
-
-  #$mgmt_nic = hiera('profile::interfaces::management')
-
-  #  $networks = {
-  #  'mgmt-net' => {
-  #    autostart          => true,
-  #    forward_mode       => 'bridge',
-  #    forward_interfaces => [ $mgmt_nic, ],
-  #  }
-  #}
-
-  $networks = hiera('profile::libvirt::networks', {})
-
-  $net_defaults = {
-    'ensure'    => 'running',
-    'autostart' => true,
-  }
-
   contain ::profile::services::libvirt::pools
+  contain ::profile::services::libvirt::networks
 
   class { '::libvirt':
-    deb_default       => {
+    deb_default => {
       'libvirtd_opts' => '',
     },
-    mdns_adv          => false,
-    networks          => $networks,
-    networks_defaults => $net_defaults,
-  }
-
-  package { 'vlan':
-    ensure => present,
+    mdns_adv    => false,
+    before      => [
+      Class['::profile::services::libvirt::networks'],
+      Class['::profile::services::libvirt::pools'],
+    ],
   }
 }
---
+++ manifests/services/libvirt/networks.pp
@@ -0,0 +1,27 @@
+# Configure networks for libvirt
+class profile::services::libvirt::networks {
+  require ::vswitch::ovs
+
+  $networks = hiera('profile::networks', {})
+  $networks.each | $network |  {
+    $vlanid = hiera("profile::networks::${network}::vlanid")
+    $physical_if = hiera("profile::kvm::interfaces::${network}", false)
+    if ( $physical_if ) {
+      vs_bridge { "br-vlan-${network}":
+        ensure => present,
+      }
+      ::profile::infrastructure::ovs::patch {
+        physical_if => $physical_if,
+        vlan_id     => $vlanid,
+        ovs_bridge  => "br-vlan-${network}",
+        require     => Vs_bridge["br-vlan-${network}"],
+      }
+      libvirt::network { $network:
+        ensure             => 'running',
+        autostart          => true,
+        forward_mode       => 'bridge',
+        forward_interfaces => [ "br-vlan-${network}", ],
+      }
+    }
+  }
+}
