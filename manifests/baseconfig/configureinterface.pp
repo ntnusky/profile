1fbad3cbe1f927317a3bca8ab06afb0df6c5610b
LarsErikP <pedersen.larserik@gmail.com>
2017-10-27 14:28:01 +0200

    Make the creation of extra routing tables optional

--- manifests/baseconfig/configureinterface.pp
+++ manifests/baseconfig/configureinterface.pp
@@ -20,57 +20,59 @@ define profile::baseconfig::configureinterface {
     }
   }
 
-  $table_id = hiera("profile::interfaces::${name}::tableid")
-  if($facts['networking']['interfaces'][$name]['ip']) {
-    $net4id = $facts['networking']['interfaces'][$name]['network']
-    $net4mask = $facts['networking']['interfaces'][$name]['netmask']
-    $v4netids = ['0.0.0.0', $net4id]
-    $v4masks = ['0.0.0.0', $net4mask]
-    $v4gateways = [$v4gateway, false]
-    $v4tables = ["table-${name}", "table-${name}"]
-    $v4families = ['inet', 'inet']
-    $v4rules = ["from ${net4id}/${net4mask} lookup table-${name}"]
-    $v4rulef = ['inet']
-  } else {
-    $v4netids = []
-    $v4masks =  []
-    $v4gateways = []
-    $v4tables = []
-    $v4families = []
-    $v4rules = []
-    $v4rulef = []
-  }
-  if($facts['networking']['interfaces'][$name]['ip6']) {
-    $net6id = $facts['networking']['interfaces'][$name]['network6']
-    $v6netids = ['::', $net6id]
-    $v6masks = ['0', '64']
-    $v6gateways = [$v6gateway, false]
-    $v6tables = ["table-${name}", "table-${name}"]
-    $v6families = ['inet6', 'inet6']
-    $v6rules = ["from ${net6id}/64 lookup table-${name}"]
-    $v6rulef = ['inet6']
-  } else {
-    $v6netids = []
-    $v6masks =  []
-    $v6gateways = []
-    $v6tables = []
-    $v6families = []
-    $v6rules = []
-    $v6rulef = []
-  }
+  $table_id = hiera("profile::interfaces::${name}::tableid", false)
+  if ($table_id) {
+    if($::facts['networking']['interfaces'][$name]['ip']) {
+      $net4id = $::facts['networking']['interfaces'][$name]['network']
+      $net4mask = $::facts['networking']['interfaces'][$name]['netmask']
+      $v4netids = ['0.0.0.0', $net4id]
+      $v4masks = ['0.0.0.0', $net4mask]
+      $v4gateways = [$v4gateway, false]
+      $v4tables = ["table-${name}", "table-${name}"]
+      $v4families = ['inet', 'inet']
+      $v4rules = ["from ${net4id}/${net4mask} lookup table-${name}"]
+      $v4rulef = ['inet']
+    } else {
+      $v4netids = []
+      $v4masks =  []
+      $v4gateways = []
+      $v4tables = []
+      $v4families = []
+      $v4rules = []
+      $v4rulef = []
+    }
+    if($::facts['networking']['interfaces'][$name]['ip6']) {
+      $net6id = $::facts['networking']['interfaces'][$name]['network6']
+      $v6netids = ['::', $net6id]
+      $v6masks = ['0', '64']
+      $v6gateways = [$v6gateway, false]
+      $v6tables = ["table-${name}", "table-${name}"]
+      $v6families = ['inet6', 'inet6']
+      $v6rules = ["from ${net6id}/64 lookup table-${name}"]
+      $v6rulef = ['inet6']
+    } else {
+      $v6netids = []
+      $v6masks =  []
+      $v6gateways = []
+      $v6tables = []
+      $v6families = []
+      $v6rules = []
+      $v6rulef = []
+    }
 
-  network::routing_table { "table-${name}":
-    table_id => $table_id,
-  }->
-  network::route { $name:
-    ipaddress => concat($v4netids, $v6netids),
-    netmask   => concat($v4masks, $v6masks),
-    gateway   => concat($v4gateways, $v6gateways),
-    table     => concat($v4tables, $v6tables),
-    family   => concat($v4families, $v6families),
-  }->
-  profile::baseconfig::networkrule { $name:
-    iprule => concat($v4rules, $v6rules),
-    family => concat($v4rulef, $v6rulef),
+    network::routing_table { "table-${name}":
+      table_id => $table_id,
+    }->
+    network::route { $name:
+      ipaddress => concat($v4netids, $v6netids),
+      netmask   => concat($v4masks, $v6masks),
+      gateway   => concat($v4gateways, $v6gateways),
+      table     => concat($v4tables, $v6tables),
+      family    => concat($v4families, $v6families),
+    }->
+    profile::baseconfig::networkrule { $name:
+      iprule => concat($v4rules, $v6rules),
+      family => concat($v4rulef, $v6rulef),
+    }
   }
 }
