925cc86e2f1566f9058f08fdde327a3ded930fa6
LarsErikP <pedersen.larserik@gmail.com>
2017-10-30 09:35:23 +0100

    Leif var innom igjen. Hyggelig type.

--- manifests/services/libvirt/networks.pp
+++ manifests/services/libvirt/networks.pp
@@ -23,7 +23,7 @@ class profile::services::libvirt::networks {
         forward_mode       => 'bridge',
         forward_interfaces => [ "br-${network}", ],
       }
-      sysctl::value { "net.ipv6-conf.br-${network}.autoconf":
+      sysctl::value { "net.ipv6.conf.br-${network}.autoconf":
         value   => '0',
         require => Vs_bridge["br-${network}"],
       }
