9fb2a7c01c251f598a96bd85de216c5ddc3fe39d
Eigil Obrestad <eigil-git@obrestad.org>
2017-10-31 13:18:01 +0100

    Revert "Do not configure a default vhost"
    This reverts commit d9418a6706b861187eadb87ca11ff630ccca0e51.

--- manifests/services/apache.pp
+++ manifests/services/apache.pp
@@ -1,12 +1,6 @@
 # This class installs and configures a simple apache webserver, and configures a
 # vhost for the fqdn of the host
 class profile::services::apache {
-  include ::apache::mod::rewrite
-  include ::apache::mod::prefork
-  include ::apache::mod::php
-  include ::apache::mod::ssl
-  include ::profile::services::apache::firewall
-
   class { '::apache':
     mpm_module => 'prefork',
     confd_dir  => '/etc/apache2/conf-enabled'
@@ -15,4 +9,21 @@ class profile::services::apache {
   package { 'libcgi-pm-perl':
     ensure => present,
   }
+
+
+  apache::vhost { "${::fqdn} http":
+    servername    => $::fqdn,
+    port          => '80',
+    docroot       => "/var/www/${::fqdn}",
+    docroot_owner => 'www-data',
+    docroot_group => 'www-data',
+  }
+
+
+  include ::apache::mod::rewrite
+  include ::apache::mod::prefork
+  include ::apache::mod::php
+  include ::apache::mod::ssl
+  include ::profile::services::apache::firewall
+
 }
