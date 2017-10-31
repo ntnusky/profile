900df03d3b0e14165e4eac8293a50f5b48344f50
Eigil Obrestad <eigil-git@obrestad.org>
2017-10-31 12:56:37 +0100

    Add default-parameter to grant.

--- manifests/services/dashboard/mysql.pp
+++ manifests/services/dashboard/mysql.pp
@@ -3,7 +3,7 @@ class profile::services::dashboard::mysql {
   $configfile = hiera('profile::dashboard::configfile',
       '/etc/machineadmin/settings.ini')
   $database_name = hiera('profile::dashboard::database::name')
-  $database_grant = hiera('profile::dashboard::database::grant')
+  $database_grant = hiera('profile::dashboard::database::grant', '%')
   $database_user = hiera('profile::dashboard::database::user')
   $database_pass = hiera('profile::dashboard::database::pass')
 
