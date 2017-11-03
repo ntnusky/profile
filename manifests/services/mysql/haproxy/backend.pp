b2ed6c558467a243cf6cccaa6b0c6a600f2820c1
LarsErikP <pedersen.larserik@gmail.com>
2017-11-03 10:50:50 +0100

    Split mysql haproxy config into frontend and backend

--- manifests/services/mysql/haproxy/backend.pp
+++ manifests/services/mysql/haproxy/backend.pp
@@ -1,10 +1,10 @@
-# Configures the haproxy backend for this mysql cluster member 
+# Configures the haproxy backend for this mysql cluster member
 class profile::services::mysql::haproxy::backend {
   $if = hiera('profile::interfaces::management')
   $ip = $::facts['networking']['interfaces'][$if]['ip']
 
   @@haproxy::balancermember { "mysql-${::fqdn}":
-    listening_service => 'mysqlcluster',
+    listening_service => 'bk_mysqlcluster',
     server_names      => $::hostname,
     ipaddresses       => $ip,
     ports             => '3306',
--- manifests/services/mysql/haproxy/frontend.pp
+++ manifests/services/mysql/haproxy/frontend.pp
@@ -6,19 +6,35 @@ class profile::services::mysql::haproxy::frontend {
   $ipv4 = hiera('profile::haproxy::management::ip')
   $ipv6 = hiera('profile::haproxy::management::ipv6', false)
 
+  $ft_options = {
+    'default_backend' => 'bk_mysqlcluster',
+  }
+
   if($ipv6) {
-    haproxy::listen { 'mysqlcluster':
-      bind => {
+    haproxy::frontend { 'ft_mysqlcluster':
+      bind    => {
         "${ipv4}:3306" => [],
         "${ipv6}:3306" => [],
       },
-      mode => 'tcp',
+      mode    => 'tcp',
+      options => $ft_options,
     }
   } else {
-    haproxy::listen { 'mysqlcluster':
+    haproxy::frontend { 'ft_mysqlcluster':
       ipaddress => $ipv4,
       ports     => '3306',
       mode      => 'tcp',
+      options   => $ft_options,
     }
+  }
+
+  haproxy::backend { 'bk_mysqlcluster':
+    mode    => 'tcp',
+    options => {
+      'balance' => 'roundrobin',
+      'option'  => [
+        'tcplog',
+      ],
+    },
   }
 }
