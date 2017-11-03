a4e07f30a7c3866b1e42d7248e8f47c81a239f8e
LarsErikP <pedersen.larserik@gmail.com>
2017-11-03 12:33:50 +0100

    Split puppetdb haproxy config into frontend and backend

--- manifests/services/puppet/db/haproxy/frontend.pp
+++ manifests/services/puppet/db/haproxy/frontend.pp
@@ -6,19 +6,34 @@ class profile::services::puppet::db::haproxy::frontend {
   $ipv4 = hiera('profile::haproxy::management::ip')
   $ipv6 = hiera('profile::haproxy::management::ipv6', false)
 
+  $ft_options = {
+    'default_backend' => 'bk_puppetdb',
+  }
+
   if($ipv6) {
-    haproxy::listen { 'puppetdb':
-      bind => {
+    haproxy::frontend { 'ft_puppetdb':
+      bind    => {
         "${ipv4}:8081" => [],
         "${ipv6}:8081" => [],
       },
-      mode => 'tcp',
+      mode    => 'tcp',
+      options => $ft_options,
     }
   } else {
-    haproxy::listen { 'puppetdb':
+    haproxy::frontend { 'ft_puppetdb':
       ipaddress => $ipv4,
       ports     => '8081',
       mode      => 'tcp',
+      options   => $ft_options,
     }
+  }
+  haproxy::backend { 'bk_puppetdb':
+    mode    => 'tcp',
+    options => {
+      'balance' => 'roundrobin',
+      'option'  => [
+        'tcplog',
+      ],
+    },
   }
 }
