1fffa6e4edd0bc9596de84cb3d0a2d86be10dc08
LarsErikP <pedersen.larserik@gmail.com>
2017-11-03 12:47:40 +0100

    Split puppetserver haproxy config into frontend and backend

--- manifests/services/puppet/server/haproxy/frontend.pp
+++ manifests/services/puppet/server/haproxy/frontend.pp
@@ -6,19 +6,34 @@ class profile::services::puppet::server::haproxy::frontend {
   $ipv4 = hiera('profile::haproxy::management::ip')
   $ipv6 = hiera('profile::haproxy::management::ipv6', false)
 
+  $ft_options = {
+    'default_backend' => 'bk_puppetserver',
+  }
+
   if($ipv6) {
-    haproxy::listen { 'puppetserver':
-      bind => {
+    haproxy::frontend { 'ft_puppetserver':
+      bind    => {
         "${ipv4}:8140" => [],
         "${ipv6}:8140" => [],
       },
-      mode => 'tcp',
+      mode    => 'tcp',
+      options => $ft_options,
     }
   } else {
-    haproxy::listen { 'puppetserver':
+    haproxy::frontend { 'ft_puppetserver':
       ipaddress => $ipv4,
       ports     => '8140',
       mode      => 'tcp',
+      options   => $ft_options,
     }
+  }
+  hapoxy::backend { 'bk_puppetserver':
+    mode    => 'tcp',
+    options => {
+      'balance' => 'roundrobin',
+      'option'  => [
+        'tcplog',
+      ],
+    },
   }
 }
