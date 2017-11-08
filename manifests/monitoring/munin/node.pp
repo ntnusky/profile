592830f38f2d5014b8cefb25fb775d19d7a9c13f
Eigil Obrestad <eigil-git@obrestad.org>
2017-11-08 10:25:14 +0100

    Bind munin-node on all interfaces

--- manifests/monitoring/munin/node.pp
+++ manifests/monitoring/munin/node.pp
@@ -1,8 +1,5 @@
 # Defines a generic munin node
 class profile::monitoring::munin::node {
-  $management_if = hiera('profile::interfaces::management')
-  $management_ip = $::facts['networking']['interfaces'][$management_if]['ip']
-
   $management_ipv4 = hiera('profile::networks::management::ipv4::prefix')
   $management_ipv6 = hiera('profile::networks::management::ipv6::prefix')
 
@@ -11,7 +8,6 @@ class profile::monitoring::munin::node {
   include ::profile::monitoring::munin::plugin::puppet
 
   class {'::munin::node':
-    bind_address   => $management_ip,
     allow          => [
       '127.0.0.0/8',
       '::1/128',
