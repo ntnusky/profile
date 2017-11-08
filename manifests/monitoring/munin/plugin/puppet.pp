b5f893ec04dc3c31cbf0b7fe6fa5e0b73f1da30e
Eigil Obrestad <eigil-git@obrestad.org>
2017-11-08 11:01:34 +0100

    Run plugin as a privileged user

--- manifests/monitoring/munin/plugin/puppet.pp
+++ manifests/monitoring/munin/plugin/puppet.pp
@@ -3,17 +3,21 @@ class profile::monitoring::munin::plugin::puppet {
   munin::plugin { 'puppet_lastrun':
     ensure => present,
     source => 'puppet:///modules/profile/muninplugins/puppet_lastrun',
+    config => [ 'user root' ],
   }
   munin::plugin { 'puppet_resources':
     ensure => present,
     source => 'puppet:///modules/profile/muninplugins/puppet_resources',
+    config => [ 'user root' ],
   }
   munin::plugin { 'puppet_resource_stat':
     ensure => present,
     source => 'puppet:///modules/profile/muninplugins/puppet_resource_stat',
+    config => [ 'user root' ],
   }
   munin::plugin { 'puppet_runtime':
     ensure => present,
     source => 'puppet:///modules/profile/muninplugins/puppet_runtime',
+    config => [ 'user root' ],
   }
 }
