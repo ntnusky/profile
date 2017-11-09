2b9aa9e86522bded0152bf232fe28fdd85fa74ad
Eigil Obrestad <eigil-git@obrestad.org>
2017-11-09 12:18:42 +0100

    Leave only haproxy_ng

--- manifests/monitoring/munin/plugin/haproxy.pp
+++ manifests/monitoring/munin/plugin/haproxy.pp
@@ -7,16 +7,6 @@ class profile::monitoring::munin::plugin::haproxy {
     }
   )
   
-  munin::plugin { "haproxy_bk_puppetserver":
-    ensure => link,
-    target => 'haproxy_',
-    config => ['env.url http://localhost:9000/haproxy-status;csv;norefresh'],
-  }
-  munin::plugin { "haproxy_ft_puppetserver":
-    ensure => link,
-    target => 'haproxy_',
-    config => ['env.url http://localhost:9000/haproxy-status;csv;norefresh'],
-  }
   munin::plugin { "haproxy_ng":
     ensure => link,
     config => ['env.url http://localhost:9000/haproxy-status;csv;norefresh'],
