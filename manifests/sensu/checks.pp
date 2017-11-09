dda78a684409f9ca9d5506e8e223aa68ad1e4eee
LarsErikP <pedersen.larserik@gmail.com>
2017-11-09 09:23:54 +0100

    A comma to break it all

--- manifests/sensu/checks.pp
+++ manifests/sensu/checks.pp
@@ -34,7 +34,7 @@ class profile::sensu::checks {
   }
 
   sensu::check { 'puppetrun':
-    command     => "sudo check-puppet-last-run.rb -r -w ${puppetwarn} -c ${puppetcrit}"
+    command     => "sudo check-puppet-last-run.rb -r -w ${puppetwarn} -c ${puppetcrit}",
     interval    => 300,
     standalone  => false,
     subscribers => [ 'all' ],
