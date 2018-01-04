edae0a3fccb2e9bf33d958a8ecb1867c0ec06606
Eigil Obrestad <eigil-git@obrestad.org>
2018-01-04 16:28:25 +0100

    Add support for two management network prefixes in the firewall config of galera

--- manifests/services/mysql/firewall/server.pp
+++ manifests/services/mysql/firewall/server.pp
@@ -3,6 +3,8 @@ class profile::services::mysql::firewall::server {
   require ::firewall
 
   $management_net = hiera('profile::networks::management::ipv4::prefix')
+  $extra_net = hiera('profile::networks::management::ipv4::prefix::extra',
+      false)
   $management_if = hiera('profile::interfaces::management')
   $ip = $facts['networking']['interfaces'][$management_if]['ip']
 
@@ -30,5 +32,23 @@ class profile::services::mysql::firewall::server {
     action      => 'accept',
     source      => $management_net,
     destination => $ip,
+  }
+
+  if($extra_net) {
+    firewall { '073 Accept incoming MySQL galera':
+      proto       => 'tcp',
+      dport       => [4567, 4568, 4444, 9000],
+      action      => 'accept',
+      source      => $extra_net,
+      destination => $ip,
+    }
+
+    firewall { '074 Accept incoming MySQL galera':
+      proto       => 'udp',
+      dport       => 4567,
+      action      => 'accept',
+      source      => $extra_net,
+      destination => $ip,
+    }
   }
 }
