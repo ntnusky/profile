41b60c26f33ed4497804aed84a05b7469cf03d23
Eigil Obrestad <eigil-git@obrestad.org>
2018-01-23 14:25:32 +0100

    Add folder for a git repo which should be replicated to all tftp servers. Can be used for storing router ACL's for instance.

---
+++ manifests/services/tftp/acl.pp
@@ -0,0 +1,21 @@
+# Installs a script which ensures that all tftp-servers are containing the same
+# set of files which the routers can download. Used for ACL distribution.
+class profile::services::tftp::acl {
+  @@ssh_authorized_key { "routeracl-${::fqdn}":
+    user    => 'root',
+    type    => 'ssh-rsa',
+    key     => $::facts['ssh']['rsa']['key'],
+    tag     => 'routeracl-hostkeys',
+    require => File['/root/.ssh'],
+  }
+
+  Ssh_authorized_key <<| tag == 'routeracl-hostkeys' |>>
+
+  file { '/usr/local/sbin/pull_acl.sh':
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/profile/pull_acl.sh',
+  }
+}
