0b27c62d571ba3c7f56e1e2dc2f55c9c1d88ae85
Eigil Obrestad <eigil-git@obrestad.org>
2018-01-26 16:02:24 +0100

    Added script for deletion of old cacerts on the puppetca role

--- manifests/services/puppet/ca.pp
+++ manifests/services/puppet/ca.pp
@@ -1,6 +1,7 @@
 # Installs and configures a puppetmaster 
 class profile::services::puppet::ca {
   include ::profile::services::puppet::backup::ca
+  include ::profile::services::puppet::ca::certclean
   include ::profile::services::puppet::server::config
   include ::profile::services::puppet::server::firewall
   include ::profile::services::puppet::server::hiera
