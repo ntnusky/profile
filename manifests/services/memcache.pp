a6e27dfd3c3f80ab8fcfc6716e6b322c0615f1fd
LarsErikP <pedersen.larserik@gmail.com>
2018-02-02 11:00:22 +0100

    Support the usage of keepalived for memcached

--- manifests/services/memcache.pp
+++ manifests/services/memcache.pp
@@ -4,15 +4,22 @@ class profile::services::memcache {
   $management_if = hiera('profile::interfaces::management')
   $memcached_port = '11211'
   $installsensu = hiera('profile::sensu::install', true)
+  $use_keepalived = hiera('profile::memcache::keepalived', false)
 
-  # Memcache IP
-  $memcache_ipv4 = $::facts['networking']['interfaces'][$management_if]['ip']
-  $memcache_ipv6 = $::facts['networking']['interfaces'][$management_if]['ip6']
+  if ( $use_keepalived ) {
+    contain ::profile::services::memcache::keepalived
+    $memcache_ip = hiera('profile::memcache::ip')
+    $listen = $memcache_ip
+  } else {
+    $memcache_ipv4 = $::facts['networking']['interfaces'][$management_if]['ip']
+    $memcache_ipv6 = $::facts['networking']['interfaces'][$management_if]['ip6']
+    $listen = "${memcache_ipv4},${memcache_ipv6}"
+  }
 
   contain ::profile::services::memcache::firewall
 
   class { 'memcached':
-    listen_ip => "127.0.0.1,${memcache_ipv4},${memcache_ipv6}",
+    listen_ip => $listen,
     tcp_port  => $memcached_port,
     udp_port  => $memcached_port,
   }
