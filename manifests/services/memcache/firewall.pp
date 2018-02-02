aababc9912b6f2d291a7e5dd42a7ac2552ef2561
LarsErikP <pedersen.larserik@gmail.com>
2018-02-02 13:03:57 +0100

    Ugly hack to avoid non-puppet3-friendly code on old nodes

--- manifests/services/memcache/firewall.pp
+++ manifests/services/memcache/firewall.pp
@@ -5,9 +5,14 @@ class profile::services::memcache::firewall {
   $management_if = hiera('profile::interfaces::management')
   $sourcev4 = hiera('profile::networks::management::ipv4::prefix')
   $sourcev6 = hiera('profile::networks::management::ipv6::prefix')
-  $destv4 = $::facts['networking']['interfaces'][$management_if]['ip']
-  $destv6 = $::facts['networking']['interfaces'][$management_if]['ip6']
   $memcached_port = '11211'
+  $use_keepalived = hiera('profile::memcache::keepalived', false)
+
+  if ( $use_keepalived ) {
+    $destv4 = hiera('profile::memcache::ip')
+  } else {
+    $destv4 = $::facts['networking']['interfaces'][$management_if]['ip']
+  }
 
   firewall { '500 accept incoming memcached tcp':
     source      => $sourcev4,
@@ -25,21 +30,24 @@ class profile::services::memcache::firewall {
     action      => 'accept',
   }
 
-  firewall { '500 accept ipv6 incoming memcached tcp':
-    source      => $sourcev6,
-    destination => $destv6,
-    proto       => 'tcp',
-    dport       => $memcached_port,
-    action      => 'accept',
-    provider    => 'ip6tables',
-  }
+  if ( ! $use_keepalived ) {
+    $destv6 = $::facts['networking']['interfaces'][$management_if]['ip6']
+    firewall { '500 accept ipv6 incoming memcached tcp':
+      source      => $sourcev6,
+      destination => $destv6,
+      proto       => 'tcp',
+      dport       => $memcached_port,
+      action      => 'accept',
+      provider    => 'ip6tables',
+    }
 
-  firewall { '500 accept ipv6 incoming memcached udp':
-    source      => $sourcev6,
-    destination => $destv6,
-    proto       => 'udp',
-    dport       => $memcached_port,
-    action      => 'accept',
-    provider    => 'ip6tables',
+    firewall { '500 accept ipv6 incoming memcached udp':
+      source      => $sourcev6,
+      destination => $destv6,
+      proto       => 'udp',
+      dport       => $memcached_port,
+      action      => 'accept',
+      provider    => 'ip6tables',
+    }
   }
 }
