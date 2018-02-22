6331ac4636f7c58515c3090078ac211cc45ac9ac
Eigil Obrestad <eigil-git@obrestad.org>
2018-02-22 09:17:17 +0100

    Add TLS for the keystone API

--- manifests/openstack/keystone/haproxy/management.pp
+++ manifests/openstack/keystone/haproxy/management.pp
@@ -1,11 +1,40 @@
 # Configures the haproxy frontend for the internal and admin keystone API
 class profile::openstack::keystone::haproxy::management {
   require ::profile::services::haproxy
+  require ::profile::services::haproxy::certs::manageapi
+
   include ::profile::openstack::keystone::firewall::haproxy::management
   include ::profile::openstack::keystone::haproxy::backend::oldmanagement
 
   $ipv4 = hiera('profile::haproxy::management::ipv4')
   $ipv6 = hiera('profile::haproxy::management::ipv6', false)
+  $certificate = hiera('profile::haproxy::services::apicert::certfile', false)
+  $certfile = hiera('profile::haproxy::services::apicert::certfile',
+                    '/etc/ssl/private/haproxy.servicesapi.pem')
+
+  if($certificate) {
+    $ssl = ['ssl', 'crt', $certfile]
+  } else {
+    $ssl = []
+  }
+
+  if($ipv6) {
+    $bind_adm => { 
+      "${ipv4}:35357" => $ssl,
+      "${ipv6}:35357" => $ssl,
+    }
+    $bind_int => { 
+      "${ipv4}:5000" => $ssl,
+      "${ipv6}:5000" => $ssl,
+    }
+  } else {
+    $bind_adm => { 
+      "${ipv4}:35357" => $ssl,
+    }
+    $bind_int => { 
+      "${ipv4}:5000" => $ssl,
+    }
+  }
 
   $ft_admin_options = {
     'default_backend' => 'bk_keystone_admin',
@@ -14,36 +43,15 @@ class profile::openstack::keystone::haproxy::management {
     'default_backend' => 'bk_keystone_internal',
   }
 
-  if($ipv6) {
-    haproxy::frontend { 'ft_keystone_admin':
-      bind    => {
-        "${ipv4}:35357" => [],
-        "${ipv6}:35357" => [],
-      },
-      mode    => 'tcp',
-      options => $ft_admin_options,
-    }
-    haproxy::frontend { 'ft_keystone_internal':
-      bind    => {
-        "${ipv4}:5000" => [],
-        "${ipv6}:5000" => [],
-      },
-      mode    => 'tcp',
-      options => $ft_internal_options,
-    }
-  } else {
-    haproxy::frontend { 'ft_keystone_admin':
-      ipaddress => $ipv4,
-      ports     => '35357',
-      mode      => 'tcp',
-      options   => $ft_admin_options,
-    }
-    haproxy::frontend { 'ft_keystone_internal':
-      ipaddress => $ipv4,
-      ports     => '5000',
-      mode      => 'tcp',
-      options   => $ft_internal_options,
-    }
+  haproxy::frontend { 'ft_keystone_admin':
+    bind    => $bind_adm,
+    mode    => 'http',
+    options => $ft_admin_options,
+  }
+  haproxy::frontend { 'ft_keystone_internal':
+    bind    => $bind_int,
+    mode    => 'http',
+    options => $ft_internal_options,
   }
 
   $backend_options = {
@@ -56,12 +64,12 @@ class profile::openstack::keystone::haproxy::management {
   }
 
   haproxy::backend { 'bk_keystone_admin':
-    mode    => 'tcp',
+    mode    => 'http',
     options => $backend_options,
   }
 
   haproxy::backend { 'bk_keystone_internal':
-    mode    => 'tcp',
+    mode    => 'http',
     options => $backend_options,
   }
 }
--- manifests/openstack/keystone/haproxy/services.pp
+++ manifests/openstack/keystone/haproxy/services.pp
@@ -1,36 +1,46 @@
 # Configures the haproxy frontend for the public keystone API
 class profile::openstack::keystone::haproxy::services {
   require ::profile::services::haproxy
+  require ::profile::services::haproxy::certs::serviceapi
+
   include ::profile::openstack::keystone::firewall::haproxy::services
   include ::profile::openstack::keystone::haproxy::backend::oldpublic
 
   $ipv4 = hiera('profile::haproxy::services::ipv4')
   $ipv6 = hiera('profile::haproxy::services::ipv6', false)
+  $certificate = hiera('profile::haproxy::services::apicert::certfile', false)
+  $certfile = hiera('profile::haproxy::services::apicert::certfile',
+                    '/etc/ssl/private/haproxy.servicesapi.pem')
 
   $ft_options = {
     'default_backend' => 'bk_keystone_public',
   }
 
+  if($certificate) {
+    $ssl = ['ssl', 'crt', $certfile]
+  } else {
+    $ssl = []
+  }
+
   if($ipv6) {
-    haproxy::frontend { 'ft_keystone_public':
-      bind    => {
-        "${ipv4}:5000" => [],
-        "${ipv6}:5000" => [],
-      },
-      mode    => 'tcp',
-      options => $ft_options,
+    $bind => { 
+      "${ipv4}:5000" => $ssl,
+      "${ipv6}:5000" => $ssl,
     }
   } else {
-    haproxy::frontend { 'ft_keystone_public':
-      ipaddress => $ipv4,
-      ports     => '5000',
-      mode      => 'tcp',
-      options   => $ft_options,
+    $bind => { 
+      "${ipv4}:5000" => $ssl,
     }
   }
 
+  haproxy::frontend { 'ft_keystone_public':
+    bind    => $bind
+    mode    => 'http',
+    options => $ft_options,
+  }
+
   haproxy::backend { 'bk_keystone_public':
-    mode    => 'tcp',
+    mode    => 'http',
     options => {
       'balance' => 'source',
       'option'  => [
