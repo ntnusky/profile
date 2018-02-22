336d430f48bd02d1884aa7bcb4ee72cb1d32de39
Eigil Obrestad <eigil-git@obrestad.org>
2018-02-22 13:55:09 +0100

    Allow glance to grab its endpoint from hiera

--- manifests/openstack/glance/api.pp
+++ manifests/openstack/glance/api.pp
@@ -2,10 +2,24 @@
 class profile::openstack::glance::api {
   $region = hiera('profile::region')
 
-  $keystone_public_ip = hiera('profile::api::keystone::public::ip')
-  $keystone_admin_ip = hiera('profile::api::keystone::admin::ip')
+  $adminlb_ip = hiera('profile::haproxy::management::ipv4', false)
+
   $glance_public_ip = hiera('profile::api::glance::public::ip')
   $glance_admin_ip = hiera('profile::api::glance::admin::ip')
+  $keystone_public_ip = hiera('profile::api::keystone::public::ip')
+  $keystone_admin_ip = hiera('profile::api::keystone::admin::ip')
+  $keystone_password = hiera('profile::glance::keystone::password')
+
+  $admin_endpoint = hiera('profile::openstack::endpoint::admin', false)
+  $internal_endpoint = hiera('profile::openstack::endpoint::internal', false)
+  $public_endpoint = hiera('profile::openstack::endpoint::public', false)
+
+  $glance_admin    = pick($admin_endpoint, "http://${glance_admin_ip}")
+  $glance_internal = pick($internal_endpoint, "http://${glance_admin_ip}")
+  $glance_public   = pick($public_endpoint, "http://${glance_public_ip}")
+  $keystone_admin    = pick($admin_endpoint, "http://${keystone_admin_ip}")
+  $keystone_internal = pick($internal_endpoint, "http://${keystone_admin_ip}")
+  $keystone_public   = pick($public_endpoint, "http://${keystone_public_ip}")
 
   $memcached_ip = hiera('profile::memcache::ip')
 
@@ -16,55 +30,38 @@ class profile::openstack::glance::api {
   $mysql_ip = hiera('profile::mysql::ip')
   $database_connection = "mysql://glance:${mysql_pass}@${mysql_ip}/glance"
 
-  # Firewall settings
-  $source_firewall_management_net = hiera('profile::networks::management::ipv4::prefix')
-
   require ::profile::baseconfig::firewall
   require ::profile::openstack::repo
   require ::profile::openstack::glance::database
+  require ::profile::openstack::glance::firewall::server::api
   contain ::profile::openstack::glance::keepalived
 
-  firewall { '500 accept incoming admin glance tcp':
-    source      => $source_firewall_management_net,
-    proto       => 'tcp',
-    destination => $glance_admin_ip,
-    dport       => '9292',
-    action      => 'accept',
-  }
-
-  firewall { '500 accept incoming public glance tcp':
-    source      => $source_firewall_management_net,
-    proto       => 'tcp',
-    destination => $glance_public_ip,
-    dport       => '9292',
-    action      => 'accept',
-  }
-
   class { '::glance::api':
-    keystone_password       => $mysql_pass,
+    # Auth_strategy is blank to prevent glance::api from including
+    # ::glance::api::authtoken.
     auth_strategy           => '',
     database_connection     => $database_connection,
-    registry_host           => $management_ip,
-    os_region_name          => $region,
+    keystone_password       => $keystone_password,
     known_stores            => ['glance.store.rbd.Store'],
+    os_region_name          => $region,
+    registry_host           => pick($adminlb_ip, $management_ip),
     show_image_direct_url   => true,
     show_multiple_locations => true,
-    pipeline                => 'keystone',
   }
 
   class { '::glance::api::authtoken':
-    password          => $mysql_pass,
-    auth_url          => "http://${keystone_admin_ip}:35357",
-    auth_uri          => "http://${keystone_public_ip}:5000",
+    password          => $keystone_password,
+    auth_url          => "${keystone_admin}:35357",
+    auth_uri          => "${keystone_public}:5000",
     memcached_servers => $memcached_ip,
     region_name       => $region,
   }
 
   class  { '::glance::keystone::auth':
-    password     => $mysql_pass,
-    public_url   => "http://${glance_public_ip}:9292",
-    internal_url => "http://${glance_admin_ip}:9292",
-    admin_url    => "http://${glance_admin_ip}:9292",
+    password     => $keystone_password,
+    public_url   => "${glance_public}:9292",
+    internal_url => "${glance_admin}:9292",
+    admin_url    => "${glance_admin}:9292",
     region       => $region,
   }
 
--- manifests/openstack/glance/registry.pp
+++ manifests/openstack/glance/registry.pp
@@ -1,33 +1,46 @@
 # Configures glance registry and backend
 class profile::openstack::glance::registry {
   $region = hiera('profile::region')
+
+  $admin_endpoint = hiera('profile::openstack::endpoint::admin', false)
+  $internal_endpoint = hiera('profile::openstack::endpoint::internal', false)
+  $public_endpoint = hiera('profile::openstack::endpoint::public', false)
+
   $keystone_public_ip = hiera('profile::api::keystone::public::ip')
   $keystone_admin_ip = hiera('profile::api::keystone::admin::ip')
+  $keystone_password = hiera('profile::glance::keystone::password')
+
+  $keystone_admin    = pick($admin_endpoint, "http://${keystone_admin_ip}")
+  $keystone_internal = pick($internal_endpoint, "http://${keystone_admin_ip}")
+  $keystone_public   = pick($public_endpoint, "http://${keystone_public_ip}")
 
   $memcached_ip = hiera('profile::memcache::ip')
   $mysql_pass = hiera('profile::mysql::glancepass')
   $mysql_ip = hiera('profile::mysql::ip')
   $database_connection = "mysql://glance:${mysql_pass}@${mysql_ip}/glance"
 
-  require ::profile::openstack::repo
-  contain ::profile::openstack::glance::ceph
   require ::profile::openstack::glance::base
+  contain ::profile::openstack::glance::ceph
   require ::profile::openstack::glance::database
+  require ::profile::openstack::glance::firewall::server::registry
+  require ::profile::openstack::repo
 
   class { '::glance::backend::rbd' :
     rbd_store_user => 'glance',
   }
 
   class { '::glance::registry':
-    keystone_password   => $mysql_pass,
-    database_connection => $database_connection,
+    # Auth_strategy is blank to prevent glance::registry from including
+    # ::glance::registry::authtoken.
     auth_strategy       => '',
+    database_connection => $database_connection,
+    keystone_password   => $keystone_password,
   }
 
   class { '::glance::registry::authtoken':
-    password          => $mysql_pass,
-    auth_url          => "http://${keystone_admin_ip}:35357",
-    auth_uri          => "http://${keystone_public_ip}:5000",
+    password          => $keystone_password,
+    auth_url          => "${keystone_admin}:35357",
+    auth_uri          => "${keystone_public}:5000",
     memcached_servers => $memcached_ip,
     region_name       => $region,
   }
