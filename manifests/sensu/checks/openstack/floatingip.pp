09e23e72bdbb521b52bf33f36bd3a64073e5969b
LarsErikP <pedersen.larserik@gmail.com>
2018-03-01 13:23:17 +0100

    Change subscription name for floating ip checks

--- manifests/sensu/checks/openstack/floatingip.pp
+++ manifests/sensu/checks/openstack/floatingip.pp
@@ -6,12 +6,12 @@ class profile::sensu::checks::openstack::floatingip {
     command     => "/etc/sensu/plugins/extra/check_os_floating_ip.sh -p :::os.password::: -u ${openstack_api}:5000/v3 -s :::os.floating-rfc1918::: -w :::os.floating-rfc1918-warn|100::: -c :::os.floating-rfc1918-critical|50:::",
     interval    => 300,
     standalone  => false,
-    subscribers => [ 'os-infra-checks'],
+    subscribers => [ 'os-floatingip'],
   }
   sensu::check { 'openstack-floating-ip-gua':
     command     => "/etc/sensu/plugins/extra/check_os_floating_ip.sh -p :::os.password::: -u ${openstack_api}:5000/v3 -s :::os.floating-gua::: -w :::os.floating-gua-warn|100::: -c :::os.floating-gua-critical|50:::",
     interval    => 300,
     standalone  => false,
-    subscribers => [ 'os-infra-checks'],
+    subscribers => [ 'os-floatingip'],
   }
 }
