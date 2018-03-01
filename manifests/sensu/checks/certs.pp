a69ee82797fa340330c1d52b526e0ab185481199
LarsErikP <pedersen.larserik@gmail.com>
2018-03-01 15:59:05 +0100

    TLS expiry checks for sensu

---
+++ manifests/sensu/checks/certs.pp
@@ -0,0 +1,14 @@
+# HTTPS certificate checks
+class profile::sensu::checks::certs {
+
+  $domains = hiera_hash('profile::sensu::checks::tlsexpiry')
+
+  $domains.each | $domain, $displayname | {
+    sensu::check { "tls-expiry-${displayname}":
+      command     => "check-https-cert.rb -u https://${domain} -w 30 -c 7",
+      interval    => 300,
+      standalone  => false,
+      subscribers => [ 'tls-expiry' ],
+    }
+  }
+}
