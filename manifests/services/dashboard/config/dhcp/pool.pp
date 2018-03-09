53d23a9e93e9fece97a8cf666a083f991745ba2c
Eigil Obrestad <eigil-git@obrestad.org>
2018-03-09 15:09:12 +0100

    Allow adding a gateway to a network to be optional

--- manifests/services/dashboard/config/dhcp/pool.pp
+++ manifests/services/dashboard/config/dhcp/pool.pp
@@ -2,7 +2,7 @@
 define profile::services::dashboard::config::dhcp::pool {
   $network = hiera("profile::networks::${name}::ipv4::id")
   $mask = hiera("profile::networks::${name}::ipv4::mask")
-  $gateway = hiera("profile::networks::${name}::ipv4::gateway")
+  $gateway = hiera("profile::networks::${name}::ipv4::gateway", false)
   $domain = hiera("profile::networks::${name}::domain")
   $range = hiera("profile::networks::${name}::ipv4::dynamicrange", '')
   $reserved = hiera_array("profile::networks::${name}::ipv4::reserved", [])
@@ -43,16 +43,28 @@ define profile::services::dashboard::config::dhcp::pool {
               File['/etc/shiftleader'],
             ],
   }
-
-  ini_setting { "Machineadmin DHCP Pool ${name} gateway":
-    ensure  => present,
-    path    => $configfile,
-    section => 'DHCP',
-    setting => "${name}Gateway",
-    value   => $gateway,
-    require => [
-              File['/etc/shiftleader'],
-            ],
+  
+  if($gateway) {
+    ini_setting { "Machineadmin DHCP Pool ${name} gateway":
+      ensure  => present,
+      path    => $configfile,
+      section => 'DHCP',
+      setting => "${name}Gateway",
+      value   => $gateway,
+      require => [
+                File['/etc/shiftleader'],
+              ],
+    }
+  } else {
+    ini_setting { "Machineadmin DHCP Pool ${name} gateway":
+      ensure  => absent,
+      path    => $configfile,
+      section => 'DHCP',
+      setting => "${name}Gateway",
+      require => [
+                File['/etc/shiftleader'],
+              ],
+    }
   }
 
   $range2 = regsubst($range, ' ', '-', 'G')
