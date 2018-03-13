50a79442255f681794771d8731baa83e5de2c16d
Eigil Obrestad <eigil-git@obrestad.org>
2018-03-13 12:03:38 +0100

    Refactoring, and adding the possibility to have more than one CIDR for cluster and public networks. There are new hierakeys, and one of them (the one for the public networks) is mandatory. - Take 2

--- manifests/ceph/monitor.pp
+++ manifests/ceph/monitor.pp
@@ -1,45 +1,31 @@
 # Install and configure ceph-mon
 class profile::ceph::monitor {
-  $controllernames = join(hiera('controller::names'), ',')
-  $controlleraddresses = join(hiera('controller::storage::addresses'), ',')
-
-  $fsid = hiera('profile::ceph::fsid')
   $mon_key = hiera('profile::ceph::monitor_key')
   $mgr_key = hiera('profile::ceph::mgr_key')
   $admin_key = hiera('profile::ceph::admin_key')
   $bootstrap_osd_key = hiera('profile::ceph::osd_bootstrap_key')
-  $replicas =  hiera('profile::ceph::replicas', undef)
-  $journal_size =  hiera('profile::ceph::journal::size', 10000)
 
-  $installMunin = hiera('profile::munin::install', true)
-  if($installMunin) {
+  $installmunin = hiera('profile::munin::install', true)
+  if($installmunin) {
     include ::profile::munin::plugin::ceph
   }
 
-  $installSensu = hiera('profile::sensu::install', true)
-  if ($installSensu) {
+  $installsensu = hiera('profile::sensu::install', true)
+  if ($installsensu) {
     include ::profile::sensu::plugin::ceph
   }
 
-  class { 'ceph::repo': }->
-  class { 'ceph':
-    fsid                  => $fsid,
-    mon_initial_members   => $controllernames,
-    mon_host              => $controlleraddresses,
-    osd_pool_default_size => $replicas,
-    before                => Anchor['profile::ceph::monitor::end']
-  }
+  require ::profile::ceph::base
+
   ceph::mgr { $::hostname :
     key        => $mgr_key,
     inject_key => true,
-  }
-  ceph_config {
-    'global/osd_journal_size': value => $journal_size;
   }
   ceph::mon { $::hostname:
     key    => $mon_key,
     before => Anchor['profile::ceph::monitor::end']
   }
+
   Ceph::Key {
     inject         => true,
     inject_as_id   => 'mon.',
@@ -59,5 +45,6 @@ class profile::ceph::monitor {
     cap_mon => 'allow profile bootstrap-osd',
     before  => Anchor['profile::ceph::monitor::end']
   }
+
   anchor{'profile::ceph::monitor::end':}
 }
--- manifests/ceph/osd.pp
+++ manifests/ceph/osd.pp
@@ -1,35 +1,12 @@
 # Installs and configures a ceph storage node, and adds osd's according to the
 # hiera 'profile::ceph::osds' key.
 class profile::ceph::osd {
-  $controllernames = join(hiera('controller::names'), ',')
-  $controlleraddresses = join(hiera('controller::storage::addresses'), ',')
-  
-  $fsid = hiera('profile::ceph::fsid')
-  $mon_key = hiera('profile::ceph::monitor_key')
-  $admin_key = hiera('profile::ceph::admin_key')
   $bootstrap_osd_key = hiera('profile::ceph::osd_bootstrap_key')
-  $replicas =  hiera('profile::ceph::replicas', undef)
-  $journal_size =  hiera('profile::ceph::journal::size', 10000)
-  $bluestore_cache_size = hiera('profile::ceph::bluestore::cache::size', undef)
 
-  $cluster_network = hiera('profile::ceph::cluster_network', undef)
-  $public_network = hiera('profile::ceph::public_network', undef)
-  
   $osds = hiera('profile::ceph::osds')
-  
-  class { 'ceph::repo': }
-  class { 'ceph':
-    fsid                  => $fsid,
-    mon_initial_members   => $controllernames,
-    mon_host              => $controlleraddresses,
-    osd_pool_default_size => $replicas,
-    public_network        => $public_network,
-    cluster_network       => $cluster_network,
-  }
-  ceph_config {
-    'global/osd_journal_size': value     => $journal_size;
-    'global/bluestore_cache_size': value => $bluestore_cache_size;
-  }
+
+  require ::profile::ceph::base
+
   ceph::key {'client.bootstrap-osd':
     keyring_path => '/var/lib/ceph/bootstrap-osd/ceph.keyring',
     secret       => $bootstrap_osd_key,
