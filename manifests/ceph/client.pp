8c341796823fd37fcbddf86e47f869a732ec303b
Eigil Obrestad <eigil-git@obrestad.org>
2018-03-13 10:57:56 +0100

    Refactoring, and adding the possibility to have more than one CIDR for cluster and public networks. There are new hierakeys, and one of them (the one for the public networks) is mandatory.

--- manifests/ceph/client.pp
+++ manifests/ceph/client.pp
@@ -1,29 +1,18 @@
 # This class installs the ceph client, and configures to be able to use the ceph
 # cluster.
 class profile::ceph::client {
-  $controllernames = join(hiera('controller::names'), ',')
-  $controlleraddresses = join(hiera('controller::storage::addresses'), ',')
-
-  $fsid = hiera('profile::ceph::fsid')
   $admin_key = hiera('profile::ceph::admin_key')
-  $replicas =  hiera('profile::ceph::replicas', undef)
 
-  $journal_size =  hiera('profile::ceph::journal::size', 10000)
+  require ::profile::ceph::base
 
-  class { 'ceph::repo': } ->
-  class { 'ceph':
-    fsid                  => $fsid,
-    mon_initial_members   => $controllernames,
-    mon_host              => $controlleraddresses,
-    osd_pool_default_size => $replicas,
-  } ->
   ceph::key { 'client.admin':
     secret => $admin_key,
-  } ->
-  anchor{'profile::ceph::client::end':}
+  }
+
   ceph_config {
-    'global/osd_journal_size': value                   => $journal_size;
-    'client/rbd cache': value                          => 'true';
-    'client/rbd cache writethrough until flush': value  => 'true';
+    'client/rbd cache':
+      value => true;
+    'client/rbd cache writethrough until flush':
+      value => true;
   }
 }
