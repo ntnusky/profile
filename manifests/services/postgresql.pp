92cab4e3f840243fc5065acc06dd091003033027
LarsErikP <pedersen.larserik@gmail.com>
2018-03-21 13:51:57 +0100

    Added munin graphs for postgresql

--- manifests/services/postgresql.pp
+++ manifests/services/postgresql.pp
@@ -4,4 +4,9 @@ class profile::services::postgresql {
   contain profile::services::postgresql::firewall
   contain profile::services::postgresql::keepalived
   contain profile::services::postgresql::server
+
+  $installMunin = hiera('profile::munin::install', true)
+  if($installMunin) {
+    include ::profile::monitoring::munin::plugin::postgresql
+  }
 }
