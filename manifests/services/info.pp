2e05594ceefb74b7407902c8c247142d520af795
LarsErikP <pedersen.larserik@gmail.com>
2018-04-13 13:36:05 +0200

    Actually specifying which interface we're trying to get addresses for may be a good idea

--- manifests/services/info.pp
+++ manifests/services/info.pp
@@ -1,6 +1,7 @@
 # Define an apache vhost for random static info
 class profile::services::info {
   $vhost = lookup('profile::info::maillist::fqdn')
+  $management_if = lookup('profile::interfaces::management')
   $management_ipv4 = $::facts['networking']['interfaces'][$management_if]['ip']
   $management_ipv6 = $::facts['networking']['interfaces'][$management_if]['ip6']
 
