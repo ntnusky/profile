333f0e6934e448b98f085817210b278b32ece994
LarsErikP <pedersen.larserik@gmail.com>
2018-04-13 13:15:09 +0200

    Create a vhost for static info, and fix the mail list template for puppet5

---
+++ manifests/services/info.pp
@@ -0,0 +1,17 @@
+# Define an apache vhost for random static info
+class profile::services::info {
+  $vhost = lookup('profile::info::maillist::fqdn')
+  $management_ipv4 = $::facts['networking']['interfaces'][$management_if]['ip']
+  $management_ipv6 = $::facts['networking']['interfaces'][$management_if]['ip6']
+
+  require ::profile::services::apache
+  contain ::profile::services::info::maillist
+
+  apache::vhost { "${vhost} http":
+    servername => $vhost,
+    port       => '80',
+    ip         => concat([], $management_ipv4, $management_ipv6),
+    add_listen => false,
+    docroot    => "/var/www/${vhost}",
+  }
+}
---
+++ manifests/services/info/maillist.pp
@@ -0,0 +1,24 @@
+# Defines a cron job for automatic mail list generation
+class profile::services::info::maillist {
+  $mailfqdn = lookup('profile::info::maillist::fqdn')
+
+  file { '/usr/local/bin/generateMailList.sh':
+    ensure  => file,
+    owner   => 'root',
+    mode    => '0755',
+    content => template('profile/generateMailList.erb'),
+  }
+
+  cron { 'maillist':
+    command => '/usr/local/bin/generateMailList.sh 2>&1 /dev/null',
+    user    => 'root',
+    hour    => '*/2',
+    minute  => '00',
+  }
+
+  file { "/var/www/${mailfqdn}/mail":
+    ensure => directory,
+    owner  => 'root',
+    mode   => '0755',
+  }
+}
