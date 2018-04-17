11183465e0b1ecef3a7de0ac05219c911b60181a
LarsErikP <pedersen.larserik@gmail.com>
2018-04-17 10:19:43 +0200

    Add basic auth to maillist

--- manifests/services/info.pp
+++ manifests/services/info.pp
@@ -4,6 +4,9 @@ class profile::services::info {
   $management_if = lookup('profile::interfaces::management')
   $management_ipv4 = $::facts['networking']['interfaces'][$management_if]['ip']
   $management_ipv6 = $::facts['networking']['interfaces'][$management_if]['ip6']
+  $auth_password = lookup('profile::info::auth_password')
+  $auth_user = lookup('profile::info::auth_user', 'sympa')
+  $pw_hash = apache_pw_hash($auth_password)
 
   require ::profile::services::apache
   contain ::profile::services::info::maillist
@@ -16,9 +19,20 @@ class profile::services::info {
     docroot           => "/var/www/${vhost}",
     access_log_format => 'forwarded',
     directories       => [
-      { path    => '/mail',
-        options => '-Indexes',
+      {
+        path           => '/mail',
+        options        => '-Indexes',
+        auth_type      => 'Basic',
+        auth_name      => 'Maillist auth',
+        auth_user_file => "/var/www/${vhost}/.htpasswd",
+        auth_require   => 'valid-user',
       },
     ],
+  }
+
+  file { "/var/www/${vhost}/.htpasswd":
+    ensure  => file,
+    mode    => '0400',
+    content => "${auth_user}:${pw_hash}",
   }
 }
