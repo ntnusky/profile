04b6b437090931d540ddc3beed845cadb2ce06a0
Eigil Obrestad <eigil-git@obrestad.org>
2018-04-25 14:51:12 +0200

    Use mysql trough the load-balancers if it is available

--- manifests/openstack/cinder/base.pp
+++ manifests/openstack/cinder/base.pp
@@ -1,9 +1,13 @@
 # Configures the base cinder config
 class profile::openstack::cinder::base {
+  # Determine where the database resides 
   $mysql_pass = hiera('profile::mysql::cinderpass')
-  $mysql_ip = hiera('profile::mysql::ip')
+  $mysql_old = hiera('profile::mysql::ip', undef)
+  $mysql_new = hiera('profile::haproxy::management::ipv4', undef)
+  $mysql_ip = pick($mysql_new, $mysql_old)
   $database_connection = "mysql://cinder:${mysql_pass}@${mysql_ip}/cinder"
 
+  # Credentials for the messagequeue
   $rabbit_ip = hiera('profile::rabbitmq::ip')
   $rabbit_user = hiera('profile::rabbitmq::rabbituser')
   $rabbit_pass = hiera('profile::rabbitmq::rabbitpass')
