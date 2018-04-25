8f824d16f53a0acf3a972ea1dfcb22973acf9472
Eigil Obrestad <eigil-git@obrestad.org>
2018-04-25 16:06:06 +0200

    Include the cinder database on the mysql hosts.

--- manifests/openstack/databases.pp
+++ manifests/openstack/databases.pp
@@ -1,5 +1,6 @@
 # Defines the databases used by openstack
 class profile::openstack::databases {
+  include ::profile::openstack::cinder::database
   include ::profile::openstack::keystone::database
   include ::profile::openstack::glance::database
 }
