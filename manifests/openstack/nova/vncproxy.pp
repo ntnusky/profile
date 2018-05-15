c21a5329b9e4b01486add22139fc52b9b45cdb6f
Eigil Obrestad <eigil-git@obrestad.org>
2018-05-15 15:35:45 +0200

    Prepare nova services for standalone install

--- manifests/openstack/nova/vncproxy.pp
+++ manifests/openstack/nova/vncproxy.pp
@@ -1,11 +1,8 @@
 # Installs the nova vnc proxy
 class profile::openstack::nova::vncproxy {
-  $vnc_proxy_ip = hiera('nova::vncproxy::host')
-
   require ::profile::openstack::repo
 
   class { '::nova::vncproxy':
-    host    => $vnc_proxy_ip,
     enabled => true,
   }
 }
