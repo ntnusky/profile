4d73c2b5c1c228e4725756a42aa8fd68d161fa07
Eigil Obrestad <eigil-git@obrestad.org>
2018-05-28 13:13:27 +0200

    Configure the vncproxy

--- manifests/openstack/nova/vncproxy.pp
+++ manifests/openstack/nova/vncproxy.pp
@@ -4,6 +4,23 @@ class profile::openstack::nova::vncproxy {
   include ::profile::openstack::nova::haproxy::backend::vnc
   require ::profile::openstack::repo
 
+  $cert = hiera('profile::haproxy::services::apicert', false)
+  $host = hiera('profile::horizon::server_name')
+  $port = hiera('profile::vncproxy::port', 6080)
+
+  if($cert) {
+    $protocol = 'https'
+  } else {
+    $protocol = 'http'
+  }
+
+  class { '::nova::vncproxy::common':
+    vncproxy_host     => $host,
+    vncproxy_protocol => $protocol,
+    vncproxy_port     => $port,
+    before            => Class['::nova::vncproxy'],
+  }
+
   class { '::nova::vncproxy':
     enabled => true,
   }
