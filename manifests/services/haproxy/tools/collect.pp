c27a4a95309972562873191a75c839c611d6d251
Eigil Obrestad <eigil-git@obrestad.org>
2018-07-05 11:54:19 +0200

    Removed ordering

--- manifests/services/haproxy/tools/collect.pp
+++ manifests/services/haproxy/tools/collect.pp
@@ -1,7 +1,7 @@
 # Adds an entry to the haproxy tools configfile.
 define profile::services::haproxy::tools::collect (
 ){
-  require ::profile::services::haproxy::tools
+  include ::profile::services::haproxy::tools
 
   Concat::Fragment <<| tag == "haproxy-${name}" |>>
 }
