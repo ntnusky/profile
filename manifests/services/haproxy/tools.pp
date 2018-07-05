cf9057fb2de78dac4f733305ef546ab07567f286
Eigil Obrestad <eigil-git@obrestad.org>
2018-07-05 11:05:20 +0200

    Start creating a configfiles for the haproxy tools.

---
+++ manifests/services/haproxy/tools.pp
@@ -0,0 +1,21 @@
+# Installs and configures a utility for simple management of the haproxy service
+# while its running.
+class profile::services::haproxy::tools {
+  $configfile = lookup({
+    'name'          => 'profile::haproxy::tools::configfile',
+    'default_value' => '/etc/haproxy/toolconfig.txt',
+  })
+
+  concat { $configfile:
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+
+  concat::fragment{ 'haproxytools config header':
+    target  => $configfile,
+    content => '# This file is managed by puppet',
+    order   => '01'
+  }
+}
