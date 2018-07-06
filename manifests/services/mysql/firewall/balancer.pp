6378b3f9990dc1216fcc159625e8b90c7a463cc3
Eigil Obrestad <eigil-git@obrestad.org>
2018-07-06 14:02:55 +0200

    Doh

--- manifests/services/mysql/firewall/balancer.pp
+++ manifests/services/mysql/firewall/balancer.pp
@@ -2,7 +2,7 @@
 class profile::services::mysql::firewall::balancer {
   require ::firewall
 
-  $managementv4 = hiera('profile::networks::management::ipv6::prefix', false)
+  $managementv4 = hiera('profile::networks::management::ipv4::prefix', false)
   $managementv6 = hiera('profile::networks::management::ipv6::prefix', false)
 
   if($managementv4) {
