368df426451e45ebee860c3e5079fb10a5b6d153
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2018-09-04 09:22:16 +0200

    Add firewall rules to allow rabbitmq clustering

--- manifests/services/rabbitmq/firewall.pp
+++ manifests/services/rabbitmq/firewall.pp
@@ -6,20 +6,41 @@ class profile::services::rabbitmq::firewall {
   $rabbitmq_ip = hiera('profile::rabbitmq::ip')
   $extra_net = hiera('profile::networks::management::ipv4::prefix::extra',false)
 
+  $enable_keepalived = hiera('profile::rabbitmq::keepalived::enable')
+  if ( $enable_keepalived ) {
+    $dest = $rabbitmq_ip
+  } else {
+    $dest = $::facts['networking']['ip']
+  }
+
   firewall { '500 accept incoming rabbitmq':
     source      => $management_net,
-    destination => $rabbitmq_ip,
+    destination => $dest,
     proto       => 'tcp',
     dport       => 5672,
+    action      => 'accept',
+  }
+  firewall { '502 accept incoming rabbitmqcluster':
+    source      => $management_net,
+    destination => $dest,
+    proto       => 'tcp',
+    dport       => 25672,
     action      => 'accept',
   }
 
   if ($extra_net) {
     firewall { '501 accept incoming rabbitmq':
       source      => $extra_net,
-      destination => $rabbitmq_ip,
+      destination => $dest,
       proto       => 'tcp',
       dport       => 5672,
+      action      => 'accept',
+    }
+    firewall { '503 accept incoming rabbitmq':
+      source      => $extra_net,
+      destination => $dest,
+      proto       => 'tcp',
+      dport       => 25672,
       action      => 'accept',
     }
   }
