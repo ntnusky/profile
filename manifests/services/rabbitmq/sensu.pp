728ae6ecbad69fdbb02d2bc422e1fa740d89e204
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2018-09-05 15:26:35 +0200

    Set rabbitmq policy for ha queues in sensu if the rabbits are clustered

--- manifests/services/rabbitmq/sensu.pp
+++ manifests/services/rabbitmq/sensu.pp
@@ -2,19 +2,31 @@
 class profile::services::rabbitmq::sensu {
 
   $sensurabbitpass = hiera('profile::sensu::rabbit_password')
+  $rabbithosts = hiera('profile::rabbitmq::servers', false)
 
   rabbitmq_user { 'sensu':
     admin    => false,
     password => $sensurabbitpass,
     provider => 'rabbitmqctl',
-  } ->
-  rabbitmq_vhost { '/sensu':
+  }
+  -> rabbitmq_vhost { '/sensu':
     ensure => 'present',
-  } ->
-  rabbitmq_user_permissions { 'sensu@/sensu':
+  }
+  -> rabbitmq_user_permissions { 'sensu@/sensu':
     configure_permission => '.*',
     write_permission     => '.*',
     read_permission      => '.*',
     provider             => 'rabbitmqctl',
+  }
+
+  if ( $rabbithosts ) {
+    rabbitmq_policy { 'ha-sensu@/sensu':
+      pattern    => '^(results$|keepalives$)',
+      definition => {
+        'ha-mode'      => 'all',
+        'ha-sync-mode' => 'automatic',
+      },
+      after      => Rabbitmq_vhost['/sensu']
+    }
   }
 }
