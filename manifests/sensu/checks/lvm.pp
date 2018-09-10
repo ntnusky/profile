1aeed24df2c9cc1520646e1f0469099c0d0db594
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2018-09-10 14:10:24 +0200

    FNUTT

--- manifests/sensu/checks/lvm.pp
+++ manifests/sensu/checks/lvm.pp
@@ -1,7 +1,7 @@
 # Sensu checks for LVM
 class profile::sensu::checks::lvm {
   sensu::check { 'lvm-vg-usage':
-    command     => 'sudo check-vg-usage.rb -I ":::lvm.vg:::" -w ":::lvm.warn|85:::" -c ":::lvm.crit|95:::',
+    command     => 'sudo check-vg-usage.rb -I ":::lvm.vg:::" -w ":::lvm.warn|85:::" -c ":::lvm.crit|95:::"',
     interval    => 300,
     standalone  => false,
     subscribers => [ 'lvm-checks' ],
