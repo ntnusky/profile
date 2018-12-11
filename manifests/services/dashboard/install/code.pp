5d8c08a6103b4e29ad743d0e9e4106a62ef51583
Eigil Obrestad <eigil-git@obrestad.org>
2018-12-11 16:46:36 +0100

    Allow specifying branch of shiftleader

--- manifests/services/dashboard/install/code.pp
+++ manifests/services/dashboard/install/code.pp
@@ -1,9 +1,14 @@
 # Installs the dashboard
 class profile::services::dashboard::install::code {
+  $revision = lookup('profile::dashboard::revision', {
+    'value_type' => 'String',
+    'default_value' => 'master',
+  })
+
   vcsrepo { '/opt/shiftleader':
     ensure   => latest,
     provider => git,
     source   => 'https://github.com/ntnusky/shiftleader.git',
-    revision => 'master',
+    revision => $revision,
   }
 }
