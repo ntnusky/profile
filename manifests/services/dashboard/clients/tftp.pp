b9879f01a36deda73f588fc5bcc5869f15b942f3
Eigil Obrestad <eigil-git@obrestad.org>
2018-12-14 10:17:59 +0100

    Add script to download and provision tftp images

--- manifests/services/dashboard/clients/tftp.pp
+++ manifests/services/dashboard/clients/tftp.pp
@@ -2,9 +2,30 @@
 class profile::services::dashboard::clients::tftp {
   require ::profile::services::dashboard::install
 
+  $cert = lookup('profile::haproxy::management::webcert', {
+    'default_value' => false,
+  })
+  $slname = lookup('profile::dashboard::name', String)
+  $tftpdir = lookup('profile::tftp::root', String, 'first', '/var/lib/tftpboot/')
+
+  if($cert) {
+    $proto = "https"
+  } else {
+    $proto = "http"
+  }
+
+  $slurl="${proto}://${slname}"
+
   cron { 'Dashboard-client tftp':
     command => "/opt/shiftleader/manage.py create_tftp",
     user    => 'root',
     minute  => '*',
+  }
+
+  file { '/usr/local/bin/tftpkernels.sh':
+    ensure  => file,
+    owner   => 'root',
+    mode    => '0755',
+    content => template('profile/tftpkernels.erb'),
   }
 }
