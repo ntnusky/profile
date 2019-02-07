5bffe200fba9e9a7a077bd211b7c6f082690e858
Eigil Obrestad <eigil-git@obrestad.org>
2019-02-07 11:12:34 +0100

    Use defines to configure firewall for mysql

--- manifests/services/mysql/firewall/balancer.pp
+++ manifests/services/mysql/firewall/balancer.pp
@@ -1,33 +1,7 @@
 #Configures the firewall for the mysql loadbalancers
 class profile::services::mysql::firewall::balancer {
-  require ::profile::baseconfig::firewall
-
-  $infrav4 = lookup('profile::networking::infrastructure::ipv4::prefixes', {
-    'value_type' => Array[Stdlib::IP::Address::V4::CIDR],
-    'merge'      => 'unique',
-  })
-  $infrav6 = lookup('profile::networking::infrastructure::ipv6::prefixes', {
-    'value_type'    => Array[Stdlib::IP::Address::V6::CIDR],
-    'merge'         => 'unique',
-    'default_value' => [],
-  })
-
-  $infrav4.each | $net | {
-    firewall { "071 Accept incoming MySQL requests from ${net}":
-      source => $net,
-      proto  => 'tcp',
-      dport  => 3306,
-      action => 'accept',
-    }
-  }
-
-  $infrav6.each | $net | {
-    firewall { "071 Accept incoming MySQL requests from ${net}":
-      source   => $net,
-      proto    => 'tcp',
-      dport    => 3306,
-      action   => 'accept',
-      provider => 'ip6tables',
-    }
+  ::profile::baseconfig::firewall::service::infra { 'MYSQL':
+    protocol => 'tcp',
+    port     => 3306,
   }
 }
--- manifests/services/mysql/firewall/server.pp
+++ manifests/services/mysql/firewall/server.pp
@@ -1,63 +1,15 @@
 #Configures the firewall for the mysql servers
 class profile::services::mysql::firewall::server {
-  require ::profile::baseconfig::firewall
-
-  $infrav4 = lookup('profile::networking::infrastructure::ipv4::prefixes', {
-    'value_type' => Array[Stdlib::IP::Address::V4::CIDR],
-    'merge'      => 'unique',
-  })
-  $infrav6 = lookup('profile::networking::infrastructure::ipv6::prefixes', {
-    'value_type'    => Array[Stdlib::IP::Address::V6::CIDR],
-    'merge'         => 'unique',
-    'default_value' => [],
-  })
-
-  $infrav4.each | $net | {
-    firewall { "070 Accept incoming MySQL requests from ${net}":
-      proto  => 'tcp',
-      dport  => 3306,
-      action => 'accept',
-      source => $net,
-    }
-
-    firewall { "071 Accept incoming MySQL galera from ${net}":
-      proto  => 'tcp',
-      dport  => [4567, 4568, 4444, 9000],
-      action => 'accept',
-      source => $net,
-    }
-
-    firewall { "072 Accept incoming MySQL galera from ${net}":
-      proto  => 'udp',
-      dport  => 4567,
-      action => 'accept',
-      source => $net,
-    }
+  ::profile::baseconfig::firewall::service::infra { 'MYSQL':
+    protocol => 'tcp',
+    port     => 3306,
   }
-
-  $infrav6.each | $net | {
-    firewall { "070 Accept incoming MySQL requests from ${net}":
-      proto    => 'tcp',
-      dport    => 3306,
-      action   => 'accept',
-      source   => $net,
-      provider => 'ip6tables',
-    }
-
-    firewall { "071 Accept incoming MySQL galera from ${net}":
-      proto    => 'tcp',
-      dport    => [4567, 4568, 4444, 9000],
-      action   => 'accept',
-      source   => $net,
-      provider => 'ip6tables',
-    }
-
-    firewall { "072 Accept incoming MySQL galera from ${net}":
-      proto    => 'udp',
-      dport    => 4567,
-      action   => 'accept',
-      source   => $net,
-      provider => 'ip6tables',
-    }
+  ::profile::baseconfig::firewall::service::infra { 'MYSQL TCP Replication':
+    protocol => 'tcp',
+    port     => [4567, 4568, 4444, 9000],
+  }
+  ::profile::baseconfig::firewall::service::infra { 'MYSQL UDP Replication':
+    protocol => 'udp',
+    port     => 4567,
   }
 }
