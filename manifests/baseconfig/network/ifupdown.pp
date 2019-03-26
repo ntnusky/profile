36eacc5e9088ce8a7dfe102978290cfff5f99cc7
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2019-03-26 15:26:20 +0100

    Static IPv4 for ifupdown

--- manifests/baseconfig/network/ifupdown.pp
+++ manifests/baseconfig/network/ifupdown.pp
@@ -1,10 +1,39 @@
 # Configure networking with ifupdown
 class profile::baseconfig::network::ifupdown (Hash $nics) {
+  $dns_servers = lookup('profile::dns::nameservers', {
+    'default_value' => undef,
+  })
+  $dns_search = lookup('profile::dns::searchdomain', {
+    'default_value' => undef
+  })
+
   $nics.each | $nic, $params | {
     $method = $params['ipv4']['method']
     if($method == 'dhcp') {
       network::interface { $nic:
         enable_dhcp => true,
+      }
+    }
+    else {
+      $address = pick_default($params['ipv4']['address'], undef)
+      $netmask = pick_default($params['ipv4']['netmask'], undef)
+      $primary = pick_default($params['ipv4']['primary'], false)
+      $mtu     = pick_default($params['mtu'], undef)
+
+      if($primary) {
+        $gateway = $params['ipv4']['gateway']
+      } else {
+        $gateway = undef
+      }
+
+      network::interface { $nic:
+        method          => $method,
+        ipaddress       => $address,
+        netmask         => $netmask,
+        gateway         => $gateway,
+        dns_nameservers => $dns_servers,
+        dns_search      => $dns_search,
+        mtu             => $mtu
       }
     }
   }
