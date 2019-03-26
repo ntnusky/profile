4dacb9daab65c616fe0f156ccf1fad2884704a9a
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2019-03-26 10:41:35 +0100

    Tell reduce that it should return a hash

--- manifests/baseconfig/network/netplan.pp
+++ manifests/baseconfig/network/netplan.pp
@@ -1,6 +1,6 @@
 # Configure networking with netplan
 class profile::baseconfig::network::netplan (Hash $nics) {
-  $ethernets = $nics.reduce | $memo, $nic | {
+  $ethernets = $nics.reduce({}) | $memo, $nic | {
     $method = $nics[$nic[0]]['ipv4']['method']
     if($method = 'dhcp') {
       $memo + { $nic[0] => { 'dhcp4' => true, } }
