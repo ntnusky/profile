037ee08c9d0b17dabcb6be3c473f97261071e581
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2019-03-26 10:45:33 +0100

    The equal signs are not equal

--- manifests/baseconfig/network/netplan.pp
+++ manifests/baseconfig/network/netplan.pp
@@ -2,7 +2,7 @@
 class profile::baseconfig::network::netplan (Hash $nics) {
   $ethernets = $nics.reduce({}) | $memo, $nic | {
     $method = $nics[$nic[0]]['ipv4']['method']
-    if($method = 'dhcp') {
+    if($method == 'dhcp') {
       $memo + { $nic[0] => { 'dhcp4' => true, } }
     }
   }
