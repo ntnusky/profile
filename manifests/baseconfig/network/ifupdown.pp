4301c03304ee3907bbd1b2b0e83dd45436ab12cf
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2019-03-26 11:00:38 +0100

    DHCP on given nics with ifupdown

--- manifests/baseconfig/network/ifupdown.pp
+++ manifests/baseconfig/network/ifupdown.pp
@@ -1,3 +1,11 @@
 # Configure networking with ifupdown
-class profile::baseconfig::network::ifupdown {
+class profile::baseconfig::network::ifupdown (Hash $nics) {
+  $nics.each | $nic, $params | {
+    $method = $params['ipv4']['method']
+    if($method == 'dhcp') {
+      network::interface { $nic:
+        enable_dhcp => true,
+      }
+    }
+  }
 }
