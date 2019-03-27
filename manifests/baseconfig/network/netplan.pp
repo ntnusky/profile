d9fdfa34c70284ef0f5942a129228d56f223da01
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2019-03-27 10:56:36 +0100

    Static IPv4 support for netplan

--- manifests/baseconfig/network/netplan.pp
+++ manifests/baseconfig/network/netplan.pp
@@ -1,10 +1,44 @@
 # Configure networking with netplan
 class profile::baseconfig::network::netplan (Hash $nics) {
+  $dns_servers = lookup('profile::dns::nameservers', {
+    'default_value' => undef,
+  })
+  $dns_search = lookup('profile::dns::searchdomain', {
+    'default_value' => undef
+  })
+
   $ethernets = $nics.reduce({}) | $memo, $nic | {
     $method = $nics[$nic[0]]['ipv4']['method']
     if($method == 'dhcp') {
       $memo + { $nic[0] => { 'dhcp4' => true, } }
     }
+    else {
+      if($params['ipv4']['address']) {
+        $address = $params['ipv4']['address']
+        $mask = netmask_to_masklen($params['ipv4']['netmask'])
+        $cidr = "${address}/${mask}"
+      } else {
+        $cidr = undef
+      }
+
+      $primary = $params['ipv4']['primary']
+      $mtu = $params['mtu']
+
+      if($primary) {
+        $gateway = $params['ipv4']['gateway']
+      } else {
+        $gateway = undef
+      }
+
+      $memo + { $nic[0] => {
+        'addresses'   => $cidr,
+        'gateway4'    => $gateway,
+        'nameservers' => {
+          'addresses' => split($dns_servers, ' ')
+          'search'    => $dns_search,
+        },
+        'mtu'         => $params['ipv4']['mtu']
+      }
   }
 
   class { '::netplan':
