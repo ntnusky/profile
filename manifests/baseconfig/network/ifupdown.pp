0e5f0a69dae967bb75ee5f9ef3b037efaabc0fd9
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2019-03-27 09:01:05 +0100

    I think you'll get undef if you try to access a non-existing element in a hash

--- manifests/baseconfig/network/ifupdown.pp
+++ manifests/baseconfig/network/ifupdown.pp
@@ -18,12 +18,13 @@ class profile::baseconfig::network::ifupdown (Hash $nics) {
       $address = pick_default($params['ipv4']['address'], undef)
       $netmask = pick_default($params['ipv4']['netmask'], undef)
       $primary = pick_default($params['ipv4']['primary'], false)
+      $mtu = $params['mtu']
 
-      if($params['mtu']) {
-        $mtu = $params['mtu']
-      } else {
-        $mtu = undef
-      }
+      #if($params['mtu']) {
+      #  $mtu = $params['mtu']
+      #} else {
+      #  $mtu = undef
+      #}
 
       if($primary) {
         $gateway = $params['ipv4']['gateway']
