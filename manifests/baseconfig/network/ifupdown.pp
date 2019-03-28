4ef04cd29c28669bd0a6071bf997fa04769b4344
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2019-03-28 13:15:42 +0100

    Fix logic with IPv6 gateway

--- manifests/baseconfig/network/ifupdown.pp
+++ manifests/baseconfig/network/ifupdown.pp
@@ -9,6 +9,11 @@ class profile::baseconfig::network::ifupdown (Hash $nics) {
 
   $nics.each | $nic, $params | {
     $v4gateway = $params['ipv4']['gateway']
+    if ('ipv6' in $params) {
+      $v6gateway = pick(params['ipv6']['gateway'],'fe80::1')
+    } else {
+      $v6gateway = 'fe80::1'
+    }
     $method = $params['ipv4']['method']
     if($method == 'dhcp') {
       network::interface { $nic:
@@ -19,7 +24,6 @@ class profile::baseconfig::network::ifupdown (Hash $nics) {
       # These will all default to undef if not present in the hash from hiera
       $v4address = $params['ipv4']['address']
       $v4netmask = $params['ipv4']['netmask']
-      $v6gateway = pick($params['ipv6']['gateway'], 'fe80::1')
       $primary = $params['ipv4']['primary']
       $mtu = $params['mtu']
 
