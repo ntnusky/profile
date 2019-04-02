f9f9c25d97a0d3634603f48b5865d18367d44418
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2019-04-02 10:06:44 +0200

    When configuring a manual interface outside of netplan, don't add anything to the netplan hash

--- manifests/baseconfig/network/netplan.pp
+++ manifests/baseconfig/network/netplan.pp
@@ -68,6 +68,7 @@ class profile::baseconfig::network::netplan (Hash $nics) {
     }
     elsif($method == 'manual') {
       profile::baseconfig::network::netplan::manual { $nic: }
+      $memo + undef
     }
     else {
       if($nics[$nic]['ipv4']['address']) {
