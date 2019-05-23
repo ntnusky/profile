4b83eb45d23e75716fbc59ce98a28fb15354a30d
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2019-05-23 14:21:54 +0200

    Re-factoring, and make sure this is removed if not specfied

--- manifests/services/puppet/altnames.pp
+++ manifests/services/puppet/altnames.pp
@@ -1,12 +1,24 @@
 # Configures the puppet alt-names.
 class profile::services::puppet::altnames {
-  $alt_names = hiera_array('profile::puppet::altnames')
+  $alt_names = lookup('profile::puppet::altnames', {
+    'default_value' => false,
+    'value_type'    => Variant[Array[Stdlib::Fqdn], Boolean],
+  })
 
-  ini_setting { 'Puppet DNS alt':
-    ensure  => present,
-    path    => '/etc/puppetlabs/puppet/puppet.conf',
-    section => 'main',
-    setting => 'dns_alt_names',
-    value   => join($alt_names, ','),
+  if($alt_names) {
+    ini_setting { 'Puppet DNS alt':
+      ensure  => present,
+      path    => '/etc/puppetlabs/puppet/puppet.conf',
+      section => 'main',
+      setting => 'dns_alt_names',
+      value   => join($alt_names, ','),
+    }
+  } else {
+    ini_setting { 'Puppet DNS alt':
+      ensure  => absent,
+      path    => '/etc/puppetlabs/puppet/puppet.conf',
+      section => 'main',
+      setting => 'dns_alt_names',
+    }
   }
 }
