681353af6f287dafd16f1103b206a1eab558dca3
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2019-06-07 09:58:59 +0200

    Adjustments to erlang class, to support puppetlabs/apt 7.0.1

--- manifests/services/erlang.pp
+++ manifests/services/erlang.pp
@@ -4,13 +4,15 @@ class profile::services::erlang {
     'default_value' => '1:21.3.6-1',
   })
   apt::source { 'erlang':
-    comment    => '',
-    location   => 'https://packages.erlang-solutions.com/ubuntu',
-    repos      => 'contrib',
-    key        => '434975BD900CCBE4F7EE1B1ED208507CA14F4FCA',
-    key_source => 'https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc',
-    notify     => Exec['apt_update'],
-    before     => Apt::Pin['erlang'],
+    comment  => '',
+    location => 'https://packages.erlang-solutions.com/ubuntu',
+    repos    => 'contrib',
+    key      => {
+      id     => '434975BD900CCBE4F7EE1B1ED208507CA14F4FCA',
+      source => 'https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc',
+    },
+    notify   => Exec['apt_update'],
+    before   => Apt::Pin['erlang'],
   }
 
   apt::pin { 'erlang':
