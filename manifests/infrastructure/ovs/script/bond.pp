56ec77c7dd790ef2c59b3e0609af8393fef3d251
Eigil Obrestad <eigil-git@obrestad.org>
2019-06-19 19:14:19 +0200

    Too much copypasta

--- manifests/infrastructure/ovs/script/bond.pp
+++ manifests/infrastructure/ovs/script/bond.pp
@@ -2,12 +2,12 @@
 class profile::infrastructure::ovs::script::bond {
   file { '/usr/local/bin/create-vswitch-lacp.sh.sh':
     ensure => file,
-    source => 'puppet:///modules/profile/vswitch/create-vswitch-lacp.sh.sh',
+    source => 'puppet:///modules/profile/vswitch/create-vswitch-lacp.sh',
     mode   => '0555',
   }
   file { '/usr/local/bin/verify-vswitch-lacp.sh.sh':
     ensure => file,
-    source => 'puppet:///modules/profile/vswitch/verify-vswitch-lacp.sh.sh',
+    source => 'puppet:///modules/profile/vswitch/verify-vswitch-lacp.sh',
     mode   => '0555',
   }
 }
