5de2a83151a7344fd27969b9b1a47a9e603c48fe
Eigil Obrestad <eigil-git@obrestad.org>
2019-08-09 15:46:43 +0200

    When creating a bond, ensure that its interfaces are brought up

--- manifests/infrastructure/ovs/port/bond.pp
+++ manifests/infrastructure/ovs/port/bond.pp
@@ -13,4 +13,26 @@ define profile::infrastructure::ovs::port::bond (
     path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
     require => Profile::Infrastructure::Ovs::Bridge[$bridge],
   }
+
+  $distro = $facts['os']['release']['major']
+  $members.each | $member | {
+    if($distro == '18.04') {
+      file { "/etc/netplan/02-bondmember-${member}.yaml":
+        ensue   => file,
+        mode    => '0644',
+        owner   => root,
+        group   => root,
+        content => epp('profile/netplan/manual.epp', {
+          'ifname' => $member,
+        }
+        notify  => Exec['netplan_apply'],
+        require => Class['::netplan'],
+      }
+    } elsif($distro == '16.04') {
+      ::network::interface { "manual-up-${nic}":
+        interface => $member,
+        method    => $manual,
+      }
+    }
+  }
 }
