731703d684c1b57409e7975d21adaae5592ef6c3
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2019-08-20 13:11:31 +0200

    Only include domain name in a DHCP pool if it's explicitly enabled.

--- manifests/services/dhcp/pool.pp
+++ manifests/services/dhcp/pool.pp
@@ -1,16 +1,32 @@
 # Creates DHCP pool based on data from hiera
 define profile::services::dhcp::pool {
-  $id = hiera("profile::networks::${name}::ipv4::id")
-  $domain = hiera("profile::networks::${name}::domain")
-  $mask = hiera("profile::networks::${name}::ipv4::mask")
-  $gateway = hiera("profile::networks::${name}::ipv4::gateway", '')
-  $range = hiera("profile::networks::${name}::ipv4::dynamicrange", '')
+  $id = lookup("profile::networks::${name}::ipv4::id")
+  $domain = lookup("profile::networks::${name}::domain")
+  $mask = lookup("profile::networks::${name}::ipv4::mask")
+  $gateway = lookup("profile::networks::${name}::ipv4::gateway", {
+    'default_value' => '',
+    'value_type'    => Variant[Stdlib::IP::Address::V4, String],
+  })
+  $range = lookup("profile::networks::${name}::ipv4::dynamicrange", {
+    'default_value' => '',
+    'value_type'    => Variant[Array[String], String],
+  })
+  $include_name = lookup("profile::networks::${name}::dhcp::include_name", {
+    'default_value' => false,
+    'value_type'    => Boolean,
+  })
+
+  if($include_name) {
+    $domain_real = $domain
+  } else {
+    $domain_real = ''
+  }
 
   ::dhcp::pool { "${name}":
     network     => $id,
     mask        => $mask,
     range       => $range,
     gateway     => $gateway,
-    domain_name => $domain,
+    domain_name => $domain_real,
   }
 }
