245cc577e7610daeccccf535ae33cdbc4b3d3092
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2019-08-22 13:54:17 +0200

    Openstack Rocky removed the v2.0 API from keystone

--- manifests/sensu/checks/openstack/publicapi.pp
+++ manifests/sensu/checks/openstack/publicapi.pp
@@ -9,12 +9,6 @@ class profile::sensu::checks::openstack::publicapi inherits profile::sensu::chec
     standalone  => false,
     subscribers => [ 'os-public-api-checks' ],
   }
-  sensu::check { 'openstack-identity-public-api':
-    command     => "check-http.rb -u ${api}:5000/v2.0",
-    interval    => 300,
-    standalone  => false,
-    subscribers => [ 'os-public-api-checks' ],
-  }
   sensu::check { 'openstack-network-public-api':
     command     => "check-http.rb -u ${api}:9696/v2.0 --response-code 401",
     interval    => 300,
