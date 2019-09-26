f2d0c0404d3c404415a02e5708e8fa2a6545ed46
Eigil Obrestad <eigil-git@obrestad.org>
2019-09-26 13:03:04 +0200

    Lag bru

--- manifests/ovstull.pp
+++ manifests/ovstull.pp
@@ -1,5 +1,9 @@
 # Class to test some ovs issues
 class profile::ovstull {
+  vs_bridge { 'tullebru':
+    ensure => present,
+  }
+
   ::profile::infrastructure::ovs::patch { "Tullogvas":
     physical_if => 'ens9',
     vlan_id     => 1337,
