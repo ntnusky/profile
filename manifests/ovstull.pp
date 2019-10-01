ba6da163365cc869ac0a9ec33fa8959e4fb51350
Eigil Obrestad <eigil-git@obrestad.org>
2019-10-01 10:59:15 +0200

    All manual

--- manifests/ovstull.pp
+++ manifests/ovstull.pp
@@ -5,10 +5,14 @@ class profile::ovstull {
   vs_bridge { 'tullebru':
     ensure => present,
   }
-
-  ::profile::infrastructure::ovs::patch { "Tullogvas":
-    physical_if => 'ens9',
-    vlan_id     => 1337,
-    ovs_bridge  => 'tullebru',
+  vs_interface { 'ens9':
+    ensure => present,
+    bridge => 'tullebru',
   }
+
+  #::profile::infrastructure::ovs::patch { "Tullogvas":
+  #  physical_if => 'ens9',
+  #  vlan_id     => 1337,
+  #  ovs_bridge  => 'tullebru',
+  #}
 }
