09b1d3c1fb6df8bec80cac4a9a40284463cdb388
Eigil Obrestad <eigil-git@obrestad.org>
2019-10-02 09:33:37 +0200

    Step by step

--- manifests/ovstull.pp
+++ manifests/ovstull.pp
@@ -3,17 +3,14 @@ class profile::ovstull {
   require ::ntnuopenstack::repo
   require ::vswitch::ovs
 
-  vs_bridge { 'tullebru':
-    ensure => present,
-  }
-  vs_bridge { 'br-vlan-ens9':
-    ensure => present,
-  }
   vs_port { 'ens9':
     ensure => present,
     bridge => 'br-vlan-ens9',
     require => Vs_bridge['br-vlan-ens9'],
   }
+
+  profile::infrastructure::ovs::bridge { 'tullebru' : }
+  profile::infrastructure::ovs::bridge { 'br-vlan-ens9' : }
 
   #::profile::infrastructure::ovs::patch { "Tullogvas":
   #  physical_if => 'ens9',
