2495c99452098eb76510cb1a36710adedb1665bf
Eigil Obrestad <eigil-git@obrestad.org>
2019-10-02 09:28:50 +0200

    Does the manual dance work?

--- manifests/ovstull.pp
+++ manifests/ovstull.pp
@@ -6,10 +6,18 @@ class profile::ovstull {
   vs_bridge { 'tullebru':
     ensure => present,
   }
-
-  ::profile::infrastructure::ovs::patch { "Tullogvas":
-    physical_if => 'ens9',
-    vlan_id     => 1337,
-    ovs_bridge  => 'tullebru',
+  vs_bridge { 'br-vlan-ens9':
+    ensure => present,
   }
+  vs_port { 'ens9':
+    ensure => present,
+    bridge => 'br-vlan-ens9',
+    require => Vs_bridge['br-vlan-ens9'],
+  }
+
+  #::profile::infrastructure::ovs::patch { "Tullogvas":
+  #  physical_if => 'ens9',
+  #  vlan_id     => 1337,
+  #  ovs_bridge  => 'tullebru',
+  #}
 }
