9fc7f49b1b7a8a112340fef09e7572309b912ee6
Eigil Obrestad <eigil-git@obrestad.org>
2019-10-25 15:19:00 +0200

    Add monitoring for the physical interfaces connected to vswitches.

--- manifests/infrastructure/ovs/port/bond.pp
+++ manifests/infrastructure/ovs/port/bond.pp
@@ -5,17 +5,20 @@ define profile::infrastructure::ovs::port::bond (
 ) {
   require ::profile::infrastructure::ovs::script::bond
 
+  # Use the create-vswitch-lacp.sh.sh script to create the bond, and connect it
+  # to the bridge.
   $memberstring = join($members, ' ')
   $args = "${name} ${memberstring}"
-
   exec { "/usr/local/bin/create-vswitch-lacp.sh.sh ${bridge} ${args}":
     unless  => "/usr/local/bin/verify-vswitch-lacp.sh.sh ${args}",
     path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
     require => Profile::Infrastructure::Ovs::Bridge[$bridge],
   }
 
+  # Iterate through all the member ports:
   $distro = $facts['os']['release']['major']
   $members.each | $member | {
+    # Make sure the physical port is up
     if($distro == '18.04') {
       file { "/etc/netplan/02-bondmember-${member}.yaml":
         ensure  => file,
@@ -30,8 +33,20 @@ define profile::infrastructure::ovs::port::bond (
     } elsif($distro == '16.04') {
       ::network::interface { "manual-up-${member}":
         interface => $member,
-        method    => 'manual', 
+        method    => 'manual',
       }
+    }
+
+    # Add monitoring for the physical port
+    munin::plugin { "if_${member}":
+      ensure => link,
+      target => 'if_',
+      config => ['user root', 'env.speed 10000'],
+    }
+    munin::plugin { "if_err_${member}":
+      ensure => link,
+      target => 'if_err_',
+      config => ['user nobody'],
     }
   }
 }
