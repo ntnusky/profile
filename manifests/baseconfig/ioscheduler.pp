363bd74462101568e5b20d3ed8498669851e08b2
Eigil Obrestad <eigil-git@obrestad.org>
2020-07-07 11:38:17 +0200

    Let scheduler be selectable from hiera

--- manifests/baseconfig/ioscheduler.pp
+++ manifests/baseconfig/ioscheduler.pp
@@ -2,22 +2,26 @@
 # purpose is to make scheduling to SSD's more efficient. 
 class profile::baseconfig::ioscheduler {
   $ssds = lookup('profile::disk::ssds', Array[String], 'unique', [])
+  $ssdscheduler = lookup('profile::disk::ssd::scheduler', 
+      Enum['noop', 'deadline', 'cfq'], 'first', 'noop')
   $hdds = lookup('profile::disk::hdds', Array[String], 'unique', [])
+  $hddscheduler = lookup('profile::disk::hdd::scheduler', 
+      Enum['noop', 'deadline', 'cfq'], 'first', 'cfq')
 
   require ::profile::baseconfig::ioscheduler::scripts
 
   $ssds.each | $ssd | {
     exec { "set ${ssd} to SSD-scheduler":
-      command => "echo noop > /sys/block/${ssd}/queue/scheduler",
-      unless  => "verify-io-scheduler.sh ${ssd} noop",
+      command => "echo ${ssdscheduler} > /sys/block/${ssd}/queue/scheduler",
+      unless  => "verify-io-scheduler.sh ${ssd} ${ssdscheduler}",
       path    => '/usr/local/bin:/bin',
     }
   }
 
   $hdds.each | $hdd | {
     exec { "set ${hdd} to HDD-scheduler":
-      command => "echo deadline > /sys/block/${hdd}/queue/scheduler",
-      unless  => "verify-io-scheduler.sh ${hdd} deadline",
+      command => "echo ${hddscheduler} > /sys/block/${hdd}/queue/scheduler",
+      unless  => "verify-io-scheduler.sh ${hdd} ${hddscheduler}",
       path    => '/usr/local/bin:/bin',
     }
   }
