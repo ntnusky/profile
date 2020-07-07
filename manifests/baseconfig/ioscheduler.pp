bebb3328de010382bb707b20dc9ba507c76e8358
Eigil Obrestad <eigil-git@obrestad.org>
2020-07-07 11:54:56 +0200

    Tune queuelength as well

--- manifests/baseconfig/ioscheduler.pp
+++ manifests/baseconfig/ioscheduler.pp
@@ -2,26 +2,39 @@
 # purpose is to make scheduling to SSD's more efficient. 
 class profile::baseconfig::ioscheduler {
   $ssds = lookup('profile::disk::ssds', Array[String], 'unique', [])
+  $ssdqueue = lookup('profile::disk::ssd::queue', Integer, 'first', 4096) 
   $ssdscheduler = lookup('profile::disk::ssd::scheduler', 
       Enum['noop', 'deadline', 'cfq'], 'first', 'noop')
+
   $hdds = lookup('profile::disk::hdds', Array[String], 'unique', [])
+  $hddqueue = lookup('profile::disk::ssd::queue', Integer, 'first', 512) 
   $hddscheduler = lookup('profile::disk::hdd::scheduler', 
       Enum['noop', 'deadline', 'cfq'], 'first', 'cfq')
 
   require ::profile::baseconfig::ioscheduler::scripts
 
   $ssds.each | $ssd | {
-    exec { "set ${ssd} to SSD-scheduler":
+    exec { "set ${ssd} to io-scheduler ${ssdscheduler}":
       command => "echo ${ssdscheduler} > /sys/block/${ssd}/queue/scheduler",
       unless  => "verify-io-scheduler.sh ${ssd} ${ssdscheduler}",
+      path    => '/usr/local/bin:/bin',
+    }
+    exec { "set ${ssd} quelength to ${ssdqueue}":
+      command => "echo ${ssdqueue} > /sys/block/${ssd}/queue/nr_requests",
+      unless  => "verify-io-queue.sh ${ssd} ${ssdqueue}",
       path    => '/usr/local/bin:/bin',
     }
   }
 
   $hdds.each | $hdd | {
-    exec { "set ${hdd} to HDD-scheduler":
+    exec { "set ${hdd} to io-scheduler ${hddscheduler}":
       command => "echo ${hddscheduler} > /sys/block/${hdd}/queue/scheduler",
       unless  => "verify-io-scheduler.sh ${hdd} ${hddscheduler}",
+      path    => '/usr/local/bin:/bin',
+    }
+    exec { "set ${hdd} quelength to ${hddqueue}":
+      command => "echo ${hddqueue} > /sys/block/${hdd}/queue/nr_requests",
+      unless  => "verify-io-queue.sh ${hdd} ${hddqueue}",
       path    => '/usr/local/bin:/bin',
     }
   }
