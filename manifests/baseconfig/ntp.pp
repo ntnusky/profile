2d8eea36e715f4e8d5f1b1cd6768e0748e200cc7
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-10-01 12:42:40 +0200

    Use chrony instead of ntpd on CentOS. And, use a module for timezone

--- manifests/baseconfig/ntp.pp
+++ manifests/baseconfig/ntp.pp
@@ -1,20 +1,34 @@
 # This class installs and configures NTP.
 class profile::baseconfig::ntp {
-  $ntpServers = hiera('profile::ntp::servers')
-  $tz = hiera('profile::ntp::timezone', 'Europe/Oslo')
+  $ntpservers = lookup('profile::ntp::servers')
+  $tz = lookup('profile::ntp::timezone', {
+    'value_type'    => String,
+    'default_value' => 'Europe/Oslo'
+  })
 
-  class { '::ntp':
-    servers  => $ntpServers,
-    restrict => [
-      '127.0.0.1',
-      '-6 ::1',
-      'default kod nomodify notrap nopeer noquery',
-      '-6 default kod nomodify notrap nopeer noquery',
-    ],
+  case $::operatingsystem {
+    'CentOS': {
+      class { '::chrony':
+        servers => $ntpservers
+      }
+    }
+    'Ubuntu': {
+      class { '::ntp':
+        servers  => $ntpservers,
+        restrict => [
+          '127.0.0.1',
+          '-6 ::1',
+          'default kod nomodify notrap nopeer noquery',
+          '-6 default kod nomodify notrap nopeer noquery',
+        ],
+      }
+    }
+    default: {
+      fail("Unsopperted operating system: ${::operatingsystem}")
+    }
   }
 
-  file {'/etc/localtime':
-    ensure => link,
-    target => "/usr/share/zoneinfo/${tz}"
+  class { '::timezone':
+    timezone => $tz,
   }
 }
