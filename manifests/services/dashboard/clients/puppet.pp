1ce40130a8f71fd3f618bb54386b46b39bd344cb
Eigil Obrestad <eigil-git@obrestad.org>
2020-10-23 13:59:43 +0200

    Actually install the relevant daemons

--- manifests/services/dashboard/clients/puppet.pp
+++ manifests/services/dashboard/clients/puppet.pp
@@ -2,6 +2,9 @@
 class profile::services::dashboard::clients::puppet {
   require ::profile::services::dashboard::install
 
+  include ::profile::services::dashboard::service::envdiscovery
+  include ::profile::services::dashboard::service::r10kdeploy
+
   cron { 'Dashboard-client puppet report clean':
     command => "/opt/shiftleader/manage.py puppet_report_clean",
     user    => 'root',
--- manifests/services/dashboard/service/r10kdeploy.pp
+++ manifests/services/dashboard/service/r10kdeploy.pp
@@ -1,5 +1,5 @@
 # Creates a service to run the r10k-deploy daemon
-class profile::services::dashboard::service:r10kdeploy {
+class profile::services::dashboard::service::r10kdeploy {
   require ::profile::services::dashboard::install
 
   ::profile::systemd::service { 'r10k-deploy':
