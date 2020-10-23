55673aea557d5853bda74975bca51296768cf122
Eigil Obrestad <eigil-git@obrestad.org>
2020-10-23 09:47:47 +0200

    Add profile for CA-server behind the LB

---
+++ manifests/services/puppet/aio.pp
@@ -0,0 +1,16 @@
+# Installs and configures a puppetmaster that both is a CA and registers with
+# the LB. 
+class profile::services::puppet::aio {
+  include ::profile::services::dashboard::clients::puppet
+  include ::profile::services::puppet::backup::ca
+  include ::profile::services::puppet::ca::certclean
+  include ::profile::services::puppet::server::config
+  include ::profile::services::puppet::server::firewall
+  include ::profile::services::puppet::server::hiera
+  include ::profile::services::puppet::server::haproxy::backend
+  include ::profile::services::puppet::server::install
+  include ::profile::services::puppet::server::service
+
+  Class['profile::services::puppet::server::install']
+  -> Class['profile::services::puppet::server::config']
+}
