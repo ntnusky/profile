4b18a3fc71b8d2089745735187984a2eb286d771
Eigil Obrestad <eigil-git@obrestad.org>
2020-12-04 12:17:31 +0100

    Add new ceph-plugins to monitor per-osd-class info

--- manifests/monitoring/munin/plugin/ceph.pp
+++ manifests/monitoring/munin/plugin/ceph.pp
@@ -3,47 +3,29 @@
 class profile::monitoring::munin::plugin::ceph {
   require ::profile::monitoring::munin::plugin::ceph::base
 
-  # Add general ceph-related graphs.
-  munin::plugin { 'ceph_objects':
-    ensure => present,
-    source => 'puppet:///modules/profile/muninplugins/ceph_objects',
-    config => ['user root'],
-  }
-  munin::plugin { 'ceph_osd':
-    ensure => present,
-    source => 'puppet:///modules/profile/muninplugins/ceph_osd',
-    config => ['user root'],
-  }
-  munin::plugin { 'ceph_pg':
-    ensure => present,
-    source => 'puppet:///modules/profile/muninplugins/ceph_pg',
-    config => ['user root'],
-  }
-  munin::plugin { 'ceph_storage':
-    ensure => present,
-    source => 'puppet:///modules/profile/muninplugins/ceph_storage',
-    config => ['user root'],
-  }
-  munin::plugin { 'ceph_total':
-    ensure => present,
-    source => 'puppet:///modules/profile/muninplugins/ceph_total',
-    config => ['user root'],
+  $plugins = [
+    'ceph_activity_iops',
+    'ceph_activity_traffic',
+    'ceph_class_osd',
+    'ceph_class_pg',
+    'ceph_class_storage',
+    'ceph_objects',
+    'ceph_osd',
+    'ceph_pg',
+    'ceph_storage',
+    'ceph_total',
+  ]
+
+  $plugins.each | $plugin | {
+    munin::plugin { $plugin:
+      ensure => present,
+      source => "puppet:///modules/profile/muninplugins/${plugin}",
+      config => ['user root'],
+    }
   }
 
-  # These ceph activity-plugins should replace the ceph-collector(s).sh scripts
-  # currently being used.
-  munin::plugin { 'ceph_activity_iops':
-    ensure => present,
-    source => 'puppet:///modules/profile/muninplugins/ceph_activity_iops',
-    config => ['user root'],
-  }
-  munin::plugin { 'ceph_activity_traffic':
-    ensure => present,
-    source => 'puppet:///modules/profile/muninplugins/ceph_activity_traffic',
-    config => ['user root'],
-  }
-
-  # Add monitoring for specific pools with the ceph-collector(s).sh scripts
+  # This class is basicly for cleaning up old ceph-collector plugins and
+  # collectors. 
   $pools = ['rbd', 'volumes', 'images']
   $pools.each | $pool | {
     ::profile::ceph::monitoring::pool { $pool: }
