398220f57e6981de079253ee2258de0ae9031944
Eigil Obrestad <eigil-git@obrestad.org>
2020-12-05 18:48:09 +0100

    Add plugins for multiple GPUS

--- manifests/monitoring/munin/plugin/vgpu.pp
+++ manifests/monitoring/munin/plugin/vgpu.pp
@@ -2,6 +2,8 @@
 class profile::monitoring::munin::plugin::vgpu {
   require ::profile::monitoring::munin::plugin::vgpu::lib
 
+  $gpus = lookup('nova::compute::vgpu::vgpu_types_device_addresses_mapping')
+
   $plugins = [
     'vgpu_memory',
     'vgpu_util_decoder',
@@ -11,10 +13,29 @@ class profile::monitoring::munin::plugin::vgpu {
   ]
 
   $plugins.each | $plugin | {
-    munin::plugin { $plugin:
+    file { "/usr/share/munin/plugins/${plugin}":
       ensure => present,
+      mode   => '0755',
+      owner  => root,
+      group  => root,
       source => "puppet:///modules/profile/muninplugins/${plugin}",
-      config => ['user nova'],
+    }
+  }
+
+  $gpus.each | $vgpu_type, $addresses | {
+    $addresses.each | $address | {
+      $plugins.each | $plugin | {
+        munin::plugin { "${plugin}-${address}":
+          ensure  => link,
+          target  => $plugin,
+          require => File["/usr/share/munin/plugins/${plugin}"],
+          config  => [
+            'user nova',
+            "env.GPU ${address}",
+            "env.VGPUTYPE ${vgpu_type}",
+          ],
+        }
+      }
     }
   }
 }
