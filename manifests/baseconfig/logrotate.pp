fc96a6a4d0471e46d83df11bfff551077db87213
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2021-05-28 10:58:43 +0200

    Attempt to ONLY add the compress default

--- manifests/baseconfig/logrotate.pp
+++ manifests/baseconfig/logrotate.pp
@@ -1,6 +1,15 @@
 # Class to set defaults for logrotate
 class profile::baseconfig::logrotate {
-  logrotate::conf { '/etc/logrotate.conf':
-    compress => true,
+  #logrotate::conf { '/etc/logrotate.conf':
+  #  compress => true,
+  #}
+
+  class { '::logrotate':
+    create_base_rules  => false,
+    manage_cron_daily  => false,
+    manage_cron_hourly => false,
+    config             => {
+      compress => true,
+    }
   }
 }
