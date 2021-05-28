0237eaedd5fc54e49d91e3975d4592eaab6c80bf
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2021-05-28 12:39:58 +0200

    This is the way. I think

--- manifests/baseconfig/logrotate.pp
+++ manifests/baseconfig/logrotate.pp
@@ -1,11 +1,6 @@
 # Class to set defaults for logrotate
 class profile::baseconfig::logrotate {
-  #logrotate::conf { '/etc/logrotate.conf':
-  #  compress => true,
-  #}
-
   class { '::logrotate':
-    create_base_rules  => false,
     config             => {
       compress => true,
     }
