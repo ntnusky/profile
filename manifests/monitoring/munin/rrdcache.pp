726122d2900fc924a299f3ea0088129628a48d86
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2021-06-10 10:47:02 +0200

    Set unix permissions on rrdcached socket

--- manifests/monitoring/munin/rrdcache.pp
+++ manifests/monitoring/munin/rrdcache.pp
@@ -1,8 +1,9 @@
 # Install the RRD Cache Daemon and perl bindings
 class profile::monitoring::munin::rrdcache {
   class { 'rrd::cache':
-    uid => 'munin',
-    gid => 'www-data',
+    uid         => 'munin',
+    gid         => 'www-data',
+    socket_mode => '755',
   }
   include rrd::bindings::perl
 }
