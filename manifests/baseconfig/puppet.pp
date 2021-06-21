cc2ec6b6f453b599654f108c5785727e25942c9d
Eigil Obrestad <eigil-git@obrestad.org>
2021-06-21 10:24:46 +0200

    Apparantly the puppet_agent package dont want to configure all of the puppet-agent...

--- manifests/baseconfig/puppet.pp
+++ manifests/baseconfig/puppet.pp
@@ -17,12 +17,34 @@ class profile::baseconfig::puppet {
   $config = [
     {'section' => 'agent', 'setting' => 'environment', 'value' => $environment},
     {'section' => 'agent', 'setting' => 'runinterval', 'value' => $runinterval},
-    {'section' => 'agent', 'setting' => 'server', 'value' => $puppetserver},
-    {'section' => 'agent', 'setting' => 'ca_server', 'value' => $caserver},
   ]
 
   class { 'puppet_agent':
     config     => $config,
     collection => $collection,
+  }
+
+  # Apparantly the puppet_agent class refuses to configure arbritary parameters,
+  # so we need to fix the rest ourselves...
+  $agentconfigfile = '/etc/puppetlabs/puppet/puppet.conf'
+  $agentpackage = 'puppet-agent'
+
+  ini_setting { 'Puppet server':
+    ensure  => present,
+    path    => $agentconfigfile,
+    section => 'agent',
+    setting => 'server',
+    value   => $puppetserver,
+    notify  => Service['puppet'],
+    require => Package[$agentpackage],
+  }
+  ini_setting { 'Puppet caserver':
+    ensure  => present,
+    path    => $agentconfigfile,
+    section => 'agent',
+    setting => 'ca_server',
+    value   => $caserver,
+    notify  => Service['puppet'],
+    require => Package[$agentpackage],
   }
 }
