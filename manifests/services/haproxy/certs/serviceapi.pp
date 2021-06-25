4f6f228c16f98e589254736bbf86ac487090ad1e
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2021-06-25 09:24:36 +0200

    Modernize haproxy files, and refactor it a bit

--- manifests/services/haproxy/certs/serviceapi.pp
+++ manifests/services/haproxy/certs/serviceapi.pp
@@ -1,8 +1,14 @@
 # Install the cacertificate for the services-api
 class profile::services::haproxy::certs::serviceapi {
-  $certificate = hiera('profile::haproxy::services::apicert', false)
-  $certfile = hiera('profile::haproxy::services::apicert::certfile',
-                    '/etc/ssl/private/haproxy.servicesapi.pem')
+
+  $certificate = lookup('profile::haproxy::services::apicert', {
+    'default_value' => false,
+    'value_type'    => Variant[Boolean, String],
+  })
+  $certfile = lookup('profile::haproxy::services::apicert::certfile', {
+    'default_value' => '/etc/ssl/private/haproxy.servicesapi.pem'
+    'value_type'    => Stdlib::Unixpath,
+  })
 
   if ($certificate) {
     file { $certfile:
