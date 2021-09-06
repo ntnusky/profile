24b874d42a6ba88937751cb45db2dc20dba5410b
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2021-09-06 11:00:04 +0200

    We need this su config, and this clever module does not merge config options...

--- manifests/baseconfig/logrotate.pp
+++ manifests/baseconfig/logrotate.pp
@@ -2,5 +2,8 @@
 class profile::baseconfig::logrotate {
   logrotate::conf { '/etc/logrotate.conf':
     compress => true,
+    su       => $::logrotate::params::default_su,
+    su_user  => $::logrotate::params::default_su_user,
+    su_group => $::logrotate::params::default_su_group,
   }
 }
