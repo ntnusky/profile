8f09eb323cd550243477299143a046c64f9e3458
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2021-09-06 12:36:17 +0200

    Maybe this would to the trick

--- manifests/baseconfig/logrotate.pp
+++ manifests/baseconfig/logrotate.pp
@@ -1,9 +1,9 @@
 # Class to set defaults for logrotate
 class profile::baseconfig::logrotate {
+
+  include ::logrotate::params
+
   logrotate::conf { '/etc/logrotate.conf':
     compress => true,
-    su       => $logrotate::params::default_su,
-    su_user  => $logrotate::params::default_su_user,
-    su_group => $logrotate::params::default_su_group,
   }
 }
