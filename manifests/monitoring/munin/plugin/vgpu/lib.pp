e08e92c4726c05490a64836770ed2bf5d7d13e71
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2021-11-15 15:50:19 +0100

    Support both CentOS and Ubuntu in our vGPU monitoring

--- manifests/monitoring/munin/plugin/vgpu/lib.pp
+++ manifests/monitoring/munin/plugin/vgpu/lib.pp
@@ -1,6 +1,11 @@
 # Install a simple python-lib for the vgpu munin plugins
 class profile::monitoring::munin::plugin::vgpu::lib {
-  file { '/usr/lib/python3.6/site-packages/munin_vgpu.py':
+  $pyvers = $facts['operatingsystem'] ? {
+    'CentOS' => 'python3.6',
+    'Ubuntu' => 'python3.8'
+  }
+
+  file { "/usr/lib/${pyvers}/site-packages/munin_vgpu.py":
     ensure => present,
     owner  => 'root',
     group  => 'root',
