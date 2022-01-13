129d0d542bf07c7d1b5543b2403f656d74cccca7
Eigil Obrestad <eigil-git@obrestad.org>
2022-01-13 09:07:17 +0100

    There was a define for that!

--- manifests/baseconfig/network/socketbuffer.pp
+++ manifests/baseconfig/network/socketbuffer.pp
@@ -1,35 +1,16 @@
 # Configure systemd-networkd.socket to avoid this bug:
 #   https://github.com/systemd/systemd/issues/14417
 class profile::baseconfig::network::socketbuffer {
-  include ::profile::systemd::reload
-
-  file { '/etc/systemd/system/systemd-networkd.socket.d/':
-    ensure => directory,
-    mode   => '0755',
-    owner  => root,
-    group  => root,
-  }
-
-  file { '/etc/systemd/system/systemd-networkd.socket.d/buffers.conf':
-    ensure  => file,
-    mode    => '0644',
-    owner   => root,
-    group   => root,
-    notify  => [
-      Exec['systemd-reload'],
-      Service['systemd-networkd.socket'],
-    ],
-    require => File['/etc/systemd/system/systemd-networkd.socket.d/'],
+  systemd::dropin_file { 'buffers.conf':
+    unit    => 'systemd-networkd.socket.d',
     source  => 'puppet:///modules/profile/systemd/networkd.socket.buffers.conf',
+    notify  => Service['systemd-networkd.socket'], 
   }
 
   service { 'systemd-networkd.socket':
     ensure   => 'running',
     notify   => Exec['netplan_apply']
     provider => 'systemd',
-    require  => [
-      File['/etc/systemd/system/systemd-networkd.socket.d/buffers.conf'],
-      Exec['systemd-reload'],
-    ],
+    require  => Systemd::Dropin_file['buffers.conf'],
   }
 }
