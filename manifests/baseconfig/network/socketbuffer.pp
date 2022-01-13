2c4f124054bc618d7239721ad513e7841f88c9f3
Eigil Obrestad <eigil-git@obrestad.org>
2022-01-13 09:10:31 +0100

    Syntax-errors...

--- manifests/baseconfig/network/socketbuffer.pp
+++ manifests/baseconfig/network/socketbuffer.pp
@@ -2,14 +2,14 @@
 #   https://github.com/systemd/systemd/issues/14417
 class profile::baseconfig::network::socketbuffer {
   systemd::dropin_file { 'buffers.conf':
-    unit    => 'systemd-networkd.socket.d',
-    source  => 'puppet:///modules/profile/systemd/networkd.socket.buffers.conf',
-    notify  => Service['systemd-networkd.socket'], 
+    unit   => 'systemd-networkd.socket.d',
+    source => 'puppet:///modules/profile/systemd/networkd.socket.buffers.conf',
+    notify => Service['systemd-networkd.socket'],
   }
 
   service { 'systemd-networkd.socket':
     ensure   => 'running',
-    notify   => Exec['netplan_apply']
+    notify   => Exec['netplan_apply'],
     provider => 'systemd',
     require  => Systemd::Dropin_file['buffers.conf'],
   }
