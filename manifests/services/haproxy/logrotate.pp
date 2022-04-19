08f520c932a3fa448420a551ca9bb909741fe79b
Eigil Obrestad <eigil-git@obrestad.org>
2022-04-19 13:27:56 +0200

    more logrotate config

--- manifests/services/haproxy/logrotate.pp
+++ manifests/services/haproxy/logrotate.pp
@@ -1,8 +1,13 @@
 # Configures hourly rotation of haproxy logs.
 class profile::services::haproxy::logrotate {
   logrotate::rule { 'haproxylogs':
-    path         => ['/var/log/haproxy.log'],
-    rotate       => 504,
-    rotate_every => 'hour',
+    path          => ['/var/log/haproxy.log'],
+    compress      => true,
+    delaycompress => true,
+    ifempty       => false,
+    missingok     => true,
+    postrotate    => '/usr/lib/rsyslog/rsyslog-rotate'
+    rotate        => 504,
+    rotate_every  => 'hour',
   }
 }
