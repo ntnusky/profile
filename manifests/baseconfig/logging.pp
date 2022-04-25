cc0d7e7c1328d00ea40bdc137ddf1de173ed8002
Eigil Obrestad <eigil-git@obrestad.org>
2022-04-25 10:36:55 +0200

    Use modules where possible

--- manifests/baseconfig/logging.pp
+++ manifests/baseconfig/logging.pp
@@ -9,6 +9,17 @@ class profile::baseconfig::logging {
   if $loggservers{
     # Install and configure filebeat.
     class { 'filebeat':
+      enable_conf_modules => true,
+      modules => [
+        'apache',
+        'haproxy',
+        'iptables',
+        'mysql',
+        'postgresql',
+        'rabbitmq',
+        'redis',
+        'system',
+      ],
       outputs => {
         'logstash' => {
           'hosts'       => $loggservers,
@@ -17,67 +28,67 @@ class profile::baseconfig::logging {
       },
     }
 
-    # Determine the typical system log-files
-    case $::operatingsystem {
-      'CentOS': {
-        $logfiles = [
-          '/var/log/boot.log',
-          '/var/log/cron',
-          '/var/log/dmesg',
-          '/var/log/maillog',
-          '/var/log/messages',
-          '/var/log/secure',
-          '/var/log/yum.log',
-          '/var/log/audit/audit.log',
-          '/var/log/tuned/tuned.log',
-        ]
-      }
-      'Ubuntu': {
-        $logfiles = [
-          '/var/log/alternatives.log',
-          '/var/log/auth.log',
-          '/var/log/dpkg.log',
-          '/var/log/kern.log',
-          '/var/log/mail.log',
-          '/var/log/syslog',
-          '/var/log/unattended-upgrades/unattended-upgrades.log',
-        ]
+    ## Determine the typical system log-files
+    #case $::operatingsystem {
+    #  'CentOS': {
+    #    $logfiles = [
+    #      '/var/log/boot.log',
+    #      '/var/log/cron',
+    #      '/var/log/dmesg',
+    #      '/var/log/maillog',
+    #      '/var/log/messages',
+    #      '/var/log/secure',
+    #      '/var/log/yum.log',
+    #      '/var/log/audit/audit.log',
+    #      '/var/log/tuned/tuned.log',
+    #    ]
+    #  }
+    #  'Ubuntu': {
+    #    $logfiles = [
+    #      '/var/log/alternatives.log',
+    #      '/var/log/auth.log',
+    #      '/var/log/dpkg.log',
+    #      '/var/log/kern.log',
+    #      '/var/log/mail.log',
+    #      '/var/log/syslog',
+    #      '/var/log/unattended-upgrades/unattended-upgrades.log',
+    #    ]
 
-        filebeat::input { 'apt-history':
-          paths    => [
-            '/var/log/apt/history.log',
-          ],
-          doc_type => 'log',
-          multiline => {
-            'pattern' => 'Start-Date',
-            'negate'  => 'true',
-            'match'   => 'after',
-            'timeout' => '30s',
-          },
-        }
+    #    filebeat::input { 'apt-history':
+    #      paths    => [
+    #        '/var/log/apt/history.log',
+    #      ],
+    #      doc_type => 'log',
+    #      multiline => {
+    #        'pattern' => 'Start-Date',
+    #        'negate'  => 'true',
+    #        'match'   => 'after',
+    #        'timeout' => '30s',
+    #      },
+    #    }
 
-        filebeat::input { 'apt-term':
-          paths    => [
-            '/var/log/apt/term.log',
-          ],
-          doc_type => 'log',
-          multiline => {
-            'pattern' => 'Log\ started',
-            'negate'  => 'true',
-            'match'   => 'after',
-            'timeout' => '30s',
-          },
-        }
-      }
-      default: {
-        fail("Unsopperted operating system: ${::operatingsystem}")
-      }
-    }
+    #    filebeat::input { 'apt-term':
+    #      paths    => [
+    #        '/var/log/apt/term.log',
+    #      ],
+    #      doc_type => 'log',
+    #      multiline => {
+    #        'pattern' => 'Log\ started',
+    #        'negate'  => 'true',
+    #        'match'   => 'after',
+    #        'timeout' => '30s',
+    #      },
+    #    }
+    #  }
+    #  default: {
+    #    fail("Unsopperted operating system: ${::operatingsystem}")
+    #  }
+    #}
 
-    # Input typical system-related logfiles
-    filebeat::input { 'syslogs':
-      paths    => $logfiles,
-      doc_type => 'log',
-    }
+    ## Input typical system-related logfiles
+    #filebeat::input { 'syslogs':
+    #  paths    => $logfiles,
+    #  doc_type => 'log',
+    #}
   }
 }
