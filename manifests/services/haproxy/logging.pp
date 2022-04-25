12edf30eee7aacbcaf8bb27907bbea677616f2c8
Eigil Obrestad <eigil-git@obrestad.org>
2022-04-25 14:33:03 +0200

    Syntax-fixes

--- manifests/services/haproxy/logging.pp
+++ manifests/services/haproxy/logging.pp
@@ -2,11 +2,11 @@
 class profile::services::haproxy::logging {
   profile::utilities::logging::module { 'haproxy' :
     content => [{
-      module => 'haproxy',
-      log    => {
-        enabled   => true,
-        var.input => 'file',
-        var.paths => [
+      'module' => 'haproxy',
+      'log'    => {
+        'enabled'   => true,
+        'var.input' => 'file',
+        'var.paths' => [
           '/var/log/haproxy.log',
         ],
       },
