f5b4192d694b9c0cf39828691e8b5246f4258666
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2022-11-09 13:03:07 +0100

    Add opt-outs for every part of the config

--- manifests/utilities/bmc.pp
+++ manifests/utilities/bmc.pp
@@ -7,8 +7,25 @@ class profile::utilities::bmc {
   $certificate = lookup('profile::bmc::cert', String)
   $private_key = lookup('profile::bmc::privatekey', String)
   $ntp_servers = lookup('profile::ntp::servers', Array[Stdlib::Fqdn])
-  # TODO: Dette har vi ikke brukt for når bmc_network ikke virker
-  #$dns_domain_name = lookup('profile::networks::bmc::domain', Stdlib::Fqdn)
+  # TODO: Dette har vi ikke brukt for når bmc_network ikke virker for alt
+  $dns_domain_name = lookup('profile::networks::bmc::domain', Stdlib::Fqdn)
+
+  $manage_users = lookup('profile::bmc::manage::users', {
+    'value_type'    => Boolean,
+    'default_value' => true,
+  })
+  $manage_certs = lookup('profile::bmc::manage::certs', {
+    'value_type'    => Boolean,
+    'default_value' => true,
+  })
+  $manage_ntp = lookup('profile::bmc::manage::ntp', {
+    'value_type'    => Boolean,
+    'default_value' => true,
+  })
+  $manage_network = lookup('profile::bmc::manage::network', {
+    'value_type'    => Boolean,
+    'default_value' => true,
+  })
 
   $ca_cert_path = '/etc/ssl/private/idrac_ca.pem'
   $certificate_path = '/etc/ssl/private/idrac.pem'
@@ -20,69 +37,77 @@ class profile::utilities::bmc {
     'bmc_server_host' => $bmc_ip,
   }
 
-  bmc_user { 'root':
-    password => $root_password,
-    callin   => true,
-    ipmi     => true,
-    link     => true,
-    idrac    => 0x1ff,
+  if $manage_users {
+    bmc_user { 'root':
+      password => $root_password,
+      callin   => true,
+      ipmi     => true,
+      link     => true,
+      idrac    => 0x1ff,
+    }
   }
 
-  # TODO: Dette virker ikke med gjeldene versjon av modulen..
-  #  bmc_network { 'network_settings':
-  #    ip_source                 => 'dhcp',
-  #    ipv4_dns_from_dhcp        => true,
-  #    dns_domain_from_dhcp      => false,
-  #    dns_domain_name_from_dhcp => false,
-  #    dns_domain_name           => $dns_domain_name,
-  #    dns_bmc_name              => $::hostname,
-  #    *                         => $connection,
-  #  }
-
-  bmc_ssl { 'CA_chain':
-    certificate_file => $ca_cert_path,
-    type             => 2,
-    require          => File[$ca_cert_path],
-    *                => $connection,
-  }
-  -> bmc_ssl { 'IDRAC ssl':
-    certificate_file => $certificate_path,
-    certificate_key  => $private_key_path,
-    require          => [
-      File[$certificate_path],
-      File[$private_key_path],
-    ],
-    *                => $connection,
-  }
-  -> bmc_service { 'reset_idrac': }
-
-  bmc_time {'ntnu-ntp':
-    ntp_enable  => true,
-    ntp_servers => $ntp_servers,
-    timezone    => 'Europe/Oslo',
+  if $manage_ntp {
+    bmc_time {'ntnu-ntp':
+      ntp_enable  => true,
+      ntp_servers => $ntp_servers,
+      timezone    => 'Europe/Oslo',
+    }
   }
 
-  file { $ca_cert_path:
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0600',
-    content => $ca_cert,
+  # TODO: Dette virker ikke på iDRAC9 nyere enn 4.40.40.0 med gjeldende versjon av modulen
+  if $manage_network {
+    bmc_network { 'network_settings':
+      ip_source                 => 'dhcp',
+      ipv4_dns_from_dhcp        => true,
+      dns_domain_from_dhcp      => false,
+      dns_domain_name_from_dhcp => false,
+      dns_domain_name           => $dns_domain_name,
+      dns_bmc_name              => $::hostname,
+      *                         => $connection,
+    }
   }
 
-  file { $certificate_path:
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0600',
-    content => $certificate,
-  }
+  if $manage_certs {
+    bmc_ssl { 'CA_chain':
+      certificate_file => $ca_cert_path,
+      type             => 2,
+      require          => File[$ca_cert_path],
+      *                => $connection,
+    }
+    -> bmc_ssl { 'IDRAC ssl':
+      certificate_file => $certificate_path,
+      certificate_key  => $private_key_path,
+      require          => [
+        File[$certificate_path],
+        File[$private_key_path],
+      ],
+      *                => $connection,
+    }
+    -> bmc_service { 'reset_idrac': }
 
-  file { $private_key_path:
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0600',
-    content => $private_key,
+    file { $ca_cert_path:
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0600',
+      content => $ca_cert,
+    }
+
+    file { $certificate_path:
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0600',
+      content => $certificate,
+    }
+
+    file { $private_key_path:
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0600',
+      content => $private_key,
+    }
   }
 }
