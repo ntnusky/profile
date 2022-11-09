39b37791d8ecad7dbc5a737c3b4a87d4cba381b3
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2022-11-09 13:58:43 +0100

    Refactoring. Added automatic opt-out for non-supported models

--- manifests/utilities/bmc.pp
+++ manifests/utilities/bmc.pp
@@ -1,15 +1,6 @@
 # Manage iDRAC settings
 class profile::utilities::bmc {
 
-  $root_password = lookup('profile::bmc::root::password', String)
-  $bmc_ip = lookup('profile::bmc::ip', Stdlib::IP::Address::V4)
-  $ca_cert = lookup('profile::bmc::ca_cert', String)
-  $certificate = lookup('profile::bmc::cert', String)
-  $private_key = lookup('profile::bmc::privatekey', String)
-  $ntp_servers = lookup('profile::ntp::servers', Array[Stdlib::Fqdn])
-  # TODO: Dette har vi ikke brukt for når bmc_network ikke virker for alt
-  $dns_domain_name = lookup('profile::networks::bmc::domain', Stdlib::Fqdn)
-
   $manage_users = lookup('profile::bmc::manage::users', {
     'value_type'    => Boolean,
     'default_value' => true,
@@ -27,87 +18,32 @@ class profile::utilities::bmc {
     'default_value' => true,
   })
 
-  $ca_cert_path = '/etc/ssl/private/idrac_ca.pem'
-  $certificate_path = '/etc/ssl/private/idrac.pem'
-  $private_key_path = '/etc/ssl/private/idrac.key'
-
-  $connection = {
-    'bmc_username'    => 'root',
-    'bmc_password'    => $root_password,
-    'bmc_server_host' => $bmc_ip,
+  # Does not work for Rx1x series (iDRAC 6)
+  $manage_users_real = $::facts['dmi']['product']['name'] ? {
+    '/R[6789]10/' => false,
+    default       => $manage_users,
+  }
+  # Does not work for Rx1x series (iDRAC 6)
+  $manage_ntp_real = $::facts['dmi']['product']['name'] ? {
+    '/R[6789]10/' => false,
+    default       => $manage_ntp,
+  }
+  # Does only work for Rx2x series (iDRAC 7)
+  $manage_network_real = $::facts['dmi']['product']['name'] ? {
+    '/R[6789]20/' => $manage_network,
+    default       => false,
   }
 
-  if $manage_users {
-    bmc_user { 'root':
-      password => $root_password,
-      callin   => true,
-      ipmi     => true,
-      link     => true,
-      idrac    => 0x1ff,
-    }
+  if $manage_users_real {
+    include ::profile::utilities::bmc::users
   }
-
-  if $manage_ntp {
-    bmc_time {'ntnu-ntp':
-      ntp_enable  => true,
-      ntp_servers => $ntp_servers,
-      timezone    => 'Europe/Oslo',
-    }
-  }
-
-  # TODO: Dette virker ikke på iDRAC9 nyere enn 4.40.40.0 med gjeldende versjon av modulen
-  if $manage_network {
-    bmc_network { 'network_settings':
-      ip_source                 => 'dhcp',
-      ipv4_dns_from_dhcp        => true,
-      dns_domain_from_dhcp      => false,
-      dns_domain_name_from_dhcp => false,
-      dns_domain_name           => $dns_domain_name,
-      dns_bmc_name              => $::hostname,
-      *                         => $connection,
-    }
-  }
-
   if $manage_certs {
-    bmc_ssl { 'CA_chain':
-      certificate_file => $ca_cert_path,
-      type             => 2,
-      require          => File[$ca_cert_path],
-      *                => $connection,
-    }
-    -> bmc_ssl { 'IDRAC ssl':
-      certificate_file => $certificate_path,
-      certificate_key  => $private_key_path,
-      require          => [
-        File[$certificate_path],
-        File[$private_key_path],
-      ],
-      *                => $connection,
-    }
-    -> bmc_service { 'reset_idrac': }
-
-    file { $ca_cert_path:
-      ensure  => file,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0600',
-      content => $ca_cert,
-    }
-
-    file { $certificate_path:
-      ensure  => file,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0600',
-      content => $certificate,
-    }
-
-    file { $private_key_path:
-      ensure  => file,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0600',
-      content => $private_key,
-    }
+    include ::profile::utilities::bmc::certs
+  }
+  if $manage_ntp_real {
+    include ::profile::utilities::bmc::ntp
+  }
+  if $manage_network_real {
+    include ::profile::utilities::bmc::networking
   }
 }
