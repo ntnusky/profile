7cb4c10556de140409e09f7a7ed9ce4cd08bf5c0
Eigil Obrestad <eigil-git@obrestad.org>
2023-03-13 12:20:12 +0100

    Jammy dont need the repos

--- manifests/ceph/repo.pp
+++ manifests/ceph/repo.pp
@@ -1,8 +1,20 @@
 # Abstraction for the ceph::repo class
 class profile::ceph::repo {
+  if($::facts['os']['distro']['codename'] == 'jammy') {
+    $default = false
+  } else {
+    $default = true
+  }
 
-  class { '::ceph::repo':
-    enable_epel => false,
-    enable_sig  => true,
+  $setup_repo = lookup('profile::ceph::repo::enabled', {
+    'default_value' = $default,
+    'value_type'    = Boolean,
+  })
+
+  if ( $setup_repo ) {
+    class { '::ceph::repo':
+      enable_epel => false,
+      enable_sig  => true,
+    }
   }
 }
