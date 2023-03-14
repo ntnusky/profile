a9433731b8c1d013281d3c9a2af0b2f8d276e932
Eigil Obrestad <eigil-git@obrestad.org>
2023-03-14 15:53:28 +0100

    Revert "Yet another dependency"
    This reverts commit eb1cf06198a6fa926d411b0d2a02c4217840aa02.

--- manifests/ceph/base.pp
+++ manifests/ceph/base.pp
@@ -53,7 +53,6 @@ class profile::ceph::base {
       'ceph-volume',
       'libcephfs2',
       'librados2',
-      'libradosstriper1',
       'librbd1',
       'libsqlite3-mod-ceph',
       'python3-ceph-argparse',
