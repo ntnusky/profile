af8267155b859881f9feffabed9e0ef2da210803
Eigil Obrestad <eigil-git@obrestad.org>
2023-03-14 15:53:24 +0100

    Revert "Handle repos in the repo class"
    This reverts commit 80474939e946707a665b643a9713ed2348f8750f.

--- manifests/ceph/base.pp
+++ manifests/ceph/base.pp
@@ -36,6 +36,24 @@ class profile::ceph::base {
   # Install the ceph repos first
   require ::profile::ceph::repo
 
+  # As of 14.03.2023 the version 17.2.5 of ceph is not correctly packaged in the
+  # ubuntu-repos; so we grab cephs own repos for focal (as they do not have a
+  # jammy-one).
+  if($::facts['os']['distro']['codename'] == 'jammy') {
+    apt::key { 'ceph-focal':
+      ensure => 'present',
+      id     => '08B73419AC32B4E966C1A330E84AC2C0460F3994',
+      source => 'https://download.ceph.com/keys/release.asc',
+      before => Apt::Source['ceph-focal'],
+    }
+    ::apt::source { 'ceph-focal':
+      ensure   => 'present',
+      location => 'https://download.ceph.com/debian-quincy/',
+      release  => 'focal',
+      tag      => 'ceph',
+    }
+  }
+
   if($cluster_networks) {
     $cluster_networks_real = join($cluster_networks, ', ')
   } else {
--- manifests/ceph/repo.pp
+++ manifests/ceph/repo.pp
@@ -1,27 +1,19 @@
 # Abstraction for the ceph::repo class
 class profile::ceph::repo {
-  # As of 14.03.2023 the version 17.2.5 of ceph is not correctly packaged in the
-  # ubuntu-repos; so we grab cephs own repos for focal (as they do not have a
-  # jammy-one).
   if($::facts['os']['distro']['codename'] == 'jammy') {
-    apt::key { 'ceph':
-      ensure => 'present',
-      id     => '08B73419AC32B4E966C1A330E84AC2C0460F3994',
-      source => 'https://download.ceph.com/keys/release.asc',
-      before => Apt::Source['ceph-focal'],
-    }
-    ::apt::source { 'ceph-focal':
-      ensure   => 'present',
-      location => 'https://download.ceph.com/debian-quincy/',
-      release  => 'focal',
-      tag      => 'ceph',
-    }
-  # If not jammy; we can use the default-repos for ceph
+    $default = 'absent'
   } else {
-    class { '::ceph::repo':
-      ensure      => 'present', 
-      enable_epel => false,
-      enable_sig  => true,
-    }
+    $default = 'present'
+  }
+
+  $ensure = lookup('profile::ceph::repo::ensure', {
+    'default_value' => $default,
+    'value_type'    => String,
+  })
+
+  class { '::ceph::repo':
+    ensure      => $ensure,
+    enable_epel => false,
+    enable_sig  => true,
   }
 }
