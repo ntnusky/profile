0e06bf4fb1a1562db895e353fc5f41c352433e8a
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2023-09-22 12:32:36 +0200

    Someone on the internet is suggesting that bootstrapping an OSD needs to be allowed to read from the mgr

--- manifests/ceph/monitor.pp
+++ manifests/ceph/monitor.pp
@@ -45,6 +45,7 @@ class profile::ceph::monitor {
   }
   ceph::key { 'client.bootstrap-osd':
     secret  => $bootstrap_osd_key,
+    cap_mgr => 'allow r',
     cap_mon => 'allow profile bootstrap-osd',
     before  => Anchor['profile::ceph::monitor::end']
   }
