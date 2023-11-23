924c4810c933b6aece9e7eb0e6d1eff74f282139
Eigil Obrestad <eigil-git@obrestad.org>
2023-11-23 15:06:39 +0100

    Create SL2-database when needed.

--- manifests/services/mysql/databases.pp
+++ manifests/services/mysql/databases.pp
@@ -1,5 +1,17 @@
 # Configures mysql with the appropriate databases
 class profile::services::mysql::databases {
+  $sl_version = lookup('profile::shiftleader::major::version', {
+    'default_value' => 1,
+    'value_type'    => Integer,
+  })
+
   include ::profile::monitoring::munin::plugin::vgpu::db
-  include ::profile::services::dashboard::mysql
+
+  # Create the correct database for shiftleader.
+  # TODO: Remove the database for SL1 when all platforms are migrated
+  if($sl_version == 1) {
+    include ::profile::services::dashboard::mysql
+  } else {
+    include ::shiftleader::database
+  }
 }
