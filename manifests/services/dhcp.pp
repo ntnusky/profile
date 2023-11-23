c5631a926ee13de5d5d33ae55280bfa804a9afc9
Eigil Obrestad <eigil-git@obrestad.org>
2023-11-23 10:17:15 +0100

    Update the tftp-profile to be compatible with SL2

--- manifests/services/dhcp.pp
+++ manifests/services/dhcp.pp
@@ -4,14 +4,14 @@ class profile::services::dhcp {
   include ::profile::services::dhcp::pools
   include ::profile::services::dhcp::server
 
-  $sl2 = lookup('profile::shiftleader::major::version', {
+  $sl_version = lookup('profile::shiftleader::major::version', {
     'default_value' => 1,
     'value_type'    => Integer,
   })
 
   # If we use shiftleader1 we need to allow for external OMAPI requests from the
   # shiftleader servers.
-  if($sl2 == 1) {
+  if($sl_version == 1) {
     include ::profile::services::dhcp::firewall::omapi
 
   # If we run a newer variant of shiftleader we need to install the 
--- manifests/services/tftp.pp
+++ manifests/services/tftp.pp
@@ -6,9 +6,21 @@ class profile::services::tftp {
     'value_type'    => Stdlib::Unixpath,
   })
 
-  include ::profile::services::dashboard::clients::tftp
+  $sl_version = lookup('profile::shiftleader::major::version', {
+    'default_value' => 1,
+    'value_type'    => Integer,
+  })
+
   include ::profile::services::tftp::acl
   include ::profile::services::tftp::firewall
+
+  # If we use shiftleader1 we need to allow for external OMAPI requests from the
+  # shiftleader servers.
+  if($sl_version == 1) {
+    include ::profile::services::dashboard::clients::tftp
+  } else {
+    include ::shiftleader::worker::tftp
+  }
 
   # Install and configure the tftp server.
   class { '::tftp':
