432527d49a28a86b7cf64d685b0f9b23c7a2f7f5
Eigil Obrestad <eigil-git@obrestad.org>
2023-11-23 09:51:55 +0100

    Allow configuring using a hiera-hash instead of a trillion separate keys

--- manifests/services/dhcp/pool.pp
+++ manifests/services/dhcp/pool.pp
@@ -1,29 +1,51 @@
 # Creates DHCP pool based on data from hiera
-define profile::services::dhcp::pool {
-  $id = lookup("profile::networks::${name}::ipv4::id")
-  $domain = lookup("profile::networks::${name}::domain")
-  $mask = lookup("profile::networks::${name}::ipv4::mask")
+define profile::services::dhcp::pool (
+  Optional[String]                            $domain    = undef,
+  Optional[Stdlib::IP::Address::V4::CIDR]     $cidr    = undef,
+  Optional[Stdlib::IP::Address::V4::Nosubnet] $gateway = undef,
+  Optional[Variant[Array[String], String]]    $range   = undef,
+) {
+  # Get the correct ID/Mask from the CIDR if it is supplied.
+  if($cidr) {
+    $networkID = ip_address($cidr)
+    $mask = ip_netmask($cidr)
+
+  # If no CIDR is supplied; grab the relevant config from the old hiera-keys.
+  } else {
+    $networkID = lookup("profile::networks::${name}::ipv4::id")
+    $mask = lookup("profile::networks::${name}::ipv4::mask")
+  }
+
+  # Determine if a gateway should be configured
   $gateway = lookup("profile::networks::${name}::ipv4::gateway", {
-    'default_value' => '',
-    'value_type'    => Variant[Stdlib::IP::Address::V4, String],
+    'default_value' => $gateway,
+    'value_type'    => Optional[Stdlib::IP::Address::V4::Nosubnet],
   })
+  
+  # Determine if a range is to be configured:
   $range = lookup("profile::networks::${name}::ipv4::dynamicrange", {
-    'default_value' => '',
-    'value_type'    => Variant[Array[String], String],
-  })
-  $include_name = lookup("profile::networks::${name}::dhcp::include_name", {
-    'default_value' => false,
-    'value_type'    => Boolean,
+    'default_value' => $range,
+    'value_type'    => Optional[Variant[Array[String], String]],
   })
 
-  if($include_name) {
+  if($domain) {
     $domain_real = $domain
   } else {
-    $domain_real = ''
+    # Determine if DHCP should hand out a domain-name.
+    $include_name = lookup("profile::networks::${name}::dhcp::include_name", {
+      'default_value' => false,
+      'value_type'    => Boolean,
+    })
+
+    if ($include_name) {
+      $domain_real = lookup("profile::networks::${name}::domain")
+    } else {
+      $domain_real = undef 
+    }
   }
 
   ::dhcp::pool { "${name}":
-    network     => $id,
+    network     => $networkID,
     mask        => $mask,
     range       => $range,
     gateway     => $gateway,
--- manifests/services/dhcp/pools.pp
+++ manifests/services/dhcp/pools.pp
@@ -1,9 +1,19 @@
 # Configures the DHCP pools 
 class profile::services::dhcp::pools {
   $networks = lookup('profile::networks', {
-    'value_type' => Array[String],
-    'merge'      => 'unique',
+    'value_type' => Variant[Array[String], Hash],
   })
 
-  profile::services::dhcp::pool { $networks:}
+  if ( $networks =~ Array ) {
+    profile::services::dhcp::pool { $networks:}
+  } else {
+    $networks.each | $networkname, $data | {
+      profile::services::dhcp::pool { $networkname :
+        cidr    => $data['ipv4']['cidr'], 
+        domain  => $data['domain'],
+        gateway => $data['ipv4']['gateway'],
+        range   => $data['ipv4']['range'],
+      }
+    }
+  }
 }
