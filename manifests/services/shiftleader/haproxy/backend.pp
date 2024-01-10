3b388f2e8ef72e333f7cebd57d7fcfad23bc873f
Eigil Obrestad <eigil-git@obrestad.org>
2024-01-10 15:31:26 +0100

    Use the management-if name from sl2 if its not given in hiera

--- manifests/services/shiftleader/haproxy/backend.pp
+++ manifests/services/shiftleader/haproxy/backend.pp
@@ -1,6 +1,9 @@
 # Haproxy backend for shiftleader
 class profile::services::shiftleader::haproxy::backend {
-  $if = lookup('profile::interfaces::management', String)
+  $if = lookup('profile::interfaces::management', {
+    'default_value' => $::sl2['server']['primary_interface']['name'],
+    'value_type'    => String,
+  })
 
   ::profile::services::haproxy::backend { 'Shiftleader2':
     backend   => 'bk_shiftleader2',
