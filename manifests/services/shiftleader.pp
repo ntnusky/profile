ad5b353346357b1304f897c63ddd68f9b60909ed
Eigil Obrestad <eigil-git@obrestad.org>
2024-01-10 15:48:27 +0100

    Install apache

--- manifests/services/shiftleader.pp
+++ manifests/services/shiftleader.pp
@@ -1,5 +1,6 @@
 # This class installs and configures both the shiftleader API and frontend.
 class profile::services::shiftleader {
+  include ::profile::services::apache
   include ::profile::services::shiftleader::haproxy::backend
   include ::shiftleader::web 
 
