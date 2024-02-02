961ac54bdbf1b47431f740593ddadf7e6e81605a
Eigil Obrestad <eigil-git@obrestad.org>
2024-02-02 10:34:33 +0100

    We no longer manage OSD-assignment by puppet

--- manifests/ceph/osd.pp
+++ manifests/ceph/osd.pp
@@ -1,13 +1,10 @@
-# Installs and configures a ceph storage node, and adds osd's according to the
-# hiera 'profile::ceph::osds' key.
+# Installs and configures a ceph storage node
 class profile::ceph::osd {
   $bootstrap_osd_key = lookup('profile::ceph::osd_bootstrap_key', String)
   $memory_target = lookup('profile::ceph::osd::memory::target', {
     'default_value' => 1610612736,
     'value_type'    => Integer,
   })
-
-  $osds = lookup('profile::ceph::osds', Hash)
 
   require ::profile::ceph::base
   include ::profile::ceph::firewall::daemons
@@ -16,10 +13,6 @@ class profile::ceph::osd {
   ceph::key {'client.bootstrap-osd':
     keyring_path => '/var/lib/ceph/bootstrap-osd/ceph.keyring',
     secret       => $bootstrap_osd_key,
-  }
-
-  class { '::ceph::osds':
-    args => $osds,
   }
 
   sudo::conf { 'cephosd':
