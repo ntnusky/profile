33faa5a2e076ca96d8e15e40e058583cf5e27136
Eigil Obrestad <eigil-git@obrestad.org>
2024-03-04 09:26:11 +0100

    Make it work with SL1

--- manifests/services/mysql/haproxy/backend.pp
+++ manifests/services/mysql/haproxy/backend.pp
@@ -1,8 +1,14 @@
 # Configures the haproxy backend for this mysql cluster member
 class profile::services::mysql::haproxy::backend {
   # TODO: Remove the hiera-lookup and just juse the sl2-data
+  if($sl2) {
+    $default = $::sl2['server']['primary_interface']['name'],
+  } else {
+    $default = undef,
+  }
+
   $if = lookup('profile::interfaces::management', {
-    'default_value' => $::sl2['server']['primary_interface']['name'],
+    'default_value' => $default, 
     'value_type'    => String,
   })
 
