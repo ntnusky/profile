0fddb41a7aa780881d4d949d47a2848a86387357
Eigil Obrestad <eigil-git@obrestad.org>
2024-03-22 13:11:28 +0100

    Listen correctly

--- manifests/services/info.pp
+++ manifests/services/info.pp
@@ -1,16 +1,29 @@
 # Define an apache vhost for random static info
 class profile::services::info {
   $vhost = lookup('profile::info::maillist::fqdn', Stdlib::Fqdn)
-  $management_if = lookup('profile::interfaces::management', String)
-  $mip = $facts['networking']['interfaces'][$management_if]['ip']
-  $management_ipv4 = lookup("profile::baseconfig::network::interfaces.${management_if}.ipv4.address", {
-    'value_type'    => Stdlib::IP::Address::V4,
-    'default_value' => $mip,
-  })
-  $management_ipv6 = $::facts['networking']['interfaces'][$management_if]['ip6']
   $auth_password = lookup('profile::info::auth_password', String)
   $auth_user = lookup('profile::info::auth_user', String, 'first', 'sympa')
   $pw_hash = apache::pw_hash($auth_password)
+
+  $sl_version = lookup('profile::shiftleader::major::version', {
+    'default_value' => 1,
+    'value_type'    => Integer,
+  })
+
+  if($sl_version == 1) {
+    $management_if = lookup('profile::interfaces::management', String)
+    $mip = $facts['networking']['interfaces'][$management_if]['ip']
+    $management_ipv4 = lookup("profile::baseconfig::network::interfaces.${management_if}.ipv4.address", {
+      'value_type'    => Stdlib::IP::Address::V4,
+      'default_value' => $mip,
+    })
+    $management_ipv6 = $::facts['networking']['interfaces'][$management_if]['ip6']
+    $listen = {
+      'ip'  => concat([], $management_ipv4, $management_ipv6),
+    }
+  } else {
+    $listen = {}
+  }
 
   require ::profile::services::apache
   contain ::profile::services::info::maillist
@@ -18,7 +31,6 @@ class profile::services::info {
   apache::vhost { "${vhost}-http":
     servername        => $vhost,
     port              => 80,
-    ip                => concat([], $management_ipv4, $management_ipv6),
     add_listen        => false,
     docroot           => "/var/www/${vhost}",
     access_log_format => 'forwarded',
@@ -32,6 +44,7 @@ class profile::services::info {
         auth_require   => 'valid-user',
       },
     ],
+    *                 => $listen,
   }
 
   file { "/var/www/${vhost}/.htpasswd":
