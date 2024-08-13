f7a48f1182435e6522a6f54be73a853e1051f499
Eigil Obrestad <eigil-git@obrestad.org>
2024-08-13 14:24:02 +0200

    Add a zabbix server profile

---
+++ manifests/zabbix/server.pp
@@ -0,0 +1,39 @@
+# This class installs a zabbix-server and a zabbix-frontend
+class profile::zabbix::server {
+  $db_pass = lookup('profile::zabbix::database::password', String)
+
+  $cert = lookup("profile::zabbix::web::cert")
+  $key = lookup("profile::zabbix::web::key")
+
+  include ::apache::mod::php
+  include ::profile::services::apache::server
+
+  file { '/etc/ssl/private/zabbix.crt':
+    ensure    => 'present',
+    content   => $cert,
+    mode      => '0600',
+    notify    => Service['httpd'],
+  }
+
+  file { '/etc/ssl/private/zabbix.key':
+    ensure    => 'present',
+    content   => $key,
+    mode      => '0600',
+    notify    => Service['httpd'],
+    show_diff => false,
+  }
+
+  class { 'zabbix::server':
+    database_type     => 'mysql',
+    database_password => $db_pass,
+  }
+
+  class { 'zabbix::web':
+    zabbix_url        => $zabbix_url,
+    apache_use_ssl    => true,
+    apache_ssl_cert   => '/etc/ssl/private/zabbix.crt',
+    apache_ssl_key    => '/etc/ssl/private/zabbix.key',
+    database_type     => 'mysql',
+    database_password => $db_pass,
+  }
+}
