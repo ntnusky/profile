866ebbc5d8ba0e818175ce069e0f57b21b832068
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2024-08-16 12:32:38 +0200

    Add a MySQL user for the Zabbix Monitoring Agent

---
+++ manifests/services/mysql/users/zabbixmonitor.pp
@@ -0,0 +1,16 @@
+# User and grats for the Zabbix Monitoring
+class profile::services::mysql::users::zabbixmonitor {
+  $zbx_monitor_password = lookup('profile::mysqlcluster::zbx_monitor_password')
+
+  mysql_user { 'zbx_monitor@%':
+    ensure        => 'present',
+    password_hash => mysql_password($zbx_monitor_password)
+  }
+  ->mysql_grant { 'zbx_monitor@%/*.*':
+    ensure     => 'present',
+    options    => ['GRANT'],
+    privileges => ['REPLICATION CLIENT', 'PROCESS', 'SHOW DATABASES', 'SHOW VIEW'],
+    table      => '*.*',
+    user       => 'zbx_monitor@%',
+  }
+}
