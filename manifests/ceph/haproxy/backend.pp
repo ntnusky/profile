9a1b8e030d56ebc84d3e455c4ce46dda8a5ccfbf
Eigil Obrestad <eigil-git@obrestad.org>
2024-08-19 14:16:21 +0200

    Checks are good to have

--- manifests/ceph/haproxy/backend.pp
+++ manifests/ceph/haproxy/backend.pp
@@ -4,6 +4,7 @@ class profile::ceph::haproxy::backend {
   ::profile::services::haproxy::backend { 'Ceph-REST':
     backend   => 'bk_Ceph-REST',
     interface => $::sl2['server']['primary_interface']['name'],
+    options   => 'check',
     port      => '8003',
   }
 }
