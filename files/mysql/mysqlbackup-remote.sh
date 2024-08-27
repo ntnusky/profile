d8bd9a8d94a1d9ec61f77801c246486e4228f5fd
Eigil Obrestad <eigil-git@obrestad.org>
2024-08-27 13:06:13 +0200

    Backup in a single transaction

--- files/mysql/mysqlbackup-remote.sh
+++ files/mysql/mysqlbackup-remote.sh
@@ -4,7 +4,7 @@ share='/mnt/backup'
 filename="${location}/MysqlDump.$(date +%y%m%d%H%M%S).sql.gz"
 
 logger "Staring mysql backup"
-mysqldump --all-databases | gzip > $filename
+mysqldump --all-databases --single-transaction=TRUE | gzip > $filename
 chmod 600 $filename
 logger "Finished mysql backup"
 
