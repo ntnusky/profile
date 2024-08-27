9ebfb5a2ff6cb071dcd9e9c561ef8330d483bb86
Eigil Obrestad <eigil-git@obrestad.org>
2024-08-27 09:30:13 +0200

    Put it the right place

--- manifests/ceph/key/mgr.pp
+++ manifests/ceph/key/mgr.pp
@@ -3,12 +3,13 @@ class profile::ceph::key::mgr {
   $mgr_key = lookup('profile::ceph::mgr_key', String)
 
   ceph::key { "mgr.${::hostname}":
-    secret  => $mgr_key,
-    cap_mon => 'allow *',
-    cap_osd => 'allow *',
-    cap_mds => 'allow *',
-    cluster => 'ceph',
-    group   => 'ceph',
-    user    => 'ceph',
+    secret       => $mgr_key,
+    cap_mon      => 'allow *',
+    cap_osd      => 'allow *',
+    cap_mds      => 'allow *',
+    cluster      => 'ceph',
+    group        => 'ceph',
+    keyring_path => "/var/lib/ceph/mgr/ceph-${::hostname}/keyring",
+    user         => 'ceph',
   }
 }
