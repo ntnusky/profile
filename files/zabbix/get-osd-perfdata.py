fafad31636bf4a524913b5bb8f92c83dfb0b83a7
Eigil Obrestad <eigil-git@obrestad.org>
2024-08-30 16:03:01 +0200

    Grab more OSD-metrics

--- files/zabbix/get-osd-perfdata.py
+++ files/zabbix/get-osd-perfdata.py
@@ -20,6 +20,21 @@ for key in data.keys():
   used = data[key]['bluefs.db_used_bytes']['value']
   result[key.lstrip('osd.')] = {
     'dbutil': 100*used/total,
+    'dbsize': total,
+    'dbused': used,
+    'op_latency': data[key]['osd.op_latency']['value'],
+    # Read
+    'op_r_latency': data[key]['osd.op_r_latency']['value'],
+    # Write
+    'op_w_latency': data[key]['osd.op_w_latency']['value'],
+    # Read-modify-write
+    'op_rw_latency': data[key]['osd.op_rw_latency']['value'],
+    # PG's
+    'numpg': data[key]['osd.numpg']['value'],
+    # Size
+    'stat_bytes': data[key]['osd.stat_bytes']['value'],
+    # Used size
+    'stat_bytes_used': data[key]['osd.stat_bytes_used']['value'],
   }
 
 print(json.dumps({'osds': result},separators=(',', ':')))
