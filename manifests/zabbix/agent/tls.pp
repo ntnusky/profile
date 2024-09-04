507ae291d8aee7ba32aa9ca4991e828fcf50f844
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2024-09-04 10:30:22 +0200

    Zabbix conf for TLS checking

---
+++ manifests/zabbix/agent/tls.pp
@@ -0,0 +1,46 @@
+# Zabbix config for TLS checks
+class profile::zabbix::agent::tls {
+
+  $domains = lookup('zabbix::tls::expiry::domains', Array[String])
+
+  zabbix::userparameters { 'ssl_cert_check':
+    content => join([
+      'UserParameter=ssl_cert_check_valid[*], /usr/local/bin/ssl_cert_check.sh valid "$1" "$2" "$3" "$4" "$5',
+      'UserParameter=ssl_cert_check_expire[*], /usr/local/bin/ssl_cert_check.sh expire "$1" "$2" "$3" "$4" "$5"',
+      'UserParameter=ssl_cert_check_json[*], /usr/local/bin/ssl_cert_check.sh json "$1" "$2" "$3" "$4" "$5"'
+    ], "\n"),
+  }
+
+  zabbix::userparameters { 'ssl_cert_discovery':
+    content => 'UserParameter=ssl_cert_list[*],/bin/cat /etc/zabbix/ssl_cert_list.json',
+  }
+
+  file { '/usr/local/bin/ssl_cert_check.sh':
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/profile/zabbix/ssl_cert_check.sh',
+  }
+
+  $domain_list = $domains.map |$domain,$port| {
+    {
+      '{#IPADDR}'    => $domain,
+      '{#SSLPORT}'   => $port,
+      '{#SSLDOMAIN}' => $domain,
+      '{#TIMEOUT}'   => 30,
+    }
+  }
+
+  $domain_hash = {
+    data => $domain_list
+  }
+
+  file { '/etc/zabbix/ssl_cert_list.json':
+    ensure  => present,
+    owner   => 'zabbix_agent',
+    group   => 'zabbix_agent',
+    mode    => '0644',
+    content => template('profile/zabbix_agent/ssl_cert_list.erb'),
+  }
+}
