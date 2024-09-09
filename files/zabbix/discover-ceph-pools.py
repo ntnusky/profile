46aa74cfd78afbe77894417911150e707d83b716
Eigil Obrestad <eigil-git@obrestad.org>
2024-09-09 10:01:20 +0200

    Discover POOL-ID as well

--- files/zabbix/discover-ceph-pools.py
+++ files/zabbix/discover-ceph-pools.py
@@ -6,6 +6,10 @@ report = subprocess.run(["/usr/bin/ceph", "report"], capture_output=True)
 data = json.loads(report.stdout)
 
 crush_rules = {x['rule_id']: x['rule_name'] for x in data['crushmap']['rules']}
-pools = [{'{#POOLNAME}': x['pool_name'], '{#CRUSHRULE}': crush_rules[x['crush_rule']]} for x in data['osdmap']['pools']]
+pools = [{
+  '{#POOLNAME}': x['pool_name'], 
+  '{#POOLID}': x['pool'], 
+  '{#CRUSHRULE}': crush_rules[x['crush_rule']]
+} for x in data['osdmap']['pools']]
 
 print(json.dumps(pools))
