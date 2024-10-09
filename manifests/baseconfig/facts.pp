120d94f42e59f31e30dd14c02d8153fdb936bec4
Eigil Obrestad <eigil-git@obrestad.org>
2024-10-09 12:40:33 +0200

    Warn if region is not set

--- manifests/baseconfig/facts.pp
+++ manifests/baseconfig/facts.pp
@@ -2,7 +2,8 @@
 # variable in hiera
 class profile::baseconfig::facts {
   $region = lookup('ntnuopenstack::region', {
-    'value_type' => String,
+    'default_value' => undef,
+    'value_type'    => Optional[String],
   })
 
   file { '/etc/puppetlabs/facter':
@@ -17,13 +18,22 @@ class profile::baseconfig::facts {
     group  => 'root',
     mode   => '0755',
   }
-  -> file { '/etc/puppetlabs/facter/facts.d/openstack.yaml':
-    ensure  => 'file',
-    mode    => '0644',
-    content => to_yaml( {
-      openstack => {
-        region => $region,
-      },
-    } ),
+
+  if($region) {
+    file { '/etc/puppetlabs/facter/facts.d/openstack.yaml':
+      ensure  => 'file',
+      mode    => '0644',
+      content => to_yaml( {
+        openstack => {
+          region => $region,
+        },
+      } ),
+      require => File['/etc/puppetlabs/facter/facts.d'],
+    }
+  } else {
+    file { '/etc/puppetlabs/facter/facts.d/openstack.yaml':
+      ensure => absent,
+    }
+    warning { "There is no region set in hiera." }
   }
 }
