efea6c1e85deda7c7e5ce589275050d15ad74dc9
Eigil Obrestad <eigil-git@obrestad.org>
2024-10-11 15:27:53 +0200

    TCP is default, and named otherwise

--- manifests/services/puppet/db/firewall.pp
+++ manifests/services/puppet/db/firewall.pp
@@ -1,7 +1,6 @@
 # Configures the firewall for puppetmasters. 
 class profile::services::puppet::db::firewall {
   ::profile::firewall::infra::region { 'PuppetDB':
-    protocol => 'tcp',
-    port     => 8081,
+    port => 8081,
   }
 }
