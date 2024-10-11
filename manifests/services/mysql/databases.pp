cc428b2df24f28bf0cb264fd9a297806b79c7a1c
Eigil Obrestad <eigil-git@obrestad.org>
2024-10-11 10:38:20 +0200

    Onlu create the sl2-database where needed

--- manifests/services/mysql/databases.pp
+++ manifests/services/mysql/databases.pp
@@ -1,4 +1,10 @@
 # Configures mysql with the appropriate databases
 class profile::services::mysql::databases {
-  include ::shiftleader::database
+  $sl2dbpassword = lookup('shiftleader::params::database_password', {
+    'default_value' => undef,
+    'value_type'    => Optional[String], 
+  })
+  if($sl2dbpassword) {
+    include ::shiftleader::database
+  }
 }
