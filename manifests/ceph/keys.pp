1eab345ba8335b70e03830e74117bde38aa9f06a
Eigil Obrestad <eigil-git@obrestad.org>
2024-11-13 10:48:03 +0100

    Use profile::ceph::key to create keys

--- manifests/ceph/keys.pp
+++ manifests/ceph/keys.pp
@@ -6,37 +6,8 @@ class profile::ceph::keys {
   })
 
   $keydata.each | $keyname, $data | {
-    if($data['caps_mds']) {
-      $cap_mds = $data['caps_mds'].join(', ')
-    } else {
-      $cap_mds = undef
-    }
-
-    if($data['caps_mgr']) {
-      $cap_mgr = $data['caps_mgr'].join(', ')
-    } else {
-      $cap_mgr = undef
-    }
-
-    if($data['caps_mon']) {
-      $cap_mon = $data['caps_mon'].join(', ')
-    } else {
-      $cap_mon = undef
-    }
-
-    if($data['caps_osd']) {
-      $cap_osd = $data['caps_osd'].join(', ')
-    } else {
-      $cap_osd = undef
-    }
-
-    ceph::key { $keyname:
-      cap_mds => $cap_mds,
-      cap_mgr => $cap_mgr,
-      cap_mon => $cap_mon,
-      cap_osd => $cap_osd,
-      inject  => pick($data['inject'], true),
-      secret  => $data['secret'],
+    ::profile::ceph::key { $keyname :
+      inject => pick($data['inject'], true),
     }
   }
 }
