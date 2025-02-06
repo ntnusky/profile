75d7aa32ef690be7a5edbe59aa9549e09610df95
Steinar Otto Sjøholt <steinar.o.sjoholt@ntnu.no>
2025-02-06 14:51:27 +0100

    Add configuration of locale

---
+++ manifests/baseconfig/locale.pp
@@ -0,0 +1,16 @@
+# Class for configuring OS locale
+class profile::baseconfig::locale {
+  $default_locale = lookup('profile::locales::default', {
+      'default_value' => 'C', # The locales module default
+      'value_type'    => String
+  })
+  $locales = lookup('profile::locales', {
+      'default_value' => ['en_US.UTF-8 UTF-8', 'en_GB.UTF8 UTF-8'], # Override module default (en_US, de_DE)
+      'value_type'    => Array[String]
+  })
+
+  class { 'locales':
+    default_locale => $default_locale,
+    locales        => $locales,
+  }
+}
