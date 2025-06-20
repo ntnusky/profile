b2c866f17c80101b245ab03c416b2d8966587641
Eigil Obrestad <eigil-git@obrestad.org>
2025-06-20 13:52:53 +0200

    Add upgrade alias, and refactor to concat to allow removal of lines as well

--- manifests/baseconfig/alias.pp
+++ manifests/baseconfig/alias.pp
@@ -4,25 +4,29 @@ define profile::baseconfig::alias (
 ) {
   if($username == 'root') {
     $filename = '/root/.bash_aliases'
+
+    concat_fragment { "Alias oppgrader in ${filename}":
+      content => "alias pca='sudo puppet agent --test --server ${caserver}'",
+      target  => $filename,
+    }
   } else {
     $filename = "/home/${username}/.bash_aliases"
   }
 
-  file { $filename:
+  concat { $filename:
     ensure => present,
-    owner => $username, 
-    mode  => '0644',
+    owner  => $username,
+    group  => ($username == 'root') ? 'root' : 'users',
+    mode   => '0644',
   }
 
   $caserver = lookup('profile::puppet::caserver')
-  file_line { "Alias pca for ${username}":
-    path    => $filename,
-    line    => "alias pca='sudo puppet agent --test --server ${caserver}'",
-    require => File[$filename],
+  concat_fragment { "Alias pca in ${filename}":
+    content => "alias pca='sudo puppet agent --test --server ${caserver}'",
+    target  => $filename,
   }
-  file_line { "Alias pat for ${username}":
-    path    => $filename,
-    line    => "alias pat='sudo puppet agent --test'",
-    require => File[$filename],
+  concat_fragment { "Alias pat in ${filename}":
+    content => "alias pca='sudo puppet agent --test --server ${caserver}'",
+    target  => $filename,
   }
 }
