82a16e93621819d62881c8ff92b0fe71fd3632a5
Eigil Obrestad <eigil-git@obrestad.org>
2025-06-23 09:03:23 +0200

    Double-quotes are needed for \n

--- manifests/baseconfig/alias.pp
+++ manifests/baseconfig/alias.pp
@@ -20,7 +20,7 @@ define profile::baseconfig::alias (
     mode   => '0644',
   }
   concat_fragment { "Puppet-heading in ${filename}":
-    content => '# This file is managed by puppet.\n',
+    content => "# This file is managed by puppet.\n",
     order   => 1,
     target  => $filename,
   }
