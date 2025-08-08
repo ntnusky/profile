cd99d1c3b1f40cf282debf8206be186a38e4b920
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2025-08-08 12:18:57 +0200

    Attempt to install correct erlang version from repo, based on rabbit version

--- manifests/services/erlang.pp
+++ manifests/services/erlang.pp
@@ -1,27 +1,51 @@
 # Module to install erlang from Erlangs repositories
 class profile::services::erlang {
-  $version = lookup('profile::services::erlang::version', {
-    'default_value' => '1:25.3.2.21-1',
+  $erlang_version = lookup('profile::services::erlang::version', {
+    'default_value' => 'auto',
   })
 
+  $rabbit_version = lookup('rabbitmq::package_ensure')
   $distro = $facts['os']['distro']['codename']
 
-  apt::source { 'erlang':
-    comment  => '',
-    location => "https://deb1.rabbitmq.com/rabbitmq-erlang/ubuntu/${distro}",
-    release  => $distro,
-    repos    => 'main',
-    key      => {
-      id     => '0A9AF2115F4687BD29803A206B73A36E6026DFCA',
-      source => 'https://keys.openpgp.org/vks/v1/by-fingerprint/0A9AF2115F4687BD29803A206B73A36E6026DFCA',
-    },
-    notify   => Exec['apt_update'],
-    before   => Apt::Pin['erlang'],
-  }
+  if ( versioncmp($rabbit_version, '3.10.13-1') == 1 ) {
 
-  apt::pin { 'erlang':
-    packages => 'erlang*',
-    version  => $version,
-    priority => 1000,
+    if ( $erlang_version == 'auto' ) {
+      $erlang_verision_real = $rabbit_version ? {
+        /^(3.10.2?|3.11)/ => '1:25.3.2.21-1',
+        /^(3.12|3.13)/    => '1:26.2.5.13-1',
+        /^(4.0|4.1)/      => '1:27.3.4.2-1',
+        default           => undef,
+      }
+    } else {
+      $erlang_version_real = $erlang_version
+    }
+
+    if (!$erlang_version_real) {
+      fail("Rabbit version ${rabbit_version} is not supported")
+    }
+
+    apt::source { 'erlang':
+      comment  => '',
+      location => "https://deb1.rabbitmq.com/rabbitmq-erlang/ubuntu/${distro}",
+      release  => $distro,
+      repos    => 'main',
+      key      => {
+        id     => '0A9AF2115F4687BD29803A206B73A36E6026DFCA',
+        source => 'https://keys.openpgp.org/vks/v1/by-fingerprint/0A9AF2115F4687BD29803A206B73A36E6026DFCA',
+      },
+      notify   => Exec['apt_update'],
+      before   => Apt::Pin['erlang'],
+    }
+
+    apt::pin { 'erlang':
+      packages => 'erlang*',
+      version  => $erlang_version_real,
+      priority => 1000,
+    }
+
+    package { 'erlang':
+      ensure  => $erlang_version_real,
+      require => Apt::Source['erlang'],
+    }
   }
 }
