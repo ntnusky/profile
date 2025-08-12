438d6ae974d50be003325c3eb01fb97a584b15ef
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2025-08-12 13:31:46 +0200

    New zookeper stats script for zabbix. More robust

--- files/zabbix/zookeeper-stats.py
+++ files/zabbix/zookeeper-stats.py
@@ -2,21 +2,43 @@
 import json
 import socket
 import sys
+import time
 
 data = {}
 
 # Try to fetch monitoring-data from the zookeeper cluster. If it fails,
 # return that the server is unavailable.
 try:
+  tmp_total_data = []
+  timeout = 2
   client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   client_socket.connect(('localhost', 2181))
+  client_socket.setblocking(0)
   client_socket.send(b'mntr')
-  statusdata = client_socket.recv(1024).decode()
+
+  begin = time.time()
+  while True:
+      if tmp_total_data and time.time()-begin > timeout:
+          break
+      elif time.time()-begin > timeout*2:
+          break
+
+      try:
+          tmp_data = client_socket.recv(1024).decode()
+          if tmp_data:
+              tmp_total_data.append(tmp_data)
+              begin = time.time()
+          else:
+              time.sleep(0.1)
+      except:
+          pass
+
+  statusdata = ''.join(tmp_total_data)
 except:
   data['available'] = 0
   print(json.dumps(data))
   sys.exit(1)
-  
+
 # If data is received, the server is available :)
 data['available'] = 1
 
