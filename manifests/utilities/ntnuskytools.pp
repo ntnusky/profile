0f3e25e29739bdd9d94d09ef6095b7325e744761
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2025-09-26 09:09:38 +0200

    Make the source configurable, but with a sane default

--- manifests/utilities/ntnuskytools.pp
+++ manifests/utilities/ntnuskytools.pp
@@ -1,5 +1,11 @@
 # Install our homemade administration scripts
 class profile::utilities::ntnuskytools {
+
+  $repo_source = lookup('profile::utilities::ntnuskytools::repo', {
+    'default_value' => 'https://git.ntnu.no/ntnusky/tools.git',
+    'value_type'    => String
+  })
+
   file { '/usr/ntnusky':
     ensure => directory,
     owner  => 'root',
@@ -9,7 +15,7 @@ class profile::utilities::ntnuskytools {
   vcsrepo { '/usr/ntnusky/tools':
     ensure   => latest,
     provider => git,
-    source   => 'https://git.ntnu.no/ntnusky/tools.git',
+    source   => $repo_source,
     revision => master,
     require  => File['/usr/ntnusky'],
   }
